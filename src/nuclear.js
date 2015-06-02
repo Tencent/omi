eceeb26963b6897431ae95cb20f22fd422b18079
张磊 <kmdjs@qq.com>
2015-06-02 13:28:09 +0800

    新增drag组件

--- src/nuclear.js
+++ src/nuclear.js
@@ -3,26 +3,39 @@
 Nuclear.renderList = [];
 
 Nuclear.extend = function (obj) {
-    obj.ctor = function (selector,option) {
-        this.option = option;
-        this.parent = document.querySelector(selector);
-        if (obj.didMount) {
-            obj.didMount.call(this);
-        }
-        Nuclear.observe(this.option, Nuclear.debounce(Nuclear.render, 50));
-        Nuclear.renderList.push({
-            tpl: this.render(),
-            data:this.option, 
-            parent: this.parent,
-            eventBinding:obj.eventBinding,
-            self:this
-        });
+    obj.ctor = function (selector, option) {
+        if (typeof selector === "string" || Nuclear.isElement(selector)) {
+            this.option = option;
+            this.parent = document.querySelector(selector);
+            if (obj.install) {
+                obj.install.call(this);
+            }
+            Nuclear.observe(this.option, Nuclear.debounce(Nuclear.render, 50));
+            Nuclear.renderList.push({
+                tpl: this.render(),
+                data: this.option,
+                parent: this.parent,
+                eventBinding: obj.eventBinding,
+                self: this
+            });
 
-        Nuclear.render();
+            Nuclear.render();
+        } else {
+            this.option = selector;
+            if (obj.install) {
+                obj.install.call(this);
+            }
+           
+        }
     }
     return Nuclear.Class.extend(obj);
 }
-
+Nuclear.isElement=function(o) {
+    return (
+      typeof HTMLElement === "object" ? o instanceof HTMLElement : //DOM2
+      o && typeof o === "object" && o !== null && o.nodeType === 1 && typeof o.nodeName === "string"
+  );
+}
 Nuclear.render = function () {
     for (var i = 0, len = Nuclear.renderList.length; i < len; i++) {
         var item = Nuclear.renderList[i];
@@ -50,3 +63,323 @@ Nuclear.debounce=function (func, wait, immediate) {
         if (callNow) func.apply(context, args);
     };
 }
+
+
+
+
+
+
+
+
+Nuclear.addEvent = (function () {
+    return function (el, type, fn) {
+        if (el && el.nodeName || el === window) {
+            el.addEventListener(type, fn, false);
+        } else if (el && el.length) {
+            for (var i = 0; i < el.length; i++) {
+                Nuclear.addEvent(el[i], type, fn);
+            }
+        }
+    };
+})();
+
+Nuclear.removeEvent = (function () {
+    return function (el, type, fn) {
+        if (el && el.nodeName || el === window) {
+            el.removeEventListener(type, fn, false);
+        } else if (el && el.length) {
+            for (var i = 0; i < el.length; i++) {
+                Nuclear.removeEvent.removeEvent(el[i], type, fn);
+            }
+        }
+    };
+})();
+
+Nuclear.getNodeIndex = function (node) {
+    var index = 0;
+    while ((node = node.previousSibling)) {
+        if (node.nodeType != 3 || !/^\s*$/.test(node.data)) {
+            index++;
+        }
+    }
+    return index;
+}
+
+Nuclear.hasClass = function (ele, cls) {
+    return !!ele.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));
+}
+
+Nuclear.addClass = function (node, cls) {
+    if (Nuclear.isNodeList(node)) {
+        Nuclear._iteration("addClass", arguments);
+        return;
+    }
+    if (!Nuclear.hasClass(node, cls)) node.className += " " + cls;
+
+}
+
+Nuclear.removeClass = function (node, cls) {
+    if (Nuclear.isNodeList(node)) {
+        Nuclear._iteration("removeClass", arguments);
+        return;
+    }
+    if (Nuclear.hasClass(node, cls)) {
+        var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');
+        node.className = node.className.replace(reg, ' ');
+    }
+}
+
+Nuclear.toggleClass = function (node, classStr) {
+    if (Nuclear.isNodeList(node)) {
+        Nuclear._iteration("toggleClass", arguments);
+        return;
+    }
+
+    var cls = ' ' + node.className + ' ';
+    if (cls.indexOf(' ' + Nuclear.trim(classStr) + ' ') >= 0) {
+        Nuclear.removeClass(node, classStr);
+    } else {
+        Nuclear.addClass(node, classStr);
+    }
+}
+
+Nuclear.trim = function (str) {
+    return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+};
+
+Nuclear._iteration = function (method, args) {
+    var args = Array.prototype.slice.call(args);
+    Nuclear.each(args[0], function (index) {
+        var temp = args[0];
+        args[0] = args[0][index];
+        Nuclear[method].apply(Nuclear, args);
+        args[0] = temp;
+    })
+}
+
+Nuclear.css = function (node, styleName, value) {
+    if (!node) return;
+
+    if (Nuclear.isNodeList(node)) {
+        Nuclear._iteration("css", arguments);
+        return;
+    }
+
+    if (arguments.length == 3 && node.style) {  //change by bizai
+        node.style[styleName] = value;
+    }
+    //因为有transition的存在，所有这行代码需要注释掉
+    //if (node.style[styleName]) return node.style[styleName];
+    return window.getComputedStyle(node, null)[styleName];
+
+},
+
+
+Nuclear.each = function (list, fn) {
+    for (var i = 0, len = list.length; i < len ; i++) {
+        fn.call(list[i], i);
+    }
+}
+
+Nuclear.offset = function (el) {
+    var _t = 0,
+        _l = 0;
+    if (document.documentElement.getBoundingClientRect && el.getBoundingClientRect) {
+        var box = el.getBoundingClientRect();
+        _l = box.left;
+        _t = box.top;
+    } else {
+        while (el.offsetParent) {
+            _t += el.offsetTop;
+            _l += el.offsetLeft;
+            el = el.offsetParent;
+        }
+        return { left: _l, top: _t };
+    }
+    return { left: _l + Math.max(document.documentElement.scrollLeft, document.body.scrollLeft), top: _t + Math.max(document.documentElement.scrollTop, document.body.scrollTop) };
+}
+
+Nuclear.getViewport = function () {
+    var d = document.documentElement, b = document.body, w = window, div = document.createElement("div");
+    div.innerHTML = "  <link/>";
+    var lt = !(div.firstChild.nodeType === 3) ?
+                { left: b.scrollLeft || d.scrollLeft, top: b.scrollTop || d.scrollTop } :
+                { left: w.pageXOffset, top: w.pageYOffset };
+    var wh = w.innerWidth ?
+                { width: w.innerWidth, height: w.innerHeight } :
+            (d && d.clientWidth && d.clientWidth != 0 ?
+                    { width: d.clientWidth, height: d.clientHeight } :
+                    { width: b.clientWidth, height: b.clientHeight });
+
+    return { left: lt.left, top: lt.top, width: wh.width, height: wh.height };
+}
+
+//this.parent.lastChild代替
+Nuclear.getLastNode = function (parent, selector) {
+    var childs = parent.querySelectorAll(selector), len = childs.length;
+    return childs[len - 1];
+}
+
+Nuclear.isUndefined = function (o) {
+    return typeof (o) === "undefined";
+}
+
+Nuclear.domready = (function () {
+
+    var fns = [], listener
+      , doc = document
+      , hack = doc.documentElement.doScroll
+      , domContentLoaded = 'DOMContentLoaded'
+      , loaded = (hack ? /^loaded|^c/ : /^loaded|^i|^c/).test(doc.readyState)
+
+
+    if (!loaded)
+        doc.addEventListener(domContentLoaded, listener = function () {
+            doc.removeEventListener(domContentLoaded, listener)
+            loaded = 1
+            while (listener = fns.shift()) listener()
+        })
+
+    return function (fn) {
+        loaded ? setTimeout(fn, 0) : fns.push(fn)
+    }
+
+})();
+
+; (function () {
+    var class2type = {}, toString = Object.prototype.toString;
+    "Boolean Number String Function Array Date RegExp Object Error NodeList".split(" ").forEach(function (name) {
+        class2type["[object " + name + "]"] = name.toLowerCase();
+    })
+
+    var type = function (obj) {
+        if (obj == null) {
+            return obj + "";
+        }
+        // Support: Android<4.0 (functionish RegExp)
+        return typeof obj === "object" || typeof obj === "function" ?
+			class2type[toString.call(obj)] || "object" :
+			typeof obj;
+    }
+
+    var isNodeList = function (obj) {
+        return type(obj) === "nodelist";
+    }
+
+    var isFunction = function (obj) {
+        return type(obj) === "function";
+
+    }
+    var isArray = function (obj) {
+        return type(obj) === "array";
+    }
+    var isWindow = function (obj) {
+        return obj != null && obj === obj.window;
+    }
+
+    var isPlainObject = function (obj) {
+        // Not plain objects:
+        // - Any object or value whose internal [[Class]] property is not "[object Object]"
+        // - DOM nodes
+        // - window
+        if (type(obj) !== "object" || obj.nodeType || isWindow(obj)) {
+            return false;
+        }
+
+        if (obj.constructor &&
+				!hasOwn.call(obj.constructor.prototype, "isPrototypeOf")) {
+            return false;
+        }
+
+        // If the function hasn't returned already, we're confident that
+        // |obj| is a plain object, created by {} or constructed with new Object
+        return true;
+    }
+
+    Nuclear.type = type;
+    Nuclear.isFunction = isFunction;
+    Nuclear.isArray = isArray;
+    Nuclear.isWindow = isWindow;
+    Nuclear.isPlainObject = isPlainObject;
+    Nuclear.isNodeList = isNodeList;
+})();
+
+Nuclear.merge = function () {
+    var options, name, src, copy, copyIsArray, clone,
+		target = arguments[0] || {},
+		i = 1,
+		length = arguments.length,
+		deep = false;
+
+    // Handle a deep copy situation
+    if (typeof target === "boolean") {
+        deep = target;
+
+        // Skip the boolean and the target
+        target = arguments[i] || {};
+        i++;
+    }
+
+    // Handle case when target is a string or something (possible in deep copy)
+    if (typeof target !== "object" && !Nuclear.isFunction(target)) {
+        target = {};
+    }
+
+    // Extend jQuery itself if only one argument is passed
+    if (i === length) {
+        target = this;
+        i--;
+    }
+
+    for (; i < length; i++) {
+        // Only deal with non-null/undefined values
+        if ((options = arguments[i]) != null) {
+            // Extend the base object
+            for (name in options) {
+                src = target[name];
+                copy = options[name];
+
+                // Prevent never-ending loop
+                if (target === copy) {
+                    continue;
+                }
+
+                // Recurse if we're merging plain objects or arrays
+                if (deep && copy && (Nuclear.isPlainObject(copy) ||
+					(copyIsArray = Nuclear.isArray(copy)))) {
+
+                    if (copyIsArray) {
+                        copyIsArray = false;
+                        clone = src && Nuclear.isArray(src) ? src : [];
+
+                    } else {
+                        clone = src && Nuclear.isPlainObject(src) ? src : {};
+                    }
+
+                    // Never move original objects, clone them
+                    target[name] = Nuclear.merge(deep, clone, copy);
+
+                    // Don't bring in undefined values
+                } else if (copy !== undefined) {
+                    target[name] = copy;
+                }
+            }
+        }
+    }
+
+    // Return the modified object
+    return target;
+};
+
+//因为js单线程的关系，所以就算同时执行两次Nuclear.uuid,得到的uuid也不可能相同
+Nuclear.uuid = function () {
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = (d + Math.random() * 16) % 16 | 0;
+        d = Math.floor(d / 16);
+        return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    return uuid;
+};
+
+
