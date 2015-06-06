d7e82575cf90fce4473a29beb894ca0e8c0d7035
张磊 <kmdjs@qq.com>
2015-06-06 13:47:56 +0800

    节流

--- src/nuclear.js
+++ src/nuclear.js
@@ -67,16 +67,43 @@ Nuclear._mixObj = function (obj) {
     }
 }
 
-Nuclear.throttle = function (fn, delay) {
-    var timer = null;
-    return function () {
-        var context = this, args = arguments;
-        clearTimeout(timer);
-        timer = setTimeout(function () {
-            fn.apply(context, args);
-        }, delay);
+Nuclear.throttle = function (func, wait, options) {
+    var context, args, result;
+    var timeout = null;
+    // 上次执行时间点
+    var previous = 0;
+    if (!options) options = {};
+    // 延迟执行函数
+    var later = function () {
+        // 若设定了开始边界不执行选项，上次执行时间始终为0
+        previous = options.leading === false ? 0 : Date.now();
+        timeout = null;
+        result = func.apply(context, args);
+        if (!timeout) context = args = null;
     };
-};
+    return function () {
+        var now = Date.now();
+        // 首次执行时，如果设定了开始边界不执行选项，将上次执行时间设定为当前时间。
+        if (!previous && options.leading === false) previous = now;
+        // 延迟执行时间间隔
+        var remaining = wait - (now - previous);
+        context = this;
+        args = arguments;
+        // 延迟时间间隔remaining小于等于0，表示上次执行至此所间隔时间已经超过一个时间窗口
+        // remaining大于时间窗口wait，表示客户端系统时间被调整过
+        if (remaining <= 0 || remaining > wait) {
+            clearTimeout(timeout);
+            timeout = null;
+            previous = now;
+            result = func.apply(context, args);
+            if (!timeout) context = args = null;
+            //如果延迟执行不存在，且没有设定结尾边界不执行选项
+        } else if (!timeout && options.trailing !== false) {
+            timeout = setTimeout(later, remaining);
+        }
+        return result;
+    };
+}
 
 Nuclear.isElement=function(o) {
     return (
