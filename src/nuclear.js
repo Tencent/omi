fb1935e79d1921ef7063211afc6d6ae1dd9d9675
张磊 <kmdjs@qq.com>
2015-06-12 08:08:34 +0800

    参数位置

--- src/nuclear.js
+++ src/nuclear.js
@@ -608,7 +608,7 @@ Nuclear.createCanvas = function (obj) {
 
 
 Nuclear._minCanvasObj = function (obj) {
-    obj.ctor = function (selector, width, height, option) {
+    obj.ctor = function (width, height, option, selector) {
         this.canvas = document.createElement("canvas");
         this.ctx = this.canvas.getContext("2d");
         this.canvas.width = width;
