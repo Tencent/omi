452c8ba4b008371bde7ab09fb8ae09631e73c06c
kmdjs <kmdjs@qq.com>
2016-05-21 19:15:27 +0800

    fix nuclear server render problem

--- src/intro.js
+++ src/intro.js
@@ -2,7 +2,7 @@
 
     if ( typeof module === "object" && typeof module.exports === "object" ) {
         // For CommonJS and CommonJS-like environments where a proper `window`
-        // is present, execute the factory and get jQuery.
+        // is present, execute the factory and get Nuclear.
         // For environments that do not have a `window` with a `document`
         // (such as Node.js), expose a factory as module.exports.
         // This accentuates the need for the creation of a real `window`.
