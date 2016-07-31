59fcbcfc8452cd22d701fed37f1eccefdd68d0bc
kmdjs <kmdjs@qq.com>
2016-07-31 10:43:00 +0800

    replace the diffDOM lib

--- dist/nuclear.js
+++ dist/nuclear.js
@@ -418,19 +418,20 @@ Nuclear._mixObj = function (obj) {
                 this.node = null;
                 this.HTML = "";
             } else {
-               // var newNode = Nuclear.str2Dom(this._nuclearWrap(Nuclear.render(Nuclear._fixEvent(Nuclear._fixTplIndex(item.tpl), this._ncInstanceId), item.data)));
-               //if(this._nuclearDiffDom) {
-               //    Nuclear.diffDOM.apply(this.node, Nuclear.diffDOM.diff(this.node, newNode));
-               //}else {
-               //    item.parent.replaceChild(newNode, this.node);
-               //    this.node = newNode;
-               //}
-                if(this._nuclearDiffDom) {
+                // var newNode = Nuclear.str2Dom(this._nuclearWrap(Nuclear.render(Nuclear._fixEvent(Nuclear._fixTplIndex(item.tpl), this._ncInstanceId), item.data)));
+                //if(this._nuclearDiffDom) {
+                //    Nuclear.diffDOM.apply(this.node, Nuclear.diffDOM.diff(this.node, newNode));
+                //}else {
+                //    item.parent.replaceChild(newNode, this.node);
+                //    this.node = newNode;
+                //}
+                if (this._nuclearDiffDom) {
                     Nuclear.setDOM(this.node, this._nuclearWrap(Nuclear.render(Nuclear._fixEvent(Nuclear._fixTplIndex(item.tpl), this._ncInstanceId), item.data)));
-                }else {
-                    //    item.parent.replaceChild(newNode, this.node);
-                    //    this.node = newNode;
-                    //}
+                } else {
+                    var newNode = Nuclear.str2Dom(this._nuclearWrap(Nuclear.render(Nuclear._fixEvent(Nuclear._fixTplIndex(item.tpl), this._ncInstanceId), item.data)));
+                    item.parent.replaceChild(newNode, this.node);
+                    this.node = newNode;
+                }
             }
         } else {
             //第一次渲染
@@ -574,6 +575,53 @@ Nuclear._fixTplIndex = function (tpl) {
     return tpl.replace(/{{@index}}/g, "{{_nuclearIndex}}");
 };
 
+Nuclear.str2Dom = function (html) {
+    var wrapMap = {
+        option: [1, "<select multiple='multiple'>", "</select>"],
+        legend: [1, "<fieldset>", "</fieldset>"],
+        area: [1, "<map>", "</map>"],
+        param: [1, "<object>", "</object>"],
+        thead: [1, "<table>", "</table>"],
+        tr: [2, "<table><tbody>", "</tbody></table>"],
+        col: [2, "<table><tbody></tbody><colgroup>", "</colgroup></table>"],
+        td: [3, "<table><tbody><tr>", "</tr></tbody></table>"],
+        body: [0, "", ""],
+        _default: [1, "<div>", "</div>"]
+    };
+    wrapMap.optgroup = wrapMap.option;
+    wrapMap.tbody = wrapMap.tfoot = wrapMap.colgroup = wrapMap.caption = wrapMap.thead;
+    wrapMap.th = wrapMap.td;
+    var match = /<\s*\w.*?>/g.exec(html);
+    var element = document.createElement('div');
+    if (match != null) {
+        var tag = match[0].replace(/</g, '').replace(/>/g, '').split(' ')[0];
+        if (tag.toLowerCase() === 'body') {
+            //var dom = document.implementation.createDocument('http://www.w3.org/1999/xhtml', 'html', null);
+            var body = document.createElement("body");
+            // keeping the attributes
+            element.innerHTML = html.replace(/<body/g, '<div').replace(/<\/body>/g, '</div>');
+            var attrs = element.firstChild.attributes;
+            body.innerHTML = html;
+            for (var i = 0; i < attrs.length; i++) {
+                body.setAttribute(attrs[i].name, attrs[i].value);
+            }
+            return body;
+        } else {
+            var map = wrapMap[tag] || wrapMap._default;
+            html = map[1] + html + map[2];
+            element.innerHTML = html;
+            // Descend through wrappers to the right content
+            var j = map[0] + 1;
+            while (j--) {
+                element = element.lastChild;
+            }
+        }
+    } else {
+        element.innerHTML = html;
+        element = element.lastChild;
+    }
+    return element;
+};
 
 Nuclear.isUndefined = function (o) {
     return typeof (o) === "undefined";
