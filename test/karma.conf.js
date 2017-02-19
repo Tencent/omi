f444b968f9dd422755a7e7c0a0ec789ff311a918
kmdjs <kmdjs@qq.com>
2017-02-19 12:55:50 +0800

    fix ci build error

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -63,7 +63,7 @@ module.exports = function(config) {
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
+    singleRun: true,
 
     // Concurrency level
     // how many browser should be started simultaneous
