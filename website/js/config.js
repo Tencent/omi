a2c3b0595b388ec04d6dcb031f77b443d0c9b641
xcatliu <xcatliu@gmail.com>
2017-02-23 20:52:50 +0800

    Translate en_form.md en_inherit.md en_template.md

--- website/js/config.js
+++ website/js/config.js
@@ -25,7 +25,7 @@
     },
     mds:{
         cn:['installation', 'hello_world', 'components', 'communication', 'lifecycle', 'events', 'condition', 'loop', 'form', 'inherit', 'template', 'get_dom','plugin', 'thinking_in_omi','pr_env','pr_hello'],
-        en:['installation', 'hello_world', 'components', 'communication', 'lifecycle', 'events', 'condition', 'loop']
+        en:['installation', 'hello_world', 'components', 'communication', 'lifecycle', 'events', 'condition', 'loop', 'form', 'inherit', 'template']
     },
     menus:{
         cn:[
@@ -64,10 +64,11 @@
                 {"name": "Lifecycle"},
                 {"name": "Handling Events"},
                 {"name": "Conditional Rendering"},
-                {"name": "Loop"}
+                {"name": "Loop"},
                 //{"name": "Lists and Keys"},
-                //{"name": "Forms"},
-                //{"name": "Inheritance"},
+                {"name": "Forms"},
+                {"name": "Inheritance"},
+                {"name": "Templates"}
                 //{"name": "Sever-side Rendering"},
                 //{"name": "Thinking In Omi"}
             ]},
