f44e9580418a965239c1423d00d5b2c570eb52cf
dntzhang <kmdjs@qq.com>
2017-03-07 09:05:57 +0800

    update omi-touch

--- plugins/omi-touch/omi-touch.js
+++ plugins/omi-touch/omi-touch.js
@@ -17,8 +17,7 @@
     };
 
     var getHandler = function(name, dom, instance) {
-        var evtName ='on'+ name.substring(0, 1).toUpperCase() + name.substring(1);
-        var value = dom.getAttribute(evtName);
+        var value = dom.getAttribute(name);
         if (value === null) {
             return noop;
         }else{
