d850003083495ea1e914a3ebb82afd50f7ebb36a
dntzhang <kmdjs@qq.com>
2017-04-19 10:28:28 +0800

    prepare v1.5.0

---
+++ example/todo/list.js
@@ -0,0 +1,11 @@
+import Omi from '../../src/index.js';
+
+class List extends Omi.Component {
+    render () {
+        return `<ul>
+                    <li o-repeat="item in items">{{item}}</li>
+                </ul>`;
+    }
+}
+
+export default List;
