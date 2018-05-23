a9a00e45ee8dff745ce68e005decfae263cb98df
当耐特 <kmdjs@qq.com>
2018-05-24 06:55:42 +0800

    Update karma.conf.js

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,10 +1,11 @@
 /*eslint no-var:0, object-shorthand:0 */
 
-var coverage = String(process.env.COVERAGE) === 'true',
+//var coverage = String(process.env.COVERAGE) === 'true',
 	// ci = String(process.env.CI).match(/^(1|true)$/gi),
 	// pullRequest = !String(process.env.TRAVIS_PULL_REQUEST).match(/^(0|false|undefined)$/gi),
 	// masterBranch = String(process.env.TRAVIS_BRANCH).match(/^master$/gi),
 	// sauceLabs = ci && !pullRequest && masterBranch,
+var coverage = false,
 	sauceLabs = false,
 	performance = !coverage && String(process.env.PERFORMANCE)!=='false',
 	webpack = require('webpack');
