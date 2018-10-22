7547a7d3571b4e81c89007bc6e1d5792d55b2a15
dntzhang <dntzhang@qq.com>
2018-10-23 06:56:40 +0800

    update omi-elment-ui

--- packages/omi-element-ui/src/index.js
+++ packages/omi-element-ui/src/index.js
@@ -1,8 +1,2 @@
-import { render } from 'omi'
-import './assets/index.css'
-import './elements/app'
-import registerServiceWorker from './assets/register-service-worker'
-import store from './store/app-store'
-
-render(<my-app />, '#root', store)
-registerServiceWorker()
+//you can change this to debug your elemnts
+location.href = 'button.html'
--- packages/omi-element-ui/src/store/admin-store.js
+++
@@ -1,7 +0,0 @@
-
-export default {
-  data: { name: 'I am admin page' },
-  rename: function (name) {
-    this.data.name = name
-  }
-}
