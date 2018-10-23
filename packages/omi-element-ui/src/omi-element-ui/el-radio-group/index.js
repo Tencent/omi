3becba58b033f0b14d8d97e01ee00d2aa3db4f65
张磊 <kmdjs@qq.com>
2018-10-23 11:10:53 +0800

    add el-button-group

---
+++ packages/omi-element-ui/src/omi-element-ui/el-radio-group/index.js
@@ -0,0 +1,17 @@
+import { tag, WeElement } from 'omi'
+
+@tag('el-radio-group', true)
+class ElRadioGroup extends WeElement {
+
+    install(){
+        this.data.value = this.props.value
+    }
+
+    render(props) {
+        return (
+            <div >
+                <slot></slot>
+            </div>
+        )
+    }
+}
--- packages/omi-element-ui/src/omi-element-ui/el-radio/index.js
+++ packages/omi-element-ui/src/omi-element-ui/el-radio/index.js
@@ -8,24 +8,41 @@ class ElRadio extends WeElement {
     }
 
     install() {
-        this.checked = this.props.checked
-        this.data.value = 1
-        if (this.parentNode.nodeName === 'EL-RADIO-GROUP') {
-        }
+        this._initData()
+    }
 
+    beforeUpdate() {
+        this._initData()
+    }
+
+    _initData() {
+        this.data.checked = this.props.checked
+        this.data.disabled = this.props.disabled
+        this.data.value = this.props.value
+
+        if (this.parentNode.nodeName === 'EL-RADIO-GROUP') {
+            if (this.parentNode.data.value === this.data.value) {
+                this.data.checked = true
+            }
+            this._inInGroup = true
+        }
     }
 
     render(props) {
         let classStr = 'el-radio'
-        let checked = ''
-        if (this.checked) {
-            checked = 'is-checked'
+        let inner = ''
+        if (this.data.checked) {
+            inner += 'is-checked'
             classStr += ' is-checked'
+        }
+        if (props.disabled) {
+            inner += ' is-disabled'
+            classStr += ' is-disabled'
         }
 
         return (
             <label role="radio" aria-checked="true" tabindex="0" class={classStr} >
-                <span class={`el-radio__input ${checked}`} onClick={this.onClick}>
+                <span class={`el-radio__input ${inner}`} onClick={this.onClick}>
                     <span class="el-radio__inner"></span>
                     <input type="radio" aria-hidden="true" tabindex="-1" class="el-radio__original" value={props.value} />
                 </span>
@@ -35,7 +52,19 @@ class ElRadio extends WeElement {
     }
 
     onClick = () => {
-        this.checked = !this.checked
-        this.update()
+        if (this._inInGroup) {
+            
+            if(this.parentNode.data.value!==this.data.value){
+                this.parentNode.data.value = this.data.value
+                this.parentNode.childNodes.forEach((node) => {
+                    node.update()
+                })
+                this.parentNode.fire('change',{value:this.data.value})
+            }
+           
+        } else if (!this.data.disabled) {
+            this.data.checked = true
+            this.update()
+        }
     }
 }
