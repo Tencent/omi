397a1d32cac50a6059c916f6787459ded739bfce
张磊 <kmdjs@qq.com>
2018-10-25 10:23:01 +0800

    add package.json

---
+++ packages/omi-transform/index.js
@@ -0,0 +1,31 @@
+import { WeElement, tag } from 'omi'
+import Transform from 'css3transform'
+
+@tag('css3-transform')
+class CSS3Transform extends WeElement {
+
+	install() {
+		this.css = this.parentNode.host.css
+	}
+
+	installed() {
+		Transform(this.host);
+		this.transformByProps()
+	}
+
+	afterUpdate() {
+		this.transformByProps()
+	}
+
+	transformByProps() {
+		['translateX', 'translateY', 'translateZ', 'scaleX', 'scaleY', 'scaleZ', 'rotateX', 'rotateY', 'rotateZ', 'skewX', 'skewY', 'originX', 'originY', 'originZ', 'perspective'].forEach(item => {
+			if (this.props.hasOwnProperty(item)) {
+				this.host[item] = this.props[item]
+			}
+		})
+	}
+
+	render(props) {
+		return props.children[0]
+	}
+}
