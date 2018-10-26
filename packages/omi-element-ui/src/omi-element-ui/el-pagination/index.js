85abfeb5caffcc07df55d0fbe2d3f36078a347b9
张磊 <kmdjs@qq.com>
2018-10-26 11:30:18 +0800

    remove dead code

--- packages/omi-element-ui/src/omi-element-ui/el-pagination/index.js
+++ packages/omi-element-ui/src/omi-element-ui/el-pagination/index.js
@@ -31,28 +31,6 @@ class ElPagination extends WeElement {
         this.pageNum = Math.ceil(this.data.total / this.data.pageSize)
     }
 
-    render(props) {
-        return (
-            <div class="el-pagination is-background">
-                <button type="button" class="btn-prev" disabled="disabled">
-                    <i class="el-icon el-icon-arrow-left"></i></button>
-                <ul class="el-pager">
-                    <li class="number active">1</li>
-                    <li class="number">2</li>
-                    <li class="number">3</li>
-                    <li class="number">4</li>
-                    <li class="number">5</li>
-                    <li class="number">6</li>
-                    <li class="el-icon more btn-quicknext el-icon-more"></li>
-                    <li class="number">100</li>
-                </ul>
-                <button type="button" class="btn-next">
-                    <i class="el-icon el-icon-arrow-right"></i>
-                </button>
-            </div>
-        )
-    }
-
     beforeRender() {
         this.pageNum = Math.ceil(this.data.total / this.data.pageSize)
     }
