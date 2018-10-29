9c313c0c1261a5ae045a01325df246113e4f5966
LeeHyungGeun <lhg4031@gmail.com>
2018-10-30 02:21:00 +1100

    add isSafeToCreateProjectIn function to verify which is valid folder to create a project

--- packages/omi-cli/lib/utils.js
+++ packages/omi-cli/lib/utils.js
@@ -1,4 +1,6 @@
 var fs = require("fs");
+var chalk = require('chalk');
+var path = require('path');
 var existsSync = fs.existsSync;
 var readdirSync = fs.readdirSync;
 var rmdirSync = fs.rmdirSync;
@@ -29,7 +31,79 @@ function emptyFs(path) {
 	}
 }
 
+/**
+ * isSafeToCreateProjectIn function is created from `create-react-app`
+ * which is on create-react-app/packages/create-react-app/createReactApp.js line 740 of version 2.0.4
+ */
+function isSafeToCreateProjectIn(root, name) {
+	// These files should be allowed to remain on a failed install,
+	// but then silently removed during the next create.
+	const errorLogFilePatterns = [
+		'npm-debug.log',
+		'yarn-error.log',
+		'yarn-debug.log',
+	];
+
+  const validFiles = [
+    '.DS_Store',
+    'Thumbs.db',
+    '.git',
+    '.gitignore',
+    '.idea',
+    'README.md',
+    'LICENSE',
+    'web.iml',
+    '.hg',
+    '.hgignore',
+    '.hgcheck',
+    '.npmignore',
+    'mkdocs.yml',
+    'docs',
+    '.travis.yml',
+    '.gitlab-ci.yml',
+    '.gitattributes',
+  ];
+  console.log();
+
+  const conflicts = fs
+    .readdirSync(root)
+    .filter(file => !validFiles.includes(file))
+    // Don't treat log files from previous installation as conflicts
+    .filter(
+      file => !errorLogFilePatterns.some(pattern => file.indexOf(pattern) === 0)
+    );
+
+  if (conflicts.length > 0) {
+    console.log(
+      `The directory ${chalk.green(name)} contains files that could conflict:`
+    );
+    console.log();
+    for (const file of conflicts) {
+      console.log(`  ${file}`);
+    }
+    console.log();
+    console.log(
+      'Either try using a new directory name, or remove the files listed above.'
+    );
+
+    return false;
+  }
+
+  // Remove any remnant files from a previous installation
+  const currentFiles = fs.readdirSync(path.join(root));
+  currentFiles.forEach(file => {
+    errorLogFilePatterns.forEach(errorLogFilePattern => {
+      // This will catch `(npm-debug|yarn-error|yarn-debug).log*` files
+      if (file.indexOf(errorLogFilePattern) === 0) {
+        unlinkSync(path.join(root, file));
+      }
+    });
+  });
+  return true;
+}
+
 module.exports = {
 	isCnFuc: isCnFuc,
-	emptyFs: emptyFs
+	emptyFs: emptyFs,
+	isSafeToCreateProjectIn: isSafeToCreateProjectIn
 };
