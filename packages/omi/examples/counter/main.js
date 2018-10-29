31bf39286b8b537eb99d445f596ab0f094a861c3
张磊 <kmdjs@qq.com>
2018-10-29 09:15:22 +0800

    add counter demo

---
+++ packages/omi/examples/counter/main.js
@@ -0,0 +1,31 @@
+import { render, WeElement, tag, observe } from "../../src/omi"
+
+
+@observe
+@tag("my-counter")
+class MyApp extends WeElement {
+
+  install() {
+    this.data.count = 0
+  }
+
+  sub = () => {
+    this.data.count--
+  }
+
+  add = () => {
+    this.data.count++
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.sub}>-</button>
+        <span>{this.data.count}</span>
+        <button onClick={this.add}>+</button>
+      </div>
+    )
+  }
+}
+
+render(<my-counter />, "body")
