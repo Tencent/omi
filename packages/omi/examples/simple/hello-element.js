48ceb353de629230d741d8550046fe17d7ab5709
dntzhang <dntzhang@qq.com>
2018-10-30 09:04:35 +0800

    update simple demo

--- packages/omi/examples/simple/hello-element.js
+++ packages/omi/examples/simple/hello-element.js
@@ -1,62 +1,26 @@
-import { WeElement, tag } from '../../src/omi'
+import { define, WeElement } from '../../src/omi'
 
-@tag('hello-element')
-class HelloElement extends WeElement {
-  static get props() {
-    return {
-      propFromParent: {
-        value: '9'
-      },
-      msg: {
-        value: ''
-      },
-      num: {
-        value: 10
-      }
-    }
+define('hello-element', class extends WeElement {
+  onClick = evt => {
+    // trigger CustomEvent
+    this.fire('abc', { name: 'dntzhang', age: 12 })
+    evt.stopPropagation()
   }
 
-  static get data() {
-    return {
-      a: 1,
-      b: {
-        c: 2
-      }
-    }
+  css() {
+    return `
+        div {
+          color: red;
+          cursor: pointer;
+        }`
   }
 
-	onClick = evt => {
-	  //trigger CustomEvent
-	  this.fire('abc', { name: 'dntzhang', age: 12 })
-	  evt.stopPropagation()
-	}
-
-	installed() {
-	  setTimeout(() => {
-	    this.data.a = 2
-	    this.update()
-	  }, 1000)
-	}
-
-	css() {
-	  return `
-         div{
-             color: red;
-             cursor: pointer;
-         }`
-	}
-
-	render(props, data) {
-	  return (
-	    <div onClick={this.onClick}>
-				Hello {props.msg} {props.propFromParent}
-	      <div>
-					Click Me!
-	        {props.num}
-	      </div>
-	      <div>data: {data.a}</div>
-	      <div>props {props.num}</div>
-	    </div>
-	  )
-	}
-}
+  render(props) {
+    return (
+      <div onClick={this.onClick}>
+        Hello {props.msg} {props.propFromParent}
+        <div>Click Me!</div>
+      </div>
+    )
+  }
+})
