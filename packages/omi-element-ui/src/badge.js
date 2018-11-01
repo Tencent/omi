97103b9dc5780f4b2b36a8bb220c826a5f0d87f1
Hakkı Can İLKBAHAR <hilkbahar@blutv.com>
2018-11-01 15:55:56 +0300

    add el-badge component

---
+++ packages/omi-element-ui/src/badge.js
@@ -0,0 +1,5 @@
+import { render } from 'omi'
+import './assets/index.css'
+import './elements/badge'
+
+render(<my-app />, '#root')
---
+++ packages/omi-element-ui/src/elements/badge/index.js
@@ -0,0 +1,48 @@
+import { tag, WeElement } from 'omi'
+import style from './_index.css'
+import '../../omi-element-ui/el-badge'
+import '../../omi-element-ui/el-button'
+
+@tag('my-app')
+class MyApp extends WeElement {
+    css () {
+        return style
+    }
+    
+    render(props, data) {
+        return (
+          <div>
+            <div class="row">
+                <el-badge value="12" class="item">
+                    <el-button size="small">comments</el-button>
+                </el-badge>
+                <el-badge value="3" class="item">
+                    <el-button size="small">replies</el-button>
+                </el-badge>
+            </div> 
+            <div class="row">
+                <el-badge value={200} max={99} class="item">
+                    <el-button size="small">comments</el-button>
+                </el-badge>
+                <el-badge value={100} max={10} class="item">
+                    <el-button size="small">replies</el-button>
+                </el-badge>
+            </div>
+            <div class="row">
+                <el-badge value="new" class="item">
+                    <el-button size="small">comments</el-button>
+                </el-badge>
+                <el-badge value="hot" class="item">
+                    <el-button size="small">replies</el-button>
+                </el-badge>
+            </div>
+            <div class="row">
+                <el-badge is-dot class="item">query</el-badge>
+                <el-badge is-dot class="item">
+                    <el-button class="share-button" icon="el-icon-share" type="primary"></el-button>
+                </el-badge>
+            </div>
+          </div>
+        )
+    }
+}
