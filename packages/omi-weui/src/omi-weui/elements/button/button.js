f56fb7dfee404b790eaceef918335d0cbe192347
dntzhang <dntzhang@qq.com>
2018-11-05 12:47:08 +0800

    for diff

--- packages/omi-weui/src/omi-weui/elements/button/button.js
+++ packages/omi-weui/src/omi-weui/elements/button/button.js
@@ -1,19 +1,30 @@
-import { define, WeElement } from 'omi'
+import React from 'react'
+import PropTypes from 'prop-types'
 import classNames from '../../utils/classnames'
-import css from '../../style/widget/weui-button/_weui-button.less'
+
 /**
  *  Button usage：OK(primary)、Cancel(default)、Warn(warn).
  *
  */
-define('ow-button', class extends WeElement {
+export default class Button extends React.Component {
+  static propTypes = {
+    disabled: PropTypes.bool,
+    /**
+     * Options: primary, default, warn, vcode
+     *
+     */
+    type: PropTypes.string,
+    /**
+     * Options: normal, small
+     *
+     */
+    size: PropTypes.string
+  }
+
   static defaultProps = {
     disabled: false,
     type: 'primary',
     size: 'normal'
-  }
-
-  css() {
-    return css
   }
 
   render() {
@@ -37,14 +48,14 @@ define('ow-button', class extends WeElement {
         : classNames({
           'weui-btn': true,
           'weui-btn_mini': size === 'small',
-          'weui-btn_primary': type === 'primary' && !plain,
-          'weui-btn_default': type === 'default' && !plain,
+            'weui-btn_primary': type === 'primary' && !plain,
+            'weui-btn_default': type === 'default' && !plain,
           'weui-btn_warn': type === 'warn',
-          'weui-btn_plain-primary': type === 'primary' && plain,
-          'weui-btn_plain-default': type === 'default' && plain,
+            'weui-btn_plain-primary': type === 'primary' && plain,
+            'weui-btn_plain-default': type === 'default' && plain,
           'weui-btn_disabled': this.props.disabled && !plain,
-          'weui-btn_plain-disabled': this.props.disabled && plain,
-          [className]: className
+            'weui-btn_plain-disabled': this.props.disabled && plain,
+            [className]: className
         })
 
     return (
@@ -53,4 +64,4 @@ define('ow-button', class extends WeElement {
       </Component>
     )
   }
-})
+}
