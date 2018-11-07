920c9396c2a83504640ee7ba121e38e66102be5d
dntzhang <dntzhang@qq.com>
2018-11-07 08:55:36 +0800

    Fix npm run build(Failed to minify the code from this file)

--- packages/omi-cli/template/app/config/webpack.config.prod.js
+++ packages/omi-cli/template/app/config/webpack.config.prod.js
@@ -121,7 +121,7 @@ module.exports = {
     // for React Native Web.
     extensions: ['.web.js', '.mjs', '.js', '.json', '.web.jsx', '.jsx'],
     alias: {
-      
+
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
       'react-native': 'react-native-web',
@@ -152,7 +152,7 @@ module.exports = {
       //       options: {
       //         formatter: eslintFormatter,
       //         eslintPath: require.resolve('eslint'),
-              
+
       //       },
       //       loader: require.resolve('eslint-loader'),
       //     },
@@ -177,10 +177,10 @@ module.exports = {
           // Process JS with Babel.
           {
             test: /\.(js|jsx|mjs)$/,
-            include: paths.appSrc,
+            //include: paths.appSrc,
             loader: require.resolve('babel-loader'),
             options: {
-              
+
               compact: true,
             },
           },
