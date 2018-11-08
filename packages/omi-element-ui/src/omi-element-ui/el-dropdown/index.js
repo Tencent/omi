74f121f903e03a3d123929e8162404ad1f0ac8fe
shijiezou <shijiezou@tencent.com>
2018-11-08 17:00:29 +0800

    fix dropdown positon

--- packages/omi-element-ui/src/omi-element-ui/el-dropdown/index.js
+++ packages/omi-element-ui/src/omi-element-ui/el-dropdown/index.js
@@ -1,8 +1,8 @@
-import { define, WeElement } from "omi";
+import { define, WeElement } from 'omi'
 
 import '../style/global/index.css'
-import dropdown from "../style/_dropdown.scss"
-import popper from "../style/_popper.scss"
+import dropdown from '../style/_dropdown.scss'
+import popper from '../style/_popper.scss'
 import icon from '../../omi-element-ui/style/global/_icon.scss'
 
 import '../../omi-element-ui/el-button'
@@ -14,87 +14,117 @@ define('el-dropdown', class extends WeElement {
     left: 0,
     top: 0,
     arrow_left: 0,
-    trigger: ''
+    trigger: '',
+    timer: null
   }
 
   css() {
     return dropdown + popper + icon
   }
 
-  clickHandle = (evt) => {
-    if(this.data.showPopper) {
+  clickHandle = evt => {
+    if (this.data.showPopper) {
       this.hide()
-    }else{
+    } else {
       this.show()
     }
   }
 
-
   show = () => {
-    this.data.showPopper = true;
+    clearTimeout(this.data.timer)
+    this.data.showPopper = true
     const boxButton = this.dropElm.getBoundingClientRect()
     const boxList = this.menuElm.getBoundingClientRect()
+    // this.data.left = (boxButton.width + 60 - boxList.width) / 2;
     this.data.left = boxButton.left - (boxList.width - boxButton.width) / 2
+    // this.data.left = boxButton.left + boxButton.width / 2 - boxButton.width / 2
     this.data.top = boxButton.top + boxButton.height
-    this.data.arrow_left = boxButton.width / 2;
-    this.update();
+    // this.data.top = boxButton.height + 30;
+    this.data.arrow_left = boxButton.width / 2
+    this.update()
   }
 
   hide = () => {
-    this.data.showPopper = false;
-    this.update();
-
+    this.data.timer = setTimeout(() => {
+      this.data.showPopper = false
+      this.update()
+    }, 200)
   }
 
   render(props, data) {
-    data.trigger = props.trigger || 'hover';
-    console.log(props);
+    data.trigger = props.trigger || 'hover'
+
+    props.size = props.size || 'medium'
 
     const triggerElm = props.splitButton ? (
-              <el-button-group style="color:#fff">
-                <el-button size={props.size} type={props.type}>
-                  <slot></slot>
-                </el-button>
-                <el-button size={props.size} type={props.type}  class="el-dropdown__caret-button" style="padding: 0px;">
-                    <i class="el-dropdown__icon el-icon-arrow-down"></i>
-                </el-button>
-              </el-button-group>
-            ) : props.type ? (
-              <el-button type={props.type}>
-                <slot></slot>
-              </el-button>
-            ) : (
-              <slot></slot>
-            )
-
-    const box = props.trigger === 'click' ? (
-      <div ref={e => this.dropElm = e} class="el-dropdown" onClick={this.clickHandle}>
-        {triggerElm}
-      </div>
+      <el-button-group style="color:#fff">
+        <el-button size={props.size} type={props.type}>
+          <slot />
+        </el-button>
+        <el-button
+          size={props.size}
+          type={props.type}
+          style={{
+            'border-left': '1px solid'
+          }}
+        >
+          <i class="el-dropdown__icon el-icon-arrow-down" />
+        </el-button>
+      </el-button-group>
+    ) : props.type ? (
+      <el-button type={props.type}>
+        <slot />
+      </el-button>
     ) : (
-      <div ref={e => this.dropElm = e} class="el-dropdown" onMouseleave={this.hide} onMouseenter={this.show}>
-        {triggerElm}
-      </div>
+      <slot />
     )
 
-
-
+    const box =
+      props.trigger === 'click' ? (
+        <div
+          ref={e => (this.dropElm = e)}
+          class="el-dropdown"
+          onClick={this.clickHandle}
+        >
+          {triggerElm}
+        </div>
+      ) : (
+        <div
+          ref={e => (this.dropElm = e)}
+          class="el-dropdown"
+          onMouseleave={this.hide}
+          onMouseenter={this.show}
+        >
+          {triggerElm}
+        </div>
+      )
 
     const popper = (
-      <ul class="el-dropdown-menu el-popper" ref={e => this.menuElm = e} style={`transform-origin: center top 0px;position: absolute; left:${data.left}px; top:${data.top}px; z-index: 2032;visibility: ${data.showPopper ? 'visible' : 'hidden'};`}>
+      <ul
+        class="el-dropdown-menu el-popper"
+        onClick={this.hide}
+        onMouseenter={this.show}
+        onMouseleave={this.hide}
+        ref={e => (this.menuElm = e)}
+        style={`transform-origin: center top 0px;position: fixed; left:${
+          data.left
+        }px; top:${data.top}px; z-index: 2032;visibility: ${
+          data.showPopper ? 'visible' : 'hidden'
+        };`}
+      >
         {props.menu}
       </ul>
     )
-
-
 
     return (
       <div>
         {box}
         {popper}
       </div>
-
     )
   }
-
 })
+
+function renderMenu() {
+  render(<el-notification type={opts.position} ops={opts} />, document.body)
+}
