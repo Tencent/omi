732bc3ec02acc28320b05ce6888484d14ab79da4
dntzhang <dntzhang@qq.com>
2018-11-14 09:34:28 +0800

    omi - add template example

---
+++ packages/omi/examples/template/main.js
@@ -0,0 +1,22 @@
+import { define, render, WeElement } from '../../src/omi'
+import tpl from './my-template'
+
+define('my-app', class extends WeElement {
+
+	css() {
+		return `
+         div{
+             color: green;
+         }`
+	}
+
+	render(props, data) {
+		return (
+			<div>
+				{tpl({ name: 'zhanglei', age: 18 })}
+			</div>
+		)
+	}
+})
+
+render(<my-app name="Omi v4.0" />, 'body')
