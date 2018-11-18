e619aa4d3e71ebb6f0b316f82a81be6cff140193
LeeHyungGeun <lhg4031@gmail.com>
2018-11-18 14:49:12 +1100

    add comment about reference and nameing rules

--- packages/omi-cli/lib/utils.js
+++ packages/omi-cli/lib/utils.js
@@ -6,6 +6,7 @@ var readdirSync = fs.readdirSync;
 var rmdirSync = fs.rmdirSync;
 var unlinkSync = fs.unlinkSync;
 var statSync = fs.statSync;
+var validateProjectName = require('validate-npm-package-name');
 
 function isCnFuc(language) {
 	return language === "cn" ? true : false;
@@ -102,8 +103,34 @@ function isSafeToCreateProjectIn(root, name) {
   return true;
 }
 
+// validate an app(project) name
+// - Nameing Rules: https://www.npmjs.com/package/validate-npm-package-name#naming-rules
+// - Referenced: https://github.com/facebook/create-react-app/blob/master/packages/create-react-app/createReactApp.js#L664
+function checkAppName(appName) {
+  const validationResult = validateProjectName(appName);
+  if (!validationResult.validForNewPackages) {
+    console.error(
+      `Could not create a project called ${chalk.red(
+        `"${appName}"`
+      )} because of npm naming restrictions: `
+    );
+    printValidationResults(validationResult.errors);
+    printValidationResults(validationResult.warnings);
+    process.exit(1);
+  }
+
+  function printValidationResults(results) {
+    if (typeof results !== 'undefined') {
+      results.forEach(error => {
+        console.error(chalk.red(`  *  ${error}`));
+      });
+    }
+  }
+}
+
 module.exports = {
 	isCnFuc: isCnFuc,
 	emptyFs: emptyFs,
-	isSafeToCreateProjectIn: isSafeToCreateProjectIn
+  isSafeToCreateProjectIn: isSafeToCreateProjectIn,
+  checkAppName: checkAppName
 };
