07e9d2cb0bf8abac2229c135511b90300bc10301
张磊 <kmdjs@qq.com>
2018-12-03 16:43:27 +0800

    omi-cli - update weui template

--- packages/omi-cli/template/weui/src/admin.js
+++
@@ -1,8 +0,0 @@
-import { render } from 'omi'
-import './assets/index.css'
-import './elements/hello'
-import registerServiceWorker from './assets/register-service-worker'
-import store from './store/admin-store'
-
-render(<hello-element />, '#root', store)
-registerServiceWorker()
