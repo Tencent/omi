df42e8f2f644b239cd72753da81d5eeba6f1f3f0
张磊 <kmdjs@qq.com>
2018-12-06 15:43:07 +0800

    add title

--- packages/omi-cli/template/weui/src/elements/button-panel/index.js
+++ packages/omi-cli/template/weui/src/elements/button-panel/index.js
@@ -10,19 +10,25 @@ define('button-panel', class extends WeElement {
   render(props, data) {
     return (
       <div>
-        <ow-button type="primary">primary</ow-button>
-        <ow-button type="primary" disabled>
-          primary disabled
+        <div class="page__hd">
+          <h1 class="page__title">Button</h1>
+          <p class="page__desc">按钮</p>
+        </div>
+        <div>
+          <ow-button type="primary">primary</ow-button>
+          <ow-button type="primary" disabled>
+            primary disabled
         </ow-button>
-        <ow-button type="warn">warn</ow-button>
-        <ow-button type="warn" disabled>
-          wran disabled
+          <ow-button type="warn">warn</ow-button>
+          <ow-button type="warn" disabled>
+            wran disabled
         </ow-button>
-        <ow-button type="secondary">secondary</ow-button>
-        <ow-button type="secondary" disabled>
-          secondary disabled
+          <ow-button type="secondary">secondary</ow-button>
+          <ow-button type="secondary" disabled>
+            secondary disabled
         </ow-button>
-        <ow-button size="small">small</ow-button>
+          <ow-button size="small">small</ow-button>
+        </div>
       </div>
     )
   }
