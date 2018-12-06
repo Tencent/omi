d3bc5591296eaad2b2e1497b7f9d6762093448d9
张磊 <kmdjs@qq.com>
2018-12-06 18:37:51 +0800

    update list of omi-weui

--- packages/omi-cli/template/weui/src/elements/list-panel/index.js
+++ packages/omi-cli/template/weui/src/elements/list-panel/index.js
@@ -10,7 +10,12 @@ define('list-panel', class extends WeElement {
   render() {
     return (
       <div>
-        <ow-list title="如果爱情有天意……">
+         <div class="page__hd">
+          <h1 class="page__title">List</h1>
+          <p class="page__desc">列表</p>
+        </div>
+
+        <ow-list title="列表标题">
           <ow-list-item
             icon="https://ws1.sinaimg.cn/large/0065Zy9egy1fxtdkt9566j305k05kdfs.jpg"
             title="Title"
