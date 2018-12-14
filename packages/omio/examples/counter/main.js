39f201c6f54be8560b2eb19dfcc7fd7bcefce633
dntzhang <dntzhang@qq.com>
2018-12-14 08:18:46 +0800

    omio - add counter example

---
+++ packages/omio/examples/counter/main.js
@@ -0,0 +1,30 @@
+import { render, WeElement, define, observe } from '../../src/omi'
+
+define('my-counter', 
+class extends WeElement {
+  static observe = true
+
+  data = {
+    count: 1
+  }
+
+  sub = () => {
+    this.data.count--
+  }
+
+  add = () => {
+    this.data.count++
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.sub}>-</button>
+        <span>{this.data.count}</span>
+        <button onClick={this.add}>+</button>
+      </div>
+    )
+  }
+})
+
+render(<my-counter />, 'body')
