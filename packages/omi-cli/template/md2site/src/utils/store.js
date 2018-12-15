51c5d3ad10b46bd96406551a7d19a20ac2be5e3b
dntzhang <dntzhang@qq.com>
2018-12-15 22:24:34 +0800

    omi-cli - update template of md2site

--- packages/omi-cli/template/md2site/src/utils/store.js
+++ packages/omi-cli/template/md2site/src/utils/store.js
@@ -3,18 +3,20 @@ import config from '../docs/config.js'
 class AppStore {
   constructor() {
     this.lan = config.lan
-    this.md = config.menus[this.lan][0].list[0].md
+    this.menus = config.menus[this.lan]
+    this.md = this.menus[0].list[0].md
     this.remarkable = new Remarkable({ html: true })
     this.html = this.remarkable.render(this.getMarkDown(this.md, this.lan))
-
-    //this.updatePager()
+    this.menus[0].active = true
+    this.menus[0].currentIndex = 0
+    this.updatePager()
   }
 
   goto(md, index) {
-    let items = this.data.menus
+    let items = this.menus
     items.every(item => {
       if (item.active) {
-        this.data.md = md
+        this.md = md
         item.currentIndex = index
 
         item.list.forEach((listItem, currentIndex) => {
@@ -45,7 +47,7 @@ class AppStore {
     let xobj = new XMLHttpRequest()
     //xobj.overrideMimeType("application/json");
     xobj.open('GET', url, true) // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function() {
+    xobj.onreadystatechange = function () {
       if (xobj.readyState == 4 && xobj.status == '200') {
         // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
         callback(xobj.responseText)
@@ -66,28 +68,30 @@ class AppStore {
   }
 
   updatePager() {
-    this.data.preMd = null
-    this.data.preName = null
-    this.data.nextMd = null
-    this.data.nextName = null
+    this.preMd = null
+    this.preName = null
+    this.nextMd = null
+    this.nextName = null
     let menu = this.getCurrentMenu()
 
     let pre = menu.list[menu.currentIndex - 1]
     if (pre) {
-      this.data.preMd = pre.md
-      this.data.preName = pre.name
+      this.preMd = pre.md
+      this.preName = pre.name
     }
     let next = menu.list[menu.currentIndex + 1]
     if (next) {
-      this.data.nextMd = next.md
-      this.data.nextName = next.name
+      this.nextMd = next.md
+      this.nextName = next.name
     }
+    console.log(this.preName, this.nextName)
   }
 
   getCurrentMenu() {
     let menu = null
 
-    let items = this.data.menus
+    let items = this.menus
+    console.log(items)
     items.every(item => {
       if (item.active) {
         menu = item
