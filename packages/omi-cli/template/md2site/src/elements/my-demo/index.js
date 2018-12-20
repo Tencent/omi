61e135b0f4f57b0621fdbea603f5947409d95f44
张磊 <kmdjs@qq.com>
2018-12-20 18:37:37 +0800

    md2site - update style

--- packages/omi-cli/template/md2site/src/elements/my-demo/index.js
+++ packages/omi-cli/template/md2site/src/elements/my-demo/index.js
@@ -5,16 +5,41 @@ import css from './_index.css'
 define('my-demo', class extends WeElement {
   install() {
     this.store.mySidebar = this
+    this.show = false
   }
 
   css() {
     return css
   }
 
+  onShow = () => {
+
+    this.show = true
+    this.update()
+  }
+
+  onClose = () => {
+
+    this.show = false
+    this.update()
+  }
+
+  
   render() {
     return (
       <div>
-        <iframe src='https://tencent.github.io/omi/packages/omi-chart/repl/index.html' ></iframe>
+        {!this.show && <div class="switch code" onClick={this.onShow}>
+          <img src={require('./code.png')}></img>
+        </div>
+        }
+
+        {this.show && <div class="switch close" onClick={this.onClose}>
+          <img src={require('./close.png')}></img>
+        </div>}
+
+        {this.show && <iframe src='http://localhost:8080/packages/omi-chart/repl/index.html' ></iframe>
+        }
+
       </div>
     )
   }
