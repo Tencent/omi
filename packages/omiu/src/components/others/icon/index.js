e8a5bd4357d328fcbc17801164f37bb7dba6d514
张磊 <kmdjs@qq.com>
2018-12-26 11:39:32 +0800

    omiu - fix icon class

--- packages/omiu/src/components/others/icon/index.js
+++ packages/omiu/src/components/others/icon/index.js
@@ -13,7 +13,7 @@ define('o-icon', class extends WeElement {
       props.class = 'rotate '
     }
     return (
-      <i class={`o-icon ${this.props.class}`}>
+      <i class={`o-icon ${this.props.class||''}`}>
         <svg viewBox="0 0 1024 1024" {...props} data-icon="loading" width="1em" height="1em" fill="currentColor" aria-hidden="true">
           <path d={path[this.props.type]}></path>
         </svg>
