a8b658120787f85c1351c32461f6bd3a9f6db3ec
张磊 <kmdjs@qq.com>
2018-12-28 14:44:03 +0800

    omiu - build

--- packages/omiu/src/toptip/index.js
+++ packages/omiu/src/toptip/index.js
@@ -1,14 +1,16 @@
 import { define, WeElement } from 'omi'
-import style from './_index.scss'
+import style from './_index.css'
 
 define('o-toptip', class extends WeElement {
   css() {
     return style
   }
-  hide(){
+
+  hide() {
     clearTimeout(this.timer)
     this.props.hide && this.props.hide()
   }
+
   render(props) {
     let top = props.show ? '0' : '-42px'
     this.timer = setTimeout(() => {
@@ -18,7 +20,7 @@ define('o-toptip', class extends WeElement {
       ? `weui-toptip-${props.type}`
       : 'weui-toptip-primary'
     return (
-      <div className={`ow-toptip ${type}`} style={{top}}>
+      <div className={`ow-toptip ${type}`} style={{ top }}>
         {props.msg}
       </div>
     )
