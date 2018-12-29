983c40b04414b8716118c308a1e1f9d9cfdb065a
张磊 <kmdjs@qq.com>
2018-12-29 12:30:09 +0800

    omiu - update icon, add default value to scale

--- packages/omiu/src/icon/index.js
+++ packages/omiu/src/icon/index.js
@@ -7,20 +7,22 @@ define('o-icon', class extends WeElement {
     return css
   }
 
+  static defaultProps = {
+    scale: 2
+  }
+
   onClick = (e) => {
     this.props.onClick && this.props.onClick(e)
     //e.stopPropagation()
   }
 
-  render() {
-    const props = {}
-    props.scale = this.props.scale || 2
+  render(props) {
     return (
-      <i {...extractClass(this.props, 'o-icon')} onClick={this.onClick}>
-        <svg viewBox="0 0 1024 1024" class={classNames({ 'rotate': this.props.rotate })} width={props.scale + "em"} height={props.scale + "em"} fill={this.props.color} aria-hidden="true">
-          <path d={props.path ? props.path : (path[this.props.type][this.props.isFill ? 1 : 0] || path[this.props.type][0])}></path>
+      <i {...extractClass(props, 'o-icon')} onClick={this.onClick}>
+        <svg viewBox="0 0 1024 1024" class={classNames({ 'rotate': props.rotate })} width={props.scale + "em"} height={props.scale + "em"} fill={props.color} aria-hidden="true">
+          <path d={props.path ? props.path : (path[props.type][props.isFill ? 1 : 0] || path[props.type][0])}></path>
         </svg>
-        {this.props.children && <div style={`color:${this.props.color || 'black'};`}>{this.props.children[0]}</div>}
+        {props.children && <div style={`color:${props.color || 'black'};`}>{props.children[0]}</div>}
       </i>
     )
 
