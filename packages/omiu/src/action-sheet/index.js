f348abe6f20cc53642662cdfa512b09ec5172cfe
dntzhang <dntzhang@qq.com>
2018-12-29 15:41:57 +0800

    omiu - add action sheet

---
+++ packages/omiu/src/action-sheet/index.js
@@ -0,0 +1,74 @@
+import { define, WeElement, classNames } from 'omi'
+import css from './_index.css';
+
+define('o-action-sheet', class extends WeElement {
+
+  static defaultProps = {
+    type: '',
+    menus: [],
+    actions: [],
+    show: false,
+  }
+
+  css() {
+    return css
+  }
+
+  renderMenuItem() {
+    return this.props.menus.map((menu, idx) => {
+      const { label, className, ...others } = menu;
+      const cls = classNames({
+        'weui-actionsheet__cell': true,
+        [className]: className
+      });
+
+      return (
+        <div key={idx} {...others} className={cls}>{label}</div>
+      );
+    });
+  }
+
+  renderActions() {
+    return this.props.actions.map((action, idx) => {
+      const { label, className, ...others } = action;
+      const cls = classNames({
+        'weui-actionsheet__cell': true,
+        [className]: className
+      });
+
+      return (
+        <div key={idx} {...others} className={cls}>{label}</div>
+      );
+    });
+  }
+
+  handleMaskClick = (e) => {
+    if (this.props.onRequestClose) this.props.onRequestClose(e);
+  }
+
+  render() {
+    const { show, type, onRequestClose, menus, actions, ...others } = this.props;
+    const cls = classNames({
+      'weui-actionsheet': true,
+      'weui-actionsheet_toggle': show
+    });
+
+    let styleType = type ? type : 'ios';
+
+    return (
+      <div
+        className={styleType === 'android' ? 'weui-skin_android' : ''}
+      >
+        <div class='mask' style={{ display: show ? 'block' : 'none' }} onClick={this.handleMaskClick} />
+        <div className={cls} {...others} >
+          <div className="weui-actionsheet__menu">
+            {this.renderMenuItem()}
+          </div>
+          <div className="weui-actionsheet__action">
+            {this.renderActions()}
+          </div>
+        </div>
+      </div>
+    );
+  }
+})
