dd5142b7c6a1cd6a5238f160636ebfdfd19951b0
dntzhang <dntzhang@qq.com>
2018-12-29 17:12:12 +0800

    omiu - add toast

---
+++ packages/omiu/examples/src/toast.js
@@ -0,0 +1,32 @@
+import { WeElement, define, render } from 'omi'
+import '../../src/toast'
+import '../../src/button'
+
+define('my-app', class extends WeElement {
+
+  successShow = false
+  loadingShow = false
+
+  showSuccess =() => {
+    this.successShow = true
+    this.update()
+  }
+
+  showLoading =() => {
+    this.loadingShow = true
+    this.update()
+  }
+
+  render() {
+    return (
+      <div>
+        <o-button onClick={this.showSuccess}>Show Success Toast</o-button>
+        <o-button onClick={this.showLoading}>Show Loading Toast</o-button>
+        <o-toast show={this.successShow}>支付成功</o-toast>
+        <o-toast type='loading' show={this.loadingShow}>加载中</o-toast>
+      </div>
+    );
+  }
+})
+
+render(<my-app />, '#root')
---
+++ packages/omiu/src/toast/index.js
@@ -0,0 +1,33 @@
+import { define, WeElement, classNames } from 'omi'
+import css from './_index.css'
+import '../icon';
+
+define('o-toast', class extends WeElement {
+  static defaultProps = {
+    type: 'success',
+    show: false,
+  }
+
+  css() {
+    return css
+  }
+
+  render() {
+    const { className, type, show, children, iconSize, ...others } = this.props
+    const cls = classNames('weui-toast', {
+      [className]: className
+    })
+    
+    return (
+      <div style={{ display: show ? 'block' : 'none' }}>
+        <div className={cls} {...others}>
+          {type === 'success'?
+          <o-icon type={'check'}  color='white' class='icon' scale={4} className="weui-icon_toast" />:
+          <o-icon type={'loading'} rotate color='white' class='icon-l' scale={3.5} className="weui-icon_toast" />}
+          
+          <p className="weui-toast_content">{children}</p>
+        </div>
+      </div>
+    )
+  }
+})
