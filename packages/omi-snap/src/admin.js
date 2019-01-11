afee6797f106ec53ec0ae02ee383777a368a2520
dntzhang <dntzhang@qq.com>
2019-01-11 10:59:50 +0800

    omi-snap - using omio latest

--- packages/omi-snap/src/admin.js
+++ packages/omi-snap/src/admin.js
@@ -3,4 +3,4 @@ import './assets/index.css'
 import './elements/hello'
 import store from './store/admin-store'
 
-render(<hello-element />, '#root', store)
+render(<hello-element />, '#root', store, true)
--- packages/omi-snap/src/index.js
+++ packages/omi-snap/src/index.js
@@ -2,6 +2,4 @@ import { render } from 'omi'
 import './assets/index.css'
 import './elements/app'
 
-render(<my-app />, '#root', {
-  merge: document.querySelector('#root').firstChild
-})
+render(<my-app />, '#root', null, true)
