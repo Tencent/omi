7540b5124da152e75a6f288241bd3330f5b15a8a
张磊 <kmdjs@qq.com>
2019-01-25 10:53:54 +0800

    omiu - update style

--- packages/omiu/src/step/index.js
+++ packages/omiu/src/step/index.js
@@ -7,20 +7,24 @@ define('o-step', class extends WeElement {
     return (
 
       <div {...extractClass(props, 'o-step _horizontal')}>
-        <div class="_item _item-finish"><div class="_item-tail">
-        </div>
-          <div class="_item-icon"><span class="icon">
-            <i class="anticon anticon-check finish-icon">
-              <svg viewBox="64 64 896 896" class="" data-icon="check" width="1em" height="1em" fill="#07C160" aria-hidden="true">
-                <path d="M912 190h-69.9c-9.8 0-19.1 4.5-25.1 12.2L404.7 724.5 207 474a32 32 0 0 0-25.1-12.2H112c-6.7 0-10.4 7.7-6.3 12.9l273.9 347c12.8 16.2 37.4 16.2 50.3 0l488.4-618.9c4.1-5.1.4-12.8-6.3-12.8z"></path>
-              </svg>
-            </i>
-          </span></div><div class="_item-content">
+        <div class="_item _item-finish">
+          <div class="_item-tail">
+          </div>
+          <div class="_item-icon">
+            <span class="icon">
+              <i class="anticon anticon-check finish-icon">
+                <svg viewBox="64 64 896 896" class="" data-icon="check" width="1em" height="1em" fill="#07C160" aria-hidden="true">
+                  <path d="M912 190h-69.9c-9.8 0-19.1 4.5-25.1 12.2L404.7 724.5 207 474a32 32 0 0 0-25.1-12.2H112c-6.7 0-10.4 7.7-6.3 12.9l273.9 347c12.8 16.2 37.4 16.2 50.3 0l488.4-618.9c4.1-5.1.4-12.8-6.3-12.8z"></path>
+                </svg>
+              </i>
+            </span>
+          </div>
+          <div class="_item-content">
             <div class="_item-title">Finished</div>
             <div class="_item-description">This is a description.</div>
           </div>
         </div>
-        
+
         <div class="_item _item-process">
           <div class="_item-tail"></div>
           <div class="_item-icon">
