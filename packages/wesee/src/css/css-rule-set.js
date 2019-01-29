a8f21368d327c32d903916472b469f64cffb5787
dntzhang <dntzhang@qq.com>
2019-01-29 08:22:52 +0800

    wesee - update CssRuleSet

--- packages/wesee/src/css/css-rule-set.js
+++ packages/wesee/src/css/css-rule-set.js
@@ -1,105 +1,108 @@
+import CssRule from './css-rule'
+
 export default class CssRuleSet {
   constructor(selector) {
     //string
-    this.selector = selector
-    //Array<CSSRule>;
-		this.rules = new Array()
+    this.selector = selector || ''
+    //Array<CssRule>;
+    this.rules = new Array()
   }
-}
 
-package tannus.css;
+  addSelector(sel) {
+    this.selector += sel
+    return this
+  }
 
-import tannus.css.CSSRule;
-import tannus.core.Object;
 
-class CSSRuleSet {
+  hasRule(name) {
+    for (let i = 0, len = this.rules.length; i < len; i++) {
+      if (this.rules[i].name === name) {
+        return true
+      }
+    }
+    return false
+  }
 
-	public function addSelector(sel:String):CSSRuleSet {
-		this.selector += sel;
-		return this;
-	}
-	
-	public function hasRule(name:String):Bool {
-		for (rule in rules) {
-			if (rule.name == name) return true;
-		}
-		return false;
-	}
+  getCSSRule(name) {
+    for (let i = 0, len = this.rules.length; i < len; i++) {
+      if (this.rules[i].name === name) {
+        return this.rules[i]
+      }
+    }
+    return null
+  }
 
-	private function getCSSRule(name:String):Null<CSSRule> {
-		for (rule in rules) {
-			if (rule.name == name) return rule;
-		}
-		return null;
-	}
-	public function getRule(name:String):Null<String> {
-		var rule:Null<CSSRule> = getCSSRule(name);
-		if (rule != null) {
-			return rule.value;
-		} else {
-			return null;
-		}
-	}
+  getRule(name) {
+    const rule = this.getCSSRule(name)
+    if (rule !== null) {
+      return rule.value
+    } else {
+      return null
+    }
+  }
 
-	public function setRule(name:String, value:String):Void {
-		var rule:Null<CSSRule> = getCSSRule(name);
-		if (rule != null) {
-			rule.value = value;
-		} else {
-			rules.push(new CSSRule(name, value));
-		}
-	}
+  setRule(name, value) {
+    const rule = this.getCSSRule(name);
+    if (rule !== null) {
+      rule.value = value
+    } else {
+      this.rules.push(new CssRule(name, value))
+    }
+  }
 
-	public function toString():String {
-		var code:String = '';
-		for (rule in rules) {
-			code += '${rule.name}: ${rule.value};';
-		}
-		if (selector != null) {
-			code = ('${selector} {' + code + '}');
-		}
-		return code;
-	}
-	public function toObject():Object {
-		var obj:Object = new Object({});
+  toString() {
+    const code = '';
+    for (let i = 0, len = this.rules.length; i < len; i++) {
+      const rule = this.rules[i]
+      code += `${rule.name}: ${rule.value};`
+    }
+    if (this.selector !== null) {
+      code = (`${this.selector} {` + code + '}')
+    }
+    return code
+  }
 
-		for (rule in rules) {
-			obj[rule.name] = rule.value;
-		}
+  toObject() {
+    const obj = {}
 
-		return obj;
-	}
+    for (let i = 0, len = this.rules.length; i < len; i++) {
+      const rule = this.rules[i]
+      obj[rule.name] = rule.value;
+    }
 
-	public static function fromObject(obj:Object):CSSRuleSet {
-		var set:CSSRuleSet = new CSSRuleSet();
-		for (key in obj.keys()) {
-			set.setRule(key, obj[key]);
-		}
-		return set;
-	}
+    return obj
+  }
 
-	public static function parse(txt:String):CSSRuleSet {
-		var set:CSSRuleSet = new CSSRuleSet();
-		var assigns:Array<String> = txt.split(';');
+  static fromObject = (obj) => {
+    const set = new CSSRuleSet()
+    for (let key in obj) {
+      set.setRule(key, obj[key])
+    }
+    return set
+  }
 
-		for (ass in assigns) {
-			var name:String = ass.substring(0, ass.indexOf(':'));
-			var value:String = ass.substring(ass.indexOf(':')+1);
-			set.setRule(name, value);
-		}
-		return set;
-	}
+  static parse = (txt) => {
+    const set = new CSSRuleSet()
+    const assigns = txt.split(';')
 
-	#if client
-		public static function fromElement(el:js.html.Element):CSSRuleSet {
-			var computed = js.Browser.window.getComputedStyle(el);
-			var rules = new CSSRuleSet();
+    assigns.forEach(ass => {
+      const name = ass.substring(0, ass.indexOf(':'))
+      const value = ass.substring(ass.indexOf(':') + 1)
+      set.setRule(name, value)
+    })
+    return set
+  }
 
-			for (key in computed) {
-				rules.setRule(key, computed.getPropertyValue(key));
-			}
+  //client
+  static fromElement = (el) => {
+    const computed = window.getComputedStyle(el)
+    const rules = new CSSRuleSet()
 
-			return rules;
-		}
-	#end
+    for (let key in computed) {
+      rules.setRule(key, computed.getPropertyValue(key));
+    }
+
+    return rules
+  }
+
 }
