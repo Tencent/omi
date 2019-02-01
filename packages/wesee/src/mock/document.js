c4da08fcd9ac9f4d106e1d3de881a58dbb9187a6
dntzhang <dntzhang@qq.com>
2019-02-01 19:04:36 +0800

    wesee - add mocks

---
+++ packages/wesee/src/mock/document.js
@@ -0,0 +1,52 @@
+import Element from './element'
+import { addDoc, removeDoc } from './util'
+
+
+export default class Document {
+  constructor(id) {
+    this.id = id
+    addDoc(id, this)
+    this.nodeMap = {}
+    this._isMockDocument = true
+  }
+
+  // createBody(type, props) {
+  //   if (!this.body) {
+  //     const el = new Element(type, props)
+  //     el.didMount = true
+  //     el.ownerDocument = this
+  //     el.docId = this.id
+  //     el.style.alignItems = 'flex-start'
+  //     this.body = el
+  //   }
+
+  //   return this.body
+  // }
+
+  createElement(tagName, props) {
+    let el = new Element(tagName, props)
+    el.ownerDocument = this
+    el.docId = this.id
+    return el
+  }
+
+  destroy() {
+    delete this.listener
+    delete this.nodeMap
+    removeDoc(this.id)
+  }
+
+  addEventListener(ref, type) {
+    //document.addEvent(this.id, ref, type)
+  }
+
+  removeEventListener(ref, type) {
+    //document.removeEvent(this.id, ref, type)
+  }
+
+
+  scrollTo(ref, x, y, animated) {
+    document.scrollTo(this.id, ref, x, y, animated)
+  }
+
+}
---
+++ packages/wesee/src/mock/index.js
@@ -0,0 +1,6 @@
+import Document from './document'
+
+
+export default {
+	document: new Document(0)
+}
