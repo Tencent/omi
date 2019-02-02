ec847e52b2a6da74d379bc98f95ee8223243f252
dntzhang <dntzhang@qq.com>
2019-02-02 20:15:09 +0800

    wesee  - update mock

--- packages/wesee/src/dom/README.md
+++
@@ -1,7 +0,0 @@
-## document.js
-
-Implement this web document for client rendering, send instructions to the client through it.
-
-## element.js
-
-Implement this web element for client rendering. It has the same API as dom elment.
--- packages/wesee/src/mock/element.js
+++ packages/wesee/src/mock/element.js
@@ -1,289 +1,268 @@
 import {
-  getDoc,
-  uniqueId,
-  linkParent,
-  insertIndex,
-  moveIndex,
-  removeIndex
+	getDoc,
+	uniqueId,
+	linkParent,
+	insertIndex,
+	moveIndex,
+	removeIndex
 } from './util'
 
-import { getSendBridgeFlag } from './util'
-
-const sendBridgeFlag = getSendBridgeFlag()
 
 function registerNode(docId, node) {
-  const doc = getDoc(docId)
-  doc.nodeMap[node.nodeId] = node
+	const doc = getDoc(docId)
+	doc.nodeMap[node.nodeId] = node
 }
 
 export default class Element {
-  constructor(type) {
-    this.nodeType = 1
-    this.nodeId = uniqueId()
-    this.ref = this.nodeId
-    this.type = type
-    this.attributes = {}
-    this.style = {}
-    this.classStyle = {}
-    this.event = {}
-    this.childNodes = []
+	constructor(type) {
+		this.nodeType = 1
+		this.nodeId = uniqueId()
+		this.ref = this.nodeId
+		this.type = type
+		this.attributes = {}
+		this.style = {}
+		this.classStyle = {}
+		this.event = {}
+		this.childNodes = []
 
-    this.nodeName = this.type
-    this.didMount = false
-    this.parentNode = null
-    this.nextSibling = null
-    this.previousSibling = null
-    this.firstChild = null
-  }
+		this.nodeName = this.type
 
-  appendChild(node) {
-    if (!node.parentNode) {
-      linkParent(node, this)
-      insertIndex(node, this.childNodes, this.childNodes.length, true)
+		this.parentNode = null
+		this.nextSibling = null
+		this.previousSibling = null
+		this.firstChild = null
+	}
 
-      if (this.docId != undefined) {
-        registerNode(this.docId, node)
-      }
+	appendChild(node) {
+		if (!node.parentNode) {
+			linkParent(node, this)
+			insertIndex(node, this.childNodes, this.childNodes.length, true)
 
-      if (this.didMount && sendBridgeFlag[this.docId]) {
-        this.ownerDocument.addElement(this.ref, node.toJSON(), -1)
-      }
-    } else {
-      node.parentNode.removeChild(node)
+			if (this.docId != undefined) {
+				registerNode(this.docId, node)
+			}
 
-      this.appendChild(node)
 
-      return
-    }
+			this.ownerDocument.addElement(this.ref, node.toJSON(), -1)
 
-    this.firstChild = this.childNodes[0]
+		} else {
+			node.parentNode.removeChild(node)
 
-    if (this.didMount) {
-      this._setDidMount(node, true)
-    }
-  }
+			this.appendChild(node)
 
-  _setDidMount(node, mount) {
-    node.didMount = mount
-    node.childNodes.forEach(child => {
-      this._setDidMount(child, mount)
-    })
-  }
+			return
+		}
 
-  insertBefore(node, before) {
-    if (!node.parentNode) {
-      linkParent(node, this)
-      const index = insertIndex(
-        node,
-        this.childNodes,
-        this.childNodes.indexOf(before),
-        true
-      )
-      if (this.docId != undefined) {
-        registerNode(this.docId, node)
-      }
+		this.firstChild = this.childNodes[0]
 
-      if (this.didMount && sendBridgeFlag[this.docId]) {
-        this.ownerDocument.addElement(this.ref, node.toJSON(), index)
-      }
-    } else {
-      node.parentNode.removeChild(node)
-      this.insertBefore(node, before)
-      return
-    }
-    if (this.didMount) {
-      this._setDidMount(node, true)
-    }
-    this.firstChild = this.childNodes[0]
-  }
 
-  insertAfter(node, after) {
-    if (node.parentNode && node.parentNode !== this) {
-      return
-    }
-    if (
-      node === after ||
-      (node.previousSibling && node.previousSibling === after)
-    ) {
-      return
-    }
-    if (!node.parentNode) {
-      linkParent(node, this)
-      const index = insertIndex(
-        node,
-        this.childNodes,
-        this.childNodes.indexOf(after) + 1,
-        true
-      )
+	}
 
-      if (this.docId != undefined) {
-        registerNode(this.docId, node)
-      }
+	insertBefore(node, before) {
+		if (!node.parentNode) {
+			linkParent(node, this)
+			const index = insertIndex(
+				node,
+				this.childNodes,
+				this.childNodes.indexOf(before),
+				true
+			)
+			if (this.docId != undefined) {
+				registerNode(this.docId, node)
+			}
 
-      if (this.didMount && sendBridgeFlag[this.docId]) {
-        this.ownerDocument.addElement(this.ref, node.toJSON(), index)
-      }
-    } else {
-      const index = moveIndex(
-        node,
-        this.childNodes,
-        this.childNodes.indexOf(after) + 1
-      )
 
-      if (this.didMount) {
-        this.ownerDocument.moveElement(node.ref, this.ref, index)
-      }
-    }
-    if (this.didMount) {
-      this._setDidMount(node, true)
-    }
-    this.firstChild = this.childNodes[0]
-  }
+			this.ownerDocument.addElement(this.ref, node.toJSON(), index)
 
-  removeChild(node) {
-    if (node.parentNode) {
-      removeIndex(node, this.childNodes, true)
+		} else {
+			node.parentNode.removeChild(node)
+			this.insertBefore(node, before)
+			return
+		}
 
-      if (this.didMount && sendBridgeFlag[this.docId]) {
-        this.ownerDocument.removeElement(node.ref)
-      }
-    }
+		this.firstChild = this.childNodes[0]
+	}
 
-    node.parentNode = null
+	insertAfter(node, after) {
+		if (node.parentNode && node.parentNode !== this) {
+			return
+		}
+		if (
+			node === after ||
+			(node.previousSibling && node.previousSibling === after)
+		) {
+			return
+		}
+		if (!node.parentNode) {
+			linkParent(node, this)
+			const index = insertIndex(
+				node,
+				this.childNodes,
+				this.childNodes.indexOf(after) + 1,
+				true
+			)
 
-    this._setDidMount(node, false)
+			if (this.docId != undefined) {
+				registerNode(this.docId, node)
+			}
 
-    this.firstChild = this.childNodes[0]
-  }
+			this.ownerDocument.addElement(this.ref, node.toJSON(), index)
 
-  setAttribute(key, value, silent) {
-    if (this.attributes[key] === value && silent !== false) {
-      return
-    }
-    this.attributes[key] = value
-    if (!silent) {
-      const result = {}
-      result[key] = value
-      if (this.didMount && sendBridgeFlag[this.docId]) {
-        this.ownerDocument.setAttr(this.ref, result)
-      }
-    }
-  }
+		} else {
+			const index = moveIndex(
+				node,
+				this.childNodes,
+				this.childNodes.indexOf(after) + 1
+			)
 
-  removeAttribute(key) {
-    if (this.attributes[key]) {
-      delete this.attributes[key]
-    }
-  }
+			this.ownerDocument.moveElement(node.ref, this.ref, index)
 
-  setStyle(key, value, silent) {
-    if (this.style[key] === value && silent !== false) {
-      return
-    }
-    this.style[key] = value
-    if (!silent && this.ownerDocument) {
-      const result = {}
-      result[key] = value
-      if (this.didMount && sendBridgeFlag[this.docId]) {
-        this.ownerDocument.setStyles(this.ref, result)
-      }
-    }
-  }
+		}
 
-  setStyles(styles) {
-    Object.assign(this.style, styles)
-    if (this.ownerDocument) {
-      if (this.didMount && sendBridgeFlag[this.docId]) {
-        this.ownerDocument.setStyles(this.ref, styles)
-      }
-    }
-  }
+		this.firstChild = this.childNodes[0]
+	}
 
-  setClassStyle(classStyle) {
-    for (const key in this.classStyle) {
-      this.classStyle[key] = ''
-    }
+	removeChild(node) {
+		if (node.parentNode) {
+			removeIndex(node, this.childNodes, true)
 
-    Object.assign(this.classStyle, classStyle)
 
-    if (this.didMount && sendBridgeFlag[this.docId]) {
-      this.ownerDocument.setStyles(this.ref, this.toStyle())
-    }
-  }
+			this.ownerDocument.removeElement(node.ref)
 
-  addEventListener(type, handler) {
-    if (!this.event[type]) {
-      this.event[type] = handler
+		}
 
-      sendBridgeFlag[this.docId] &&
-        this.didMount &&
-        this.ownerDocument.addEvent(this.ref, type)
-    }
-  }
+		node.parentNode = null
 
-  removeEventListener(type) {
-    if (this.event[type]) {
-      delete this.event[type]
-      let doc = getDoc(this.docId)
-      doc.nodeMap[this.ref] &&
-      doc.nodeMap[this.ref].event &&
-      doc.nodeMap[this.ref].event[type]
-        ? (doc.nodeMap[this.ref].event[type] = null)
-        : ''
-      sendBridgeFlag[this.docId] &&
-        this.didMount &&
-        this.ownerDocument.removeEvent(this.ref, type)
-    }
-  }
 
-  fireEvent(type, e) {
-    const handler = this.event[type]
-    if (handler) {
-      return handler.call(this, e)
-    }
-  }
 
-  toStyle() {
-    return Object.assign({}, this.classStyle, this.style)
-  }
+		this.firstChild = this.childNodes[0]
+	}
 
-  getComputedStyle() {}
+	setAttribute(key, value, silent) {
+		if (this.attributes[key] === value && silent !== false) {
+			return
+		}
+		this.attributes[key] = value
+		if (!silent) {
+			const result = {}
+			result[key] = value
 
-  toJSON() {
-    let result = {
-      id: this.ref,
-      type: this.type,
-      docId: this.docId || -10000,
-      attributes: this.attributes ? this.attributes : {}
-    }
-    result.attributes.style = this.toStyle()
+			this.ownerDocument.setAttr(this.ref, result)
 
-    const event = Object.keys(this.event)
-    if (event.length) {
-      result.event = event
-    }
+		}
+	}
 
-    if (this.childNodes.length) {
-      result.children = this.childNodes.map(child => child.toJSON())
-    }
-    return result
-  }
+	removeAttribute(key) {
+		if (this.attributes[key]) {
+			delete this.attributes[key]
+		}
+	}
 
-  replaceChild(newChild, oldChild) {
-    this.insertBefore(newChild, oldChild)
-    this.removeChild(oldChild)
-  }
+	setStyle(key, value, silent) {
+		if (this.style[key] === value && silent !== false) {
+			return
+		}
+		this.style[key] = value
+		if (!silent && this.ownerDocument) {
+			const result = {}
+			result[key] = value
 
-  destroy() {
-    const doc = getDoc(this.docId)
+			this.ownerDocument.setStyles(this.ref, result)
 
-    if (doc) {
-      delete doc.nodeMap[this.nodeId]
-    }
+		}
+	}
 
-    this.parentNode = null
-    this.childNodes.forEach(child => {
-      child.destroy()
-    })
-  }
+	setStyles(styles) {
+		Object.assign(this.style, styles)
+		if (this.ownerDocument) {
+
+			this.ownerDocument.setStyles(this.ref, styles)
+
+		}
+	}
+
+	setClassStyle(classStyle) {
+		for (const key in this.classStyle) {
+			this.classStyle[key] = ''
+		}
+
+		Object.assign(this.classStyle, classStyle)
+
+
+		this.ownerDocument.setStyles(this.ref, this.toStyle())
+
+	}
+
+	addEventListener(type, handler) {
+		if (!this.event[type]) {
+			this.event[type] = handler
+
+			this.ownerDocument.addEvent(this.ref, type)
+		}
+	}
+
+	removeEventListener(type) {
+		if (this.event[type]) {
+			delete this.event[type]
+			let doc = getDoc(this.docId)
+			doc.nodeMap[this.ref] &&
+				doc.nodeMap[this.ref].event &&
+				doc.nodeMap[this.ref].event[type]
+				? (doc.nodeMap[this.ref].event[type] = null)
+				: ''
+
+			this.ownerDocument.removeEvent(this.ref, type)
+		}
+	}
+
+	fireEvent(type, e) {
+		const handler = this.event[type]
+		if (handler) {
+			return handler.call(this, e)
+		}
+	}
+
+	toStyle() {
+		return Object.assign({}, this.classStyle, this.style)
+	}
+
+	getComputedStyle() { }
+
+	toJSON() {
+		let result = {
+			id: this.ref,
+			type: this.type,
+			docId: this.docId || -10000,
+			attributes: this.attributes ? this.attributes : {}
+		}
+		result.attributes.style = this.toStyle()
+
+		const event = Object.keys(this.event)
+		if (event.length) {
+			result.event = event
+		}
+
+		if (this.childNodes.length) {
+			result.children = this.childNodes.map(child => child.toJSON())
+		}
+		return result
+	}
+
+	replaceChild(newChild, oldChild) {
+		this.insertBefore(newChild, oldChild)
+		this.removeChild(oldChild)
+	}
+
+	destroy() {
+		const doc = getDoc(this.docId)
+
+		if (doc) {
+			delete doc.nodeMap[this.nodeId]
+		}
+
+		this.parentNode = null
+		this.childNodes.forEach(child => {
+			child.destroy()
+		})
+	}
 }
