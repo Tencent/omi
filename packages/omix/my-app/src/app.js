267a363b046f2b904ed9897146a1934183cb139e
dntzhang <dntzhang@qq.com>
2019-02-21 16:03:19 +0800

    omix - add taro-cli

---
+++ packages/omix/my-app/src/app.js
@@ -0,0 +1,45 @@
+import Taro, { Component } from '@tarojs/taro'
+import Index from './pages/index'
+import omi from 'omi-router'
+import './app.css'
+
+console.log(omi)
+
+// 如果需要在 h5 环境中开启 React Devtools
+// 取消以下注释：
+// if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
+//   require('nerv-devtools')
+// }
+
+class App extends Component {
+
+  config = {
+    pages: [
+      'pages/index/index'
+    ],
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: 'WeChat',
+      navigationBarTextStyle: 'black'
+    }
+  }
+
+  componentDidMount () {}
+
+  componentDidShow () {}
+
+  componentDidHide () {}
+
+  componentDidCatchError () {}
+
+  // 在 App 类中的 render() 函数没有实际作用
+  // 请勿修改此函数
+  render () {
+    return (
+      <Index />
+    )
+  }
+}
+
+Taro.render(<App />, document.getElementById('app'))
---
+++ packages/omix/my-app/src/pages/index/index.js
@@ -0,0 +1,29 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Text } from '@tarojs/components'
+import './index.css'
+
+export default class Index extends Component {
+
+  config = {
+    navigationBarTitleText: '首页'
+  }
+
+  componentWillMount () { }
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Text>Hello world!</Text>
+      </View>
+    )
+  }
+}
+
