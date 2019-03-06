dbde2796ff6bf1ba607e4953aa5343df7ff11073
dntzhang <dntzhang@qq.com>
2019-03-06 10:35:38 +0800

    omip - fix h5 runtime error

--- packages/omip/my-app/src/libs/mp/index.js
+++ packages/omip/my-app/src/libs/mp/index.js
@@ -1,1 +1,2 @@
 import './wx'
+import './get-app'
---
+++ packages/omip/my-app/src/libs/router/index.js
@@ -0,0 +1,17 @@
+import { WeElement, define } from 'omi'
+
+define('o-router', class extends WeElement {
+
+
+  render() {
+    console.log(Omi)
+    // console.log(this.props.routes[0].componentLoader()
+    // .then(res=>{
+    //   console.log(res)
+    //   console.log(Omi)
+    // }).catch(res=>{
+    //   console.log(res)
+    // }))
+  }
+})
+
