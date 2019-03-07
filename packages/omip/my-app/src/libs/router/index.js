f87fc5d2c1ad6d82227f7121fc9369dbd1c5e189
dntzhang <dntzhang@qq.com>
2019-03-07 16:09:37 +0800

    omip - update router

--- packages/omip/my-app/src/libs/router/index.js
+++ packages/omip/my-app/src/libs/router/index.js
@@ -1,18 +1,24 @@
 import { WeElement, define, render } from 'omi'
 import 'omi-router'
 
+let currentPage = null
+let stackList = []
+
+window.stackList = stackList
 define('o-router', class extends WeElement {
 
   _firstTime = true
 
   installed() {
+    const root = document.querySelector('#app')
     let main
     this.props.routes.forEach(current => {
-      route(current.path, () => {
+      route(current.path, (evt) => {
         current.componentLoader()
           .then(res => {
             const arr = current.path.replace(/pages/, 'page').split('/')
-            render({ nodeName: arr[1] + '-' + arr[2] }, '#app', null, true)
+            const selector = current.path.replace(/\//g, '-')
+            routeUpdate({ nodeName: arr[1] + '-' + arr[2], attributes: { className: selector } }, selector, evt.byNative, root)
           }).catch(res => {
             console.log(res)
           })
@@ -22,9 +28,12 @@ define('o-router', class extends WeElement {
         current.componentLoader()
           .then(res => {
             const arr = this.props.routes[0].path.replace(/pages/, 'page').split('/')
+            const selector = main.path.replace(/\//g, '-')
 
             this.vd = { nodeName: arr[1] + '-' + arr[2] }
             this.update()
+            const ele = root.childNodes[root.childNodes.length - 1]
+            stackList.push({ slt: selector, ele: ele, display: window.getComputedStyle(ele).display, scrollTop: 0 })
 
           }).catch(res => {
             console.log(res)
@@ -32,11 +41,12 @@ define('o-router', class extends WeElement {
       }
     })
 
-    route('*', () => {
+    route('*', (evt) => {
       main.componentLoader()
         .then(res => {
           const arr = main.path.replace(/pages/, 'page').split('/')
-          render({ nodeName: arr[1] + '-' + arr[2] }, '#app', null, true)
+          const selector = main.path.replace(/\//g, '-')
+          routeUpdate({ nodeName: arr[1] + '-' + arr[2] }, selector, evt.byNative, root)
         }).catch(res => {
           console.log(res)
         })
@@ -54,3 +64,79 @@ define('o-router', class extends WeElement {
   }
 })
 
+
+
+
+export function routeUpdate(vnode, selector, byNative, root) {
+  root.childNodes.forEach(child => {
+    if (child.style.display !== 'none') {
+      child.style.display = 'none'
+      child.onHide && child.onHide()
+    }
+  })
+  const item = getElement(selector)
+  if (byNative) {
+    //浏览器后退
+    if (item && item.ele) {
+      const ele = item.ele
+      ele.style.display = item.display
+      ele.onShow && ele.onShow()
+      currentPage = ele
+      document.documentElement.scrollTop = item.scrollTop
+      document.body.scrollTop = item.scrollTop
+      //set twice
+      setTimeout(function () {
+        document.documentElement.scrollTop = item.scrollTop
+        document.body.scrollTop = item.scrollTop
+      }, 0)
+    } else {
+      const node = render(vnode, root)
+      node.onShow && node.onShow()
+      currentPage = node
+      document.documentElement.scrollTop = 0
+      document.body.scrollTop = 0
+    }
+  } else {
+    //前进
+    if (item && item.ele) {
+      item.ele.parentNode.removeChild(item.ele)
+      remove(item)
+    }
+    const node = render(vnode, root)
+    node.onShow && node.onShow()
+    currentPage = node
+
+    const ele = root.childNodes[root.childNodes.length - 1]
+    stackList.push({ slt: selector, ele: ele, display: window.getComputedStyle(ele).display, scrollTop: 0 })
+    document.documentElement.scrollTop = 0
+    document.body.scrollTop = 0
+  }
+}
+
+function getElement(slt) {
+  for (let i = 0, length = stackList.length; i < length; i++) {
+    if (stackList[i].slt === slt) {
+      return stackList[i]
+    }
+  }
+}
+
+function remove(item) {
+  for (let i = 0, length = stackList.length; i < length; i++) {
+    if (stackList[i] === item) {
+      stackList.splice(i, 1)
+      return
+    }
+  }
+}
+
+
+window.onscroll = function () {
+  const top = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop
+  for (let i = 0, length = stackList.length; i < length; i++) {
+    if (stackList[i].ele === currentPage) {
+      stackList[i].scrollTop = top
+      return
+    }
+  }
+}
