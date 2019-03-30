74916b6a961d928a91bc9a39991b271247bd62aa
dntzhang <dntzhang@qq.com>
2019-03-31 07:15:37 +0800

    format jsx

--- packages/mp-jsx/pages/index/index.jsx
+++ packages/mp-jsx/pages/index/index.jsx
@@ -1,10 +1,17 @@
 <view class="container">
 	<view class="userinfo">
 		{(!hasUserInfo && canIUse) ? (
-			<button open-type="getUserInfo" bindgetuserinfo="getUserInfo"> 获取头像昵称 </button>
+			<button open-type="getUserInfo" bindgetuserinfo="getUserInfo">
+				获取头像昵称
+			</button>
 		) : (
 				<block>
-					<image bindtap={this.bindViewTap} class="userinfo-avatar" src={userInfo.avatarUrl} mode="cover"></image>
+					<image
+						bindtap={this.bindViewTap}
+						class="userinfo-avatar"
+						src={userInfo.avatarUrl}
+						mode="cover">
+					</image>
 					<text class="userinfo-nickname">{userInfo.nickName}</text>
 				</block>
 			)}
