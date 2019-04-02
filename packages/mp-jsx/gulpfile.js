e11853b4806b78c71fa0472112f23839d00d5d60
dntzhang <dntzhang@qq.com>
2019-04-02 10:00:55 +0800

    mp-jsx - support less

--- packages/mp-jsx/gulpfile.js
+++ packages/mp-jsx/gulpfile.js
@@ -6,6 +6,7 @@ var jsx2wxml = require('./_scripts/jsx2wxml')
 var watch = require('gulp-watch');
 var prettier = require('prettier')
 var colors = require('colors');
+var less = require('less');
 
 function buildComponent(code) {
   return `
@@ -34,6 +35,17 @@ gulp.task('watch', () => {
   })
 })
 
+gulp.task('watchLess', () => {
+  watch(['./**/*.less', '!./node_modules/**', '!./_scripts/**'], { events: ['add', 'change'] }, (evt, type) => {
+    var contents = fs.readFileSync(evt.path)
+    compileLess({
+      path: evt.path,
+      contents: contents.toString()
+    }, true)
+  })
+})
+
+
 //加 cache？同样的字符串返回同样的结果
 gulp.task('compile', () => {
   return gulp
@@ -52,8 +64,7 @@ gulp.task('compile', () => {
 function compile(file, watch) {
 
   var dir = path.dirname(file.path)
-  var arr = dir.split(/\\|\//)
-  var name = arr[arr.length - 1]
+  var name = path.basename(file.path, '.jsx')
   console.log('[编译文件]'.green ,  file.path)
   var template = jsx2wxml.default({
     ...baseOptions,
@@ -73,7 +84,41 @@ function compile(file, watch) {
   }
 }
 
-gulp.task('default', ['compile', 'watch'])
+function compileLess(file, watch) {
+  var dir = path.dirname(file.path)
+  var name = path.basename(file.path, '.less')
+  console.log('[编译文件]'.green, file.path)
+
+  less.render(file.contents, function (e, output) {
+    console.log('[编译完成]'.green, file.path)
+
+    fs.writeFileSync(dir + '/' + name + '.wxss', output.css)
+    console.log('[写入文件]'.green, name + '.wxss')
+
+    if (watch) {
+      console.log('[编译完成]'.green, name + '.wxss')
+      console.log('[监听更改]'.green, '...')
+    }
+  });
+
+}
+
+
+gulp.task('compileLess', () => {
+  return gulp
+    .src(['./**/*.less', '!./node_modules/**', '!./_scripts/**'])
+    .pipe(
+      tap(file => {
+        compileLess({
+          path: file.path,
+          contents: file.contents.toString()
+        })
+      })
+    )
+
+})
+
+gulp.task('default', ['compile', 'compileLess', 'watch', 'watchLess'])
 console.log('[开始编译]'.green ,'...')
 gulp.start('default',function(){
   console.log('[编译完成]'.green , '恭喜你全部文件编译完成。' )
