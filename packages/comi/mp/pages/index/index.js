7af2bb790f343c0c7291bddf755c7ab84720b2f7
dntzhang <dntzhang@qq.com>
2019-04-03 09:13:00 +0800

    init comi

---
+++ packages/comi/mp/pages/index/index.js
@@ -0,0 +1,36 @@
+var comi = require('../../comi/comi.js');
+
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    var md =
+      `
+## jsx
+\`\`\`jsx
+define('todo-list', function(props) {
+  return (
+    <ul>
+      {props.items.map(item => (
+        <li key={item.id}>{item.text}</li>
+      ))}
+    </ul>
+  )
+})
+ \`\`\`
+`;
+
+    comi(md, this)
+  }
+})
