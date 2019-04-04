c27c2bf4d5a18de7784a6d0691f5672223156218
dntzhang <dntzhang@qq.com>
2019-04-04 16:36:19 +0800

    init mp-svg

---
+++ packages/mp-svg/mp/pages/index/index.js
@@ -0,0 +1,75 @@
+import htm from './htm'
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+
+    function h(type, props, ...children) {
+      return { type, props, children };
+    }
+
+    const html = htm.bind(h);
+
+    const svg = html`
+<svg x="10">
+  <rect x="10" y="10" height="100" width="100"
+      style="stroke:#ff0000; fill: #0000ff"/>
+</svg>
+<svg x="200">
+  <rect onClick=${this.getUserInfo} x="10" y="10" height="100" width="100"
+      style="stroke:#009900; fill: #00cc00"/>
+</svg>
+`
+
+    console.log(svg)
+
+  },
+  getUserInfo: function (e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
