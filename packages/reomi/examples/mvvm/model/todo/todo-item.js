52efa79a078996f49d1320105364d07310039cfe
dntzhang <dntzhang@qq.com>
2019-04-26 09:16:23 +0800

    reomi - remove examples

--- packages/reomi/examples/mvvm/model/todo/todo-item.js
+++
@@ -1,9 +0,0 @@
-let id = 0
-
-export default class TodoItem {
-  constructor(text, completed) {
-    this.id = id++
-    this.text = text
-    this.completed = completed || false
-  }
-}
--- packages/reomi/examples/template/my-template.js
+++
@@ -1,8 +0,0 @@
-export default function tpl(data) {
-  return (
-    <div>
-      <div>name: {data.name}</div>
-      <div>age: {data.age}</div>
-    </div>
-  )
-}
