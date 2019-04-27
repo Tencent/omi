b77375287eee0ce40d7d3d1e112e8e232c94dee0
dntzhang <dntzhang@qq.com>
2019-04-27 10:43:45 +0800

    cax  - update demo

--- packages/cax/pages/image/index.js
+++ packages/cax/pages/image/index.js
@@ -5,7 +5,7 @@ Page({
 
     renderSVG(html`
     <svg width="200" height="200">
-      <image x="20" y="20" width="100" height="100" xlink:href="https://raw.githubusercontent.com/AlloyTeam/AlloyCrop/master/asset/test.png" />
+      <image x="20" y="20" width="100" height="100" xlink:href="https://sqimg.qq.com/qq_product_operations/im/qqlogo/imlogo_b.png" />
 
     </svg>`, 'svg-a', this)
 
