8eeada91fbe721022657d7d1a4b66db164b83346
dntzhang <dntzhang@qq.com>
2019-04-29 11:59:18 +0800

    omim - update npm script and webpack config

--- packages/omim/configs/webpack.demo.js
+++ packages/omim/configs/webpack.demo.js
@@ -1,59 +1,67 @@
 const path = require('path');
 const glob = require('glob');
 
+var argv;
+try {
+  argv = JSON.parse(process.env.npm_config_argv).original;
+} catch (ex) {
+  argv = process.argv;
+}
+
+
 module.exports = {
-  entry: './demos/simple/index.js',
+  entry: './demos/' + argv[2] + '/index.js',
   output: {
-    path: path.resolve(__dirname, '../demos/simple'),
+    path: path.resolve(__dirname, '../demos/' + argv[2]),
     filename: 'bundle.js'
   },
   mode: 'development',
   module: {
     rules: [{
-        test: /\.scss$/,
-        use: [
-          'to-string-loader',
-          'css-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
+      test: /\.scss$/,
+      use: [
+        'to-string-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          options: {
+            sourceMap: true,
 
-              // mdc-web doesn't use sass-loader's normal syntax for imports
-              // across modules, so we add all module directories containing
-              // mdc-web components to the Sass include path
-              // https://github.com/material-components/material-components-web/issues/351
-              includePaths: glob.sync(path.join(__dirname, '../node_modules/@material')).map((dir) => path.dirname(dir))
+            // mdc-web doesn't use sass-loader's normal syntax for imports
+            // across modules, so we add all module directories containing
+            // mdc-web components to the Sass include path
+            // https://github.com/material-components/material-components-web/issues/351
+            includePaths: glob.sync(path.join(__dirname, '../node_modules/@material')).map((dir) => path.dirname(dir))
 
-            }
           }
-        ]
-      },
-      // {
-      //   test: /\.scss$/,
-      //   use: [
-      //     "style-loader", // creates style nodes from JS strings
-      //     "css-loader", // translates CSS into CommonJS
-      //     {
-      //       loader: 'sass-loader',
-      //       options: {
-      //         sourceMap: true,
+        }
+      ]
+    },
+    // {
+    //   test: /\.scss$/,
+    //   use: [
+    //     "style-loader", // creates style nodes from JS strings
+    //     "css-loader", // translates CSS into CommonJS
+    //     {
+    //       loader: 'sass-loader',
+    //       options: {
+    //         sourceMap: true,
 
-      //         // mdc-web doesn't use sass-loader's normal syntax for imports
-      //         // across modules, so we add all module directories containing
-      //         // mdc-web components to the Sass include path
-      //         // https://github.com/material-components/material-components-web/issues/351
-      //         includePaths: glob.sync(path.join(__dirname, '../node_modules/@material')).map((dir) => path.dirname(dir))
+    //         // mdc-web doesn't use sass-loader's normal syntax for imports
+    //         // across modules, so we add all module directories containing
+    //         // mdc-web components to the Sass include path
+    //         // https://github.com/material-components/material-components-web/issues/351
+    //         includePaths: glob.sync(path.join(__dirname, '../node_modules/@material')).map((dir) => path.dirname(dir))
 
-      //       }
-      //     }
-      //   ]
-      // },
-      {
-        test: /\.[t|j]sx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
+    //       }
+    //     }
+    //   ]
+    // },
+    {
+      test: /\.[t|j]sx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/
+    }
     ]
   }
 };
