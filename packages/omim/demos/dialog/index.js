de62d5489ee2888e061fc5521ec8ffc0fbcc445b
ZainChen <2384439266@qq.com>
2019-05-07 00:38:35 +0800

    Fix omim dialog "index.script.html" exception, attribute variables must be all lowercase

--- packages/omim/demos/dialog/index.js
+++ packages/omim/demos/dialog/index.js
@@ -54,11 +54,11 @@ define('my-app', class extends WeElement {
         <m-dialog
           show={this.alertShow}
           message="Discard draft?"
-          cancelButton={{
+          cancelbutton={{
             text: 'Cancel',
             onClick: this.onClose
           }}
-          confirmButton={{
+          confirmbutton={{
             text: 'Discard',
             onClick: this.onConfirm
           }}
@@ -67,7 +67,7 @@ define('my-app', class extends WeElement {
           show={this.simpleShow}
           title='Select an account'
           message="Add account"
-          cancelButton={{
+          cancelbutton={{
             text: 'OK',
             raised: true,
             dense: true,
@@ -88,7 +88,7 @@ define('my-app', class extends WeElement {
           show={this.confirmationShow}
           title='Omim'
           message="Material Design for the Web, powered by Omi."
-          cancelButton={{
+          cancelbutton={{
             text: 'Cancel',
             unelevated: true,
             dense: true,
@@ -97,9 +97,9 @@ define('my-app', class extends WeElement {
               color: '#F95050',
               scale: 1.3
             },
-            onClick: this.onClose,
+            onClick: this.onClose
           }}
-          confirmButton={{
+          confirmbutton={{
             text: 'OK',
             unelevated: true,
             dense: true,
@@ -122,11 +122,11 @@ define('my-app', class extends WeElement {
           It was Toto that made Dorothy laugh, and saved her from growing as gray as her other surroundings. Toto was not gray; he was a little black dog, with long silky hair and small black eyes that twinkled merrily on either side of his funny, wee nose. Toto played all day long, and Dorothy played with him, and loved him dearly.
           Today, however, they were not playing. Uncle Henry sat upon the doorstep and looked anxiously at the sky, which was even grayer than usual. Dorothy stood in the door with Toto in her arms, and looked at the sky too. Aunt Em was washing the dishes.
           From the far north they heard a low wail of the wind, and Uncle Henry and Dorothy could see where the long grass bowed in waves before the coming storm. There now came a sharp whistling in the air from the south, and as they turned their eyes that way they saw ripples in the grass coming from that direction also."
-          cancelButton={{
+          cancelbutton={{
             text: 'Decline',
             onClick: this.onClose
           }}
-          confirmButton={{
+          confirmbutton={{
             text: 'Accept',
             onClick: this.onConfirm
           }}
