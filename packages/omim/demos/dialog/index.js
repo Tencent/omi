99f3bda8acfd23ac8842724c644e1ce39859b053
ZainChen <2384439266@qq.com>
2019-05-08 02:34:49 +0800

    update omim dialog: confirm, cancel, scrim

--- packages/omim/demos/dialog/index.js
+++ packages/omim/demos/dialog/index.js
@@ -9,35 +9,29 @@ define('my-app', class extends WeElement {
   confirmationShow = false
   scrollableShow = false
 
+  showText = ''
+
   onShowAlert = (e) => {
     this.alertShow = true
-    this.simpleShow = false
-    this.confirmationShow = false
-    this.scrollableShow = false
+    this.showText = 'open alert!'
     this.update()
   }
 
   onShowSimple = (e) => {
     this.simpleShow = true
-    this.alertShow = false
-    this.confirmationShow = false
-    this.scrollableShow = false
+    this.showText = 'open simple!'
     this.update()
   }
 
   onShowConfirmation = (e) => {
     this.confirmationShow = true
-    this.simpleShow = false
-    this.alertShow = false
-    this.scrollableShow = false
+    this.showText = 'open confirmation!'
     this.update()
   }
 
   onShowScrollable = (e) => {
     this.scrollableShow = true
-    this.simpleShow = false
-    this.alertShow = false
-    this.confirmationShow = false
+    this.showText = 'open scrollable!'
     this.update()
   }
   
@@ -46,6 +40,7 @@ define('my-app', class extends WeElement {
     this.alertShow = false
     this.confirmationShow = false
     this.scrollableShow = false
+    this.showText = 'Declined... Maybe next time?'
     this.update()
   }
 
@@ -54,6 +49,7 @@ define('my-app', class extends WeElement {
     this.alertShow = false
     this.confirmationShow = false
     this.scrollableShow = false
+    this.showText = 'Accepted, thanks!'
     this.update()
   }
 
@@ -65,26 +61,29 @@ define('my-app', class extends WeElement {
         <m-button onClick={this.onShowConfirmation} ripple>Confirmation</m-button>
         <m-button onClick={this.onShowScrollable} ripple>Scrollable</m-button>
         <m-dialog
+          onScrim={this.onClose}
+          onCancel={this.onClose}
+          onConfirm={this.onConfirm}
           show={this.alertShow}
-          scrimcancel
           message={<p>Discard draft?</p>}
           cancelbutton={{
             text: 'Cancel',
-            onClick: this.onClose
+            // onClick: this.onClose
           }}
           confirmbutton={{
             text: 'Discard',
-            onClick: this.onConfirm
+            // onClick: this.onConfirm
           }}
         />
         <m-dialog
+          onScrim={this.onClose}
           css={css}
           show={this.simpleShow}
           scrimcancel
           title='Select an account'
           message={
             <ul class="mdc-list mdc-list--avatar-list" style="list-style-type: none;">
-              <li onClick={this.onClose} class="mdc-list-item" tabindex="0" data-mdc-dialog-action="user1@example.com">
+              <li onClick={this.onConfirm} class="mdc-list-item" tabindex="0" data-mdc-dialog-action="user1@example.com">
                 <table>
                   <tr>
                     <td>
@@ -99,7 +98,7 @@ define('my-app', class extends WeElement {
                   </tr>
                 </table>
               </li>
-              <li onClick={this.onClose} class="mdc-list-item" tabindex="0" data-mdc-dialog-action="user1@example.com">
+              <li onClick={this.onConfirm} class="mdc-list-item" tabindex="0" data-mdc-dialog-action="user1@example.com">
                 <table>
                   <tr>
                     <td>
@@ -114,7 +113,7 @@ define('my-app', class extends WeElement {
                   </tr>
                 </table>
               </li>
-              <li onClick={this.onClose} class="mdc-list-item" tabindex="0" data-mdc-dialog-action="user1@example.com">
+              <li onClick={this.onConfirm} class="mdc-list-item" tabindex="0" data-mdc-dialog-action="user1@example.com">
                 <table>
                   <tr>
                     <td>
@@ -133,6 +132,7 @@ define('my-app', class extends WeElement {
           }
         />
         <m-dialog
+          onScrim={this.onClose}
           css={css}
           show={this.confirmationShow}
           scrimcancel
@@ -188,7 +188,7 @@ define('my-app', class extends WeElement {
             dense: true,
             icon: {
               path: 'M923 283.6a260.04 260.04 0 0 0-56.9-82.8 264.4 264.4 0 0 0-84-55.5A265.34 265.34 0 0 0 679.7 125c-49.3 0-97.4 13.5-139.2 39-10 6.1-19.5 12.8-28.5 20.1-9-7.3-18.5-14-28.5-20.1-41.8-25.5-89.9-39-139.2-39-35.5 0-69.9 6.8-102.4 20.3-31.4 13-59.7 31.7-84 55.5a258.44 258.44 0 0 0-56.9 82.8c-13.9 32.3-21 66.6-21 101.9 0 33.3 6.8 68 20.3 103.3 11.3 29.5 27.5 60.1 48.2 91 32.8 48.9 77.9 99.9 133.9 151.6 92.8 85.7 184.7 144.9 188.6 147.3l23.7 15.2c10.5 6.7 24 6.7 34.5 0l23.7-15.2c3.9-2.5 95.7-61.6 188.6-147.3 56-51.7 101.1-102.7 133.9-151.6 20.7-30.9 37-61.5 48.2-91 13.5-35.3 20.3-70 20.3-103.3.1-35.3-7-69.6-20.9-101.9zM512 814.8S156 586.7 156 385.5C156 283.6 240.3 201 344.3 201c73.1 0 136.5 40.8 167.7 100.4C543.2 241.8 606.6 201 679.7 201c104 0 188.3 82.6 188.3 184.5 0 201.2-356 429.3-356 429.3z',
-              color: '#F95050',
+              color: '#FFFFFF',
               scale: 1.3
             },
             onClick: this.onClose
@@ -199,13 +199,14 @@ define('my-app', class extends WeElement {
             dense: true,
             icon: {
               path: 'M923 283.6a260.04 260.04 0 0 0-56.9-82.8 264.4 264.4 0 0 0-84-55.5A265.34 265.34 0 0 0 679.7 125c-49.3 0-97.4 13.5-139.2 39-10 6.1-19.5 12.8-28.5 20.1-9-7.3-18.5-14-28.5-20.1-41.8-25.5-89.9-39-139.2-39-35.5 0-69.9 6.8-102.4 20.3-31.4 13-59.7 31.7-84 55.5a258.44 258.44 0 0 0-56.9 82.8c-13.9 32.3-21 66.6-21 101.9 0 33.3 6.8 68 20.3 103.3 11.3 29.5 27.5 60.1 48.2 91 32.8 48.9 77.9 99.9 133.9 151.6 92.8 85.7 184.7 144.9 188.6 147.3l23.7 15.2c10.5 6.7 24 6.7 34.5 0l23.7-15.2c3.9-2.5 95.7-61.6 188.6-147.3 56-51.7 101.1-102.7 133.9-151.6 20.7-30.9 37-61.5 48.2-91 13.5-35.3 20.3-70 20.3-103.3.1-35.3-7-69.6-20.9-101.9zM512 814.8S156 586.7 156 385.5C156 283.6 240.3 201 344.3 201c73.1 0 136.5 40.8 167.7 100.4C543.2 241.8 606.6 201 679.7 201c104 0 188.3 82.6 188.3 184.5 0 201.2-356 429.3-356 429.3z',
-              color: '#F95050',
+              color: '#FFFFFF',
               scale: 1.3
             },
             onClick: this.onConfirm
           }}
         />
         <m-dialog
+          onScrim={this.onClose}
           show={this.scrollableShow}
           scrollable
           scrimcancel
@@ -233,6 +234,7 @@ define('my-app', class extends WeElement {
             onClick: this.onConfirm
           }}
         />
+        <p>{this.showText}</p>
       </div>
     )
   }
--- packages/omim/src/dialog/index.tsx
+++ packages/omim/src/dialog/index.tsx
@@ -9,7 +9,6 @@ import { htmlToVdom } from '../util.ts'
 interface Props {
   show: boolean,
   scrollable: boolean,
-  scrimcancel: boolean,
   title: string,
   message: string,
   cancelbutton: object,
@@ -27,7 +26,6 @@ export default class Dialog extends WeElement<Props, Data>{
   static propTypes = {
     show: Boolean,
     scrollable: Boolean,
-    scrimcancel: Boolean,  //无法关闭show属性问题未解决(现在在框架中使用需要在外部打开dialog时设置关闭所有其它dialog，原生js使用没问题)
     title: String,
     message: String,
     cancelbutton: Object,
@@ -38,23 +36,28 @@ export default class Dialog extends WeElement<Props, Data>{
     
   }
 
-  showDialog = true
+  onScrim = evt  => {
+    this.fire('scrim')
+    evt.stopPropagation()
+  }
 
-  onScrimCancel = (e) => {
-    this.showDialog = false
-    this.update()
-    this.showDialog = true
+  onCancel = evt => {
+    this.fire('cancel')
+    evt.stopPropagation()
+  }
+
+  onConfirm = evt => {
+    this.fire('confirm')
+    evt.stopPropagation()
   }
   
   render(props) {
     return (
       <div {...extractClass(props, 'mdc-dialog', {
-        'mdc-dialog--open': props.show && this.showDialog,
+        'mdc-dialog--open': props.show,
         'mdc-dialog--scrollable': props.scrollable
       })}>
-        {/* {console.log(props)}
-        {console.log(props.title+":"+props.show+"---"+this.showDialog)} */}
-        {(props.scrimcancel) ? <div class='mdc-dialog__scrim' onClick={this.onScrimCancel}></div> : <div class='mdc-dialog__scrim'></div> }
+        <div class='mdc-dialog__scrim' onClick={this.onScrim}></div>
         <div class='mdc-dialog__container'>
           <div class='mdc-dialog__surface'>
             {(props.title) && <h2 class='mdc-dialog__title'>{props.title}</h2>}
@@ -64,8 +67,8 @@ export default class Dialog extends WeElement<Props, Data>{
             {
               ((props.cancelbutton) || (props.confirmbutton)) &&
               <footer class='mdc-dialog__actions'>
-                {(props.cancelbutton) && <m-button ripple {...props.cancelbutton}>{props.cancelbutton.text}</m-button>}
-                {(props.confirmbutton) && <m-button ripple {...props.confirmbutton}>{props.confirmbutton.text}</m-button>}
+                {(props.cancelbutton) && <m-button onClick={this.onCancel} ripple {...props.cancelbutton}>{props.cancelbutton.text}</m-button>}
+                {(props.confirmbutton) && <m-button onClick={this.onConfirm} ripple {...props.confirmbutton}>{props.confirmbutton.text}</m-button>}
               </footer>
             }
           </div>
