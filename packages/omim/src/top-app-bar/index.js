5eeb146e844e97e188eb6c548c8050020744f9c6
ZainChen <2384439266@qq.com>
2019-05-09 11:29:09 +0800

    omim - update dialog demo, build button, update top-app-bar

--- packages/omim/src/top-app-bar/index.js
+++ packages/omim/src/top-app-bar/index.js
@@ -470,7 +470,7 @@ function toComment(sourceMap) {
                     return _super !== null && _super.apply(this, arguments) || this;
                 }
                 Icon.prototype.render = function (props) {
-                    return (omi_1.h("i", __assign({}, omi_1.extractClass(props, 'm-icon')), omi_1.h("svg", { viewBox: "0 0 1024 1024", class: omi_1.classNames({ rotate: props.rotate }), width: props.scale + 'em', height: props.scale + 'em', fill: props.color, "aria-hidden": "true" }, props.paths ? (props.paths.map(function (item) {
+                    return (omi_1.h("i", __assign({}, omi_1.extractClass(props, 'm-icon')), omi_1.h("svg", { viewBox: '0 0 ' + props.view + ' ' + props.view, class: omi_1.classNames({ rotate: props.rotate }), width: props.scale + 'em', height: props.scale + 'em', fill: props.color, "aria-hidden": "true" }, props.paths ? (props.paths.map(function (item) {
                         var attrs = { d: item.path, fill: props.color || 'black' };
                         if (item.color)
                             attrs.fill = item.color;
@@ -479,11 +479,13 @@ function toComment(sourceMap) {
                 };
                 Icon.css = css;
                 Icon.defaultProps = {
+                    view: 1024,
                     scale: 2
                 };
                 Icon.propTypes = {
                     path: String,
                     paths: Object,
+                    view: Number,
                     scale: Number
                 };
                 Icon = __decorate([
