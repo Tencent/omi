13bb488ca73f9d18eebe2f1b696014643db08a62
dntzhang <dntzhang@qq.com>
2019-05-09 19:28:19 +0800

    omim - update snackbar

--- packages/omim/src/snackbar/index.tsx
+++ packages/omim/src/snackbar/index.tsx
@@ -3,7 +3,11 @@ import * as css from './index.scss'
 import { MDCSnackbar } from '@material/snackbar';
 
 interface Props {
-  show?: boolean
+  show?: boolean,
+  stacked?: boolean,
+  msg: string,
+  buttonText?: string,
+  leading?: boolean
 }
 
 interface Data {
@@ -20,35 +24,59 @@ export default class Snackbar extends WeElement<Props, Data>{
   }
 
   static propTypes = {
-    show: Boolean
+    show: Boolean,
+    stacked: Boolean,
+    msg: String,
+    buttonText: String,
+    leading: Boolean
   }
 
   snackbar: MDCSnackbar
   installed() {
 
     const snackbar = new MDCSnackbar(this.shadowRoot.querySelector('.mdc-snackbar'));
-  
-    snackbar.listen('MDCSnackbar:open', () => {
-      this.fire('open')
+
+    snackbar.listen('MDCSnackbar:opened', () => {
+      this.fire('opened')
     })
+
+    snackbar.listen('MDCSnackbar:closed', () => {
+      this.fire('closed')
+      //直接改掉下次需要 diff 的 attr
+      this['__omiattr_'].show = false
+    })
+
     this.snackbar = snackbar
-    if(this.props.show){
+    if (this.props.show) {
       this.snackbar.open()
+    } else {
+      this.snackbar.close()
     }
   }
 
+  updated() {
+    if (this.props.show) {
+      this.snackbar.open()
+    } else {
+      this.snackbar.close()
+    }
+  }
 
   render(props) {
     return (
-      <div class="mdc-snackbar">
+      <div {...extractClass(props, 'mdc-snackbar', {
+        'mdc-snackbar--stacked': props.stacked,
+        'mdc-snackbar--leading': props.leading
+      })}>
         <div class="mdc-snackbar__surface">
           <div class="mdc-snackbar__label"
             role="status"
             aria-live="polite">
-            Can't send photo. Retry in 5 seconds.
+            {props.msg}
           </div>
           <div class="mdc-snackbar__actions">
-            <button type="button" class="mdc-button mdc-snackbar__action">Retry</button>
+            <button type="button" class="mdc-button mdc-snackbar__action">{props.buttonText}</button>
+            <button class="mdc-icon-button mdc-snackbar__dismiss material-icons" title="Dismiss" style="color:white;">close</button>
           </div>
         </div>
       </div>
