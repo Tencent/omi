3598a92a0eb11a9565f589912336d6d23f305cfb
mtonhuang <v_mtonhuang@tencent.com>
2019-05-10 12:28:01 +0800

    mdc-tag

---
+++ packages/omim/src/tag/index.tsx
@@ -0,0 +1,39 @@
+import { tag, WeElement, h, extractClass } from 'omi'
+import * as css from './index.scss'
+import '../icon'
+
+interface Props {
+  type: string,
+  active: boolean,
+  size: string,
+  icon: object
+}
+
+interface Data {
+
+}
+
+@tag('m-tag')
+export default class Tag extends WeElement<Props, Data>{
+  static css = css
+
+  static propTypes = {
+    type: String,
+    active: Boolean,
+    size: String,
+    icon: Object
+  }
+
+  render(props) {
+    return (
+      <div {...extractClass(props, 
+        'mdc-tag',
+        props.type ? 'mdc-tag--'+`${props.type}` : '',
+        props.size ? 'mdc-tag--' + `${props.size}` : ''
+      )}>
+        <span><slot></slot></span>
+        {(props.icon) && <m-icon class='m-icon' {...props.icon}></m-icon>}
+      </div>
+    )
+  }
+}
