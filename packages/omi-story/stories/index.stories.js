8f97cd38afa86f5b46f00ca8c2828d5fd1d1da36
dntzhang <dntzhang@qq.com>
2019-05-14 14:54:27 +0800

    omi-story - init

---
+++ packages/omi-story/stories/index.stories.js
@@ -0,0 +1,12 @@
+import { document, console } from 'global';
+import { storiesOf } from '@storybook/html';
+
+storiesOf('Demo', module)
+  .add('heading', () => '<h1>Hello World</h1>')
+  .add('button', () => {
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.innerText = 'Hello Button';
+    button.addEventListener('click', e => console.log(e));
+    return button;
+  });
