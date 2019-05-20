ac0186e9f0e8be51822e6ca2cd9b0c6a1fb764ac
ZainChen <2384439266@qq.com>
2019-05-20 09:54:45 +0800

    omim - drawer add change.

--- packages/omim/src/drawer/index.tsx
+++ packages/omim/src/drawer/index.tsx
@@ -66,10 +66,12 @@ export default class Drawer extends WeElement<Props, Data>{
     }
     const list = MDCList.attachTo(this.shadowRoot.querySelector('.mdc-list'));
     list.wrapFocus = true;
-    //The event will fire twice, why?
     list.listen('MDCList:action', (evt: any) => {
-      console.log('zainzain')
-      this.fire('change', this.listAll[evt.detail.index])
+      //The event will fire twice, why?(Solution: Prevent duplication listen)
+      if(!this.listAll[evt.detail.index].markListen) {
+        this.fire('change', this.listAll[evt.detail.index])
+        this.listAll[evt.detail.index].markListen = true
+      }
     })
   }
 
