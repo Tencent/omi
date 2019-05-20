7679a819d7368765f7377f248a5c7097cec7e9b1
ZainChen <2384439266@qq.com>
2019-05-20 14:29:04 +0800

    omim - m-switch complete

--- packages/omim/demos/switch/index.js
+++ packages/omim/demos/switch/index.js
@@ -1,12 +1,24 @@
 import '../../src/switch/index.tsx'
-import { render, h } from 'omi'
 
-render(
-  <div>
-    <m-switch label='Label'> </m-switch>
-    <br />
+import { define, WeElement, render, h } from 'omi'
 
 
-  </div>
-  , 'body')
+define('my-app', class extends WeElement {
 
+  onChange = (e) => {
+    console.log(e.detail)
+  }
+
+  render() {
+    return <div>
+      <m-switch label='LabelA' value='omim one' onChange={this.onChange}></m-switch>
+      <br /><br />
+      <m-switch label='LabelB' value='omim two' checked onChange={this.onChange}></m-switch>
+      <br /><br />
+      <m-switch label='LabelC' value='omim three' checked disabled onChange={this.onChange}></m-switch>
+      <br /><br />
+      <m-switch label='LabelD' value='omim four' disabled onChange={this.onChange}></m-switch>
+    </div>
+  }
+})
+render(<my-app />, 'body')
--- packages/omim/src/switch/index.d.ts
+++ packages/omim/src/switch/index.d.ts
@@ -1,4 +1,5 @@
 import { WeElement } from 'omi';
+import { MDCSwitch, MDCSwitchFoundation } from '@material/switch';
 interface Props {
     label?: string;
     disabled?: boolean;
@@ -15,7 +16,11 @@ export default class Switch extends WeElement<Props, Data> {
         checked: BooleanConstructor;
         value: StringConstructor;
     };
+    static defaultProps: {};
+    switchControl: MDCSwitch;
+    switchFoundation: MDCSwitchFoundation;
     installed(): void;
+    onInput: (evt: any) => void;
     render(props: any): JSX.Element[];
 }
 export {};
