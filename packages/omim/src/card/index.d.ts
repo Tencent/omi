da7369d0f098f6ee1d01ee3077bb7e6aea4c9051
dntzhang <dntzhang@qq.com>
2019-05-21 17:08:35 +0800

    omim - build card

--- packages/omim/src/card/index.d.ts
+++ packages/omim/src/card/index.d.ts
@@ -1,23 +1,17 @@
 import { WeElement } from 'omi';
-import '../icon';
 interface Props {
-    ripple: boolean;
-    mini: boolean;
-    iconRight: boolean;
-    icon: string;
 }
 interface Data {
 }
-export default class Fab extends WeElement<Props, Data> {
+export default class Card extends WeElement<Props, Data> {
     static css: string;
     static propTypes: {
-        ripple: BooleanConstructor;
-        mini: BooleanConstructor;
-        iconRight: BooleanConstructor;
-        icon: StringConstructor;
+        width: StringConstructor;
+        img: StringConstructor;
+        title: StringConstructor;
+        subTitle: StringConstructor;
+        content: StringConstructor;
     };
-    install(): void;
-    installed(): void;
     render(props: any): JSX.Element;
 }
 export {};
