ad3174b4456e86994727ce9646d42ab99b9ef039
dntzhang <dntzhang@qq.com>
2019-05-21 17:24:40 +0800

    omim - update admin template

--- packages/omim/admin/src/about.tsx
+++ packages/omim/admin/src/about.tsx
@@ -1,14 +1,24 @@
 import * as css from './_dashboard.css'
 import { define, WeElement, h } from 'omi'
-
+import '@omim/core/card'
+import * as bg from './bg.png'
 
 define('my-about', class extends WeElement {
   static css = css
 
+  onClick = ()=>{
+    location.href = 'https://github.com/Tencent/omi'
+  }
+
   render() {
-    return <div><div class="settings-box padded block">
-    <div class="info-section"><div class="secondary">Omim Templates</div>
-    <div class="secondary">版权所有2019 Omi Inc. 保留所有权利。</div></div>
-    <div class="info-section"><div class="secondary">Omim Templates 的诞生离不开 <a target="_blank" href="http://www.omijs.org/">Omi</a> 开源项目以及其他<a target="_blank" href="https://github.com/Tencent/omi">开源软件</a>。</div></div><div class="secondary"><a target="_blank" href="https://github.com/Tencent/omi/tree/master/packages/omim">Omim Github 地址</a></div></div></div>
+    return <div onClick={this.onClick}>
+      <m-card 
+        width='350px'
+        css='.mdc-card{text-indent: 0;margin:40px auto;}'
+        title='Omim Templates'
+        subTitle='by omi team - omijs.org'
+        content='版权所有2019 Omi Inc. 保留所有权利。' img={bg}>
+      </m-card>
+    </div>
   }
 })
