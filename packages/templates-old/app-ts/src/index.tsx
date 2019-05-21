9c66b03dfe906f3e200d98a2abe2ff318b0154e4
dntzhang <dntzhang@qq.com>
2019-05-21 13:17:26 +0800

    omi-cli - update template of ts

--- packages/templates-old/app-ts/src/index.tsx
+++ packages/templates-old/app-ts/src/index.tsx
@@ -1,43 +1,6 @@
-import { WeElement, render, h, define, tag } from 'omi'
-import './hello-omi'
-import './index.css'
-import * as css from './_index.less'
-import * as logo from './logo.svg'
+import './index.css';
+import './my-app'
+import { render } from 'omi'
 
-interface MyAppProps {
-	name: string
-}
+render(<my-app />, 'body')
 
-interface MyAppData {
-	abc: string
-}
-
-@tag('my-app')
-export default class extends WeElement<MyAppProps, MyAppData> {
-
-	static css = css
-
-	onAbc = (evt: CustomEvent) => {
-		this.data.abc = ` by ${evt.detail.name}`
-		this.update()
-	}
-
-	render(props) {
-		return (
-			<div class="app">
-				<header class="app-header">
-					<img
-						src={logo}
-						class="app-logo"
-						alt="logo"
-					/>
-					<h1 class="app-title">Welcome to {props.name}</h1>
-				</header>
-				{this.data.abc}	
-				<hello-omi onAbc={this.onAbc} msg="Omi"></hello-omi>
-			</div>
-		)
-	}
-}
-
-render(<my-app name='Omi'></my-app>, '#root')
