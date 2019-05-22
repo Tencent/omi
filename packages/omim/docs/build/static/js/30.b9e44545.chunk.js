cc54868e46e950db2f30ebac6339a2b5482375ac
dntzhang <dntzhang@qq.com>
2019-05-22 16:07:15 +0800

    omim - build docs && repl

--- packages/omim/docs/build/static/js/30.b9e44545.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([30],{58:function(n,e){n.exports="## Switch\n\nSwitch toggle the state of a single setting on or off. They are the preferred way to adjust settings on mobile.\n\n## Usage\n\n```jsx\n<m-switch label='Label1'></m-switch>\n\n<m-switch label='Label2'></m-switch>\n\n<m-switch label='Label3'></m-switch>\n```\n\n## API\n\n### Props\n\n```jsx\n{\n  label?: string,\n  disabled?: boolean,\n  checked?: boolean,\n  value: string\n}\n```"}});
-//# sourceMappingURL=30.b9e44545.chunk.js.map
