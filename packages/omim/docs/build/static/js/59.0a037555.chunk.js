fa5919882338e21a291443aaf73d4fe91ff6d56d
dntzhang <dntzhang@qq.com>
2019-05-27 10:56:36 +0800

    omim - build docs & repl

---
+++ packages/omim/docs/build/static/js/59.0a037555.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([59],{39:function(n,r){n.exports='## Avatar\n\nUsed to represent users or things, supporting the display of pictures, icons or characters.\n\n## Usage\n\n```html\n<m-avatar size="60" src="avatar.jpg"></m-avatar>\n\n<m-avatar icon="person"></m-avatar>\n```\n\n\n## API\n\n### Props\n\n```jsx\n{\n    alt?: string;\n    src?: string;\n    size?: number;\n    square?: boolean;\n    icon?: string;\n    color?: string;\n    backgroundColor?: string;\n}\n```'}});
+//# sourceMappingURL=59.0a037555.chunk.js.map
