e4a7236d91d9696d7ecde232b6fd3c18470d674d
dntzhang <dntzhang@qq.com>
2019-05-27 17:18:26 +0800

    omim - update template

--- packages/omim/templates/src/dashboard.tsx
+++ packages/omim/templates/src/dashboard.tsx
@@ -1,6 +1,6 @@
 import * as css from './_dashboard.css'
 import { define, WeElement, h } from 'omi'
-
+import '@omim/core/row'
 
 define('my-dashboard', class extends WeElement {
   static css = css
@@ -8,7 +8,47 @@ define('my-dashboard', class extends WeElement {
   
   render() {
     return <div class="app">
-      ABC
+    
+
+      <m-row type="flex" justify="space-around" align="bottom" cols={[{
+          xs: 24,
+          sm: 24,
+          md: 8,
+          lg: 7,
+          xl: 6
+        }, {
+          xs: 24,
+          sm: 12,
+          md: 8,
+          lg: 10,
+          xl: 12
+        }, {
+          xs: 24,
+          sm: 12,
+          md: 8,
+          lg: 7,
+          xl: 6
+        }]}
+        css="
+        ::slotted(div){
+          background: #c2d5f5;
+          width: 100%;
+          display: block;
+          text-align: center;
+          color: #0052d9;
+          height:40px;
+          line-height:40px;
+        }
+        ::slotted(div:nth-child(2n + 1)) {
+          width: 100%;
+          display: block;
+          text-align: center;
+        background: #d9dde4;
+        }">
+          <div>col-a</div>
+          <div>col-b</div>
+          <div>col-c</div>
+        </m-row>
     </div>
   }
 })
