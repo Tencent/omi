ea7aa2177c273561339bfdd70821f91aaf871b52
ZainChen <2384439266@qq.com>
2019-05-29 17:05:50 +0800

    omim - dialog vdom to slot, fix list mouse hover black question

--- packages/omim/demos/dialog/index.js
+++ packages/omim/demos/dialog/index.js
@@ -122,7 +122,7 @@ define('my-app', class extends WeElement {
             text: 'Discard'
           }}
         >
-          <p>Discard draft?</p>
+          <p style='margin-top: 0;'>Discard draft?</p>
         </m-dialog>
         <m-dialog
           onOpening={this.onOpenClose}
@@ -187,7 +187,6 @@ define('my-app', class extends WeElement {
           }
         />
         <m-dialog
-          css={css}
           onOpening={this.onOpenClose}
           onOpened={this.onOpenClose}
           onClosing={this.onOpenClose}
@@ -195,103 +194,11 @@ define('my-app', class extends WeElement {
           onScrim={this.onClose}
           show={this.confirmationShow}
           title='Phone ringtone'
-          // message={
-          //   <ul class="mdc-list" style="list-style-type: none;">
-          //     <li onClick={this.onList1} class="mdc-list-item m-list-radio" tabindex={this.listIndex1}>
-          //     <m-radio
-          //         css={`
-          //           .mdc-form-field {
-          //             padding: 0 16px;
-          //             line-height: 48px;
-          //           }
-          //           .mdc-radio {
-          //             margin-left: -7.2px;
-          //           }
-          //           .mdc-form-field > label {
-          //             font-size: initial;
-          //             cursor: pointer;
-          //           }
-          //           /*以下两个css覆盖为了防止pc端dialog内radio出现一闪一闪的黑色列表*/
-          //           .mdc-radio::before {
-          //             transition: auto;
-          //             z-index: 0;
-          //           }
-          //           .mdc-radio__native-control {
-          //             z-index: 0;
-          //           }
-          //         `}
-          //         name="abc"
-          //         label='Never Gonna Give You Up'
-          //       ></m-radio>
-          //     </li>
-          //     <li onClick={this.onList2} class="mdc-list-item m-list-radio" tabindex={this.listIndex2}>
-          //       <m-radio
-          //         css={`
-          //           .mdc-form-field {
-          //             padding: 0 16px;
-          //             line-height: 48px;
-          //           }
-          //           .mdc-radio {
-          //             margin-left: -7.2px;
-          //           }
-          //           .mdc-form-field > label {
-          //             font-size: initial;
-          //             padding-right: 86px;
-          //             cursor: pointer;
-          //           }
-          //           /*以下两个css覆盖为了防止pc端dialog内radio出现一闪一闪的黑色列表*/
-          //           .mdc-radio::before {
-          //             transition: auto;
-          //             z-index: 0;
-          //           }
-          //           .mdc-radio__native-control {
-          //             z-index: 0;
-          //           }
-          //         `}
-          //         name="abc"
-          //         label='Hot Cross Buns'
-          //       ></m-radio>
-          //     </li>
-          //     <li onClick={this.onList3} class="mdc-list-item m-list-radio" tabindex={this.listIndex3}>
-          //       <m-radio
-          //         css={`
-          //           .mdc-form-field {
-          //             padding: 0 16px;
-          //             line-height: 48px;
-          //           }
-          //           .mdc-radio {
-          //             margin-left: -7.2px;
-          //           }
-          //           .mdc-form-field > label {
-          //             font-size: initial;
-          //             padding-right: 163px;
-          //             cursor: pointer;
-          //           }
-          //           /*以下两个css覆盖为了防止pc端dialog内radio出现一闪一闪的黑色列表*/
-          //           .mdc-radio::before {
-          //             transition: auto;
-          //             z-index: 0;
-          //           }
-          //           .mdc-radio__native-control {
-          //             z-index: 0;
-          //           }
-          //         `}
-          //         name="abc"
-          //         label='None'
-          //       ></m-radio>
-          //     </li>
-          //   </ul>
-          // }
           cancel-button={{
             text: 'Cancel',
             unelevated: true,
             dense: true,
             icon: 'favorite_border',
-            // svgIcon: {
-            //   path: 'M923 283.6a260.04 260.04 0 0 0-56.9-82.8 264.4 264.4 0 0 0-84-55.5A265.34 265.34 0 0 0 679.7 125c-49.3 0-97.4 13.5-139.2 39-10 6.1-19.5 12.8-28.5 20.1-9-7.3-18.5-14-28.5-20.1-41.8-25.5-89.9-39-139.2-39-35.5 0-69.9 6.8-102.4 20.3-31.4 13-59.7 31.7-84 55.5a258.44 258.44 0 0 0-56.9 82.8c-13.9 32.3-21 66.6-21 101.9 0 33.3 6.8 68 20.3 103.3 11.3 29.5 27.5 60.1 48.2 91 32.8 48.9 77.9 99.9 133.9 151.6 92.8 85.7 184.7 144.9 188.6 147.3l23.7 15.2c10.5 6.7 24 6.7 34.5 0l23.7-15.2c3.9-2.5 95.7-61.6 188.6-147.3 56-51.7 101.1-102.7 133.9-151.6 20.7-30.9 37-61.5 48.2-91 13.5-35.3 20.3-70 20.3-103.3.1-35.3-7-69.6-20.9-101.9zM512 814.8S156 586.7 156 385.5C156 283.6 240.3 201 344.3 201c73.1 0 136.5 40.8 167.7 100.4C543.2 241.8 606.6 201 679.7 201c104 0 188.3 82.6 188.3 184.5 0 201.2-356 429.3-356 429.3z',
-            //   color: '#FFFFFF',
-            //   scale: 1.3
-            // },
             onClick: this.onClose
           }}
           confirm-button={{
@@ -299,15 +206,11 @@ define('my-app', class extends WeElement {
             unelevated: true,
             dense: true,
             icon: 'favorite',
-            // svgIcon: {
-            //   path: 'M923 283.6a260.04 260.04 0 0 0-56.9-82.8 264.4 264.4 0 0 0-84-55.5A265.34 265.34 0 0 0 679.7 125c-49.3 0-97.4 13.5-139.2 39-10 6.1-19.5 12.8-28.5 20.1-9-7.3-18.5-14-28.5-20.1-41.8-25.5-89.9-39-139.2-39-35.5 0-69.9 6.8-102.4 20.3-31.4 13-59.7 31.7-84 55.5a258.44 258.44 0 0 0-56.9 82.8c-13.9 32.3-21 66.6-21 101.9 0 33.3 6.8 68 20.3 103.3 11.3 29.5 27.5 60.1 48.2 91 32.8 48.9 77.9 99.9 133.9 151.6 92.8 85.7 184.7 144.9 188.6 147.3l23.7 15.2c10.5 6.7 24 6.7 34.5 0l23.7-15.2c3.9-2.5 95.7-61.6 188.6-147.3 56-51.7 101.1-102.7 133.9-151.6 20.7-30.9 37-61.5 48.2-91 13.5-35.3 20.3-70 20.3-103.3.1-35.3-7-69.6-20.9-101.9zM512 814.8S156 586.7 156 385.5C156 283.6 240.3 201 344.3 201c73.1 0 136.5 40.8 167.7 100.4C543.2 241.8 606.6 201 679.7 201c104 0 188.3 82.6 188.3 184.5 0 201.2-356 429.3-356 429.3z',
-            //   color: '#FFFFFF',
-            //   scale: 1.3
-            // },
             onClick: this.onConfirm
           }}
         >
-          <ul class="mdc-list" style="list-style-type: none;">
+          <link rel='stylesheet' type='text/css' href='./mdc.list.min.css' />
+          <ul class="mdc-list">
             <li onClick={this.onList1} class="mdc-list-item m-list-radio" tabindex={this.listIndex1}>
             <m-radio
                 css={`
