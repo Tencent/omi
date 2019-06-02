1582cfa7a99026f19b6e4daa7de9c54fe032f42d
ZainChen <2384439266@qq.com>
2019-06-02 09:25:39 +0800

    omim - update top-app-bar

--- packages/omim/src/top-app-bar/index.tsx
+++ packages/omim/src/top-app-bar/index.tsx
@@ -88,9 +88,9 @@ export default class topAppBar extends WeElement<Props, Data>{
 
   isArray(value){
     if (typeof Array.isArray === "function") {
-      return Array.isArray(value);
+      return Array.isArray(value)
     } else {
-      return Object.prototype.toString.call(value) === "[object Array]";
+      return Object.prototype.toString.call(value) === "[object Array]"
     }
   }
 
@@ -107,80 +107,78 @@ export default class topAppBar extends WeElement<Props, Data>{
   }
 
   render(props) {
-    return (
-      <div>
-        <header {...extractClass(props, 'mdc-top-app-bar', {
-          'mdc-top-app-bar--fixed': props.fixed,
-          'mdc-top-app-bar--dense': props.dense,
-          'mdc-top-app-bar--short': props.short || props.shortCollapsed,
-          'mdc-top-app-bar--short-collapsed': props.shortCollapsed,
-          'mdc-top-app-bar--prominent': props.prominent
-        })}>
-          <div class='mdc-top-app-bar__row'>
-            {(props.navigation || props.heading) &&
-            <section class='mdc-top-app-bar__section mdc-top-app-bar__section--align-start'>
-              {/* Support for custom elements, development completed (支持自定义元素，开发完成) */}
-              {props.navigationElement ?
-              <div class='mdc-top-app-bar__navigation-icon' onClick={this.onNavigation}>
-                {typeof props.navigationElement === 'string' ? htmlToVdom(props.navigationElement) : props.navigationElement}
-              </div> :
-              props.navigation &&
-              (typeof props.navigation === 'string' ?
-              <m-icon-button class='mdc-top-app-bar__navigation-icon' icon={props.navigation} onClick={this.onNavigation}></m-icon-button> :
-              this.isArray(props.navigation) ?
-              <m-icon-button class='mdc-top-app-bar__navigation-icon' icons={props.navigation} onClick={this.onNavigation}></m-icon-button> :
-              (props.navigation.mIconButton ?
-              <m-icon-button class='mdc-top-app-bar__navigation-icon' {...props.navigation.mIconButton} onClick={this.onNavigation}>
-                {(props.children && props.children[0] && props.children[1]) && props.children}
-              </m-icon-button> :
-              props.navigation.mIcon ?
-              <button class='mdc-top-app-bar__navigation-icon' onClick={this.onNavigation}>
-                {(props.navigation.mIcon.path || props.navigation.mIcon.paths) ?
-                <m-icon {...props.navigation.mIcon}></m-icon> : props.navigation.text}
+    return [
+      <header {...extractClass(props, 'mdc-top-app-bar', {
+        'mdc-top-app-bar--fixed': props.fixed,
+        'mdc-top-app-bar--dense': props.dense,
+        'mdc-top-app-bar--short': props.short || props.shortCollapsed,
+        'mdc-top-app-bar--short-collapsed': props.shortCollapsed,
+        'mdc-top-app-bar--prominent': props.prominent
+      })}>
+        <div class='mdc-top-app-bar__row'>
+          {(props.navigation || props.heading) &&
+          <section class='mdc-top-app-bar__section mdc-top-app-bar__section--align-start'>
+            {/* Support for custom elements, development completed (支持自定义元素，开发完成) */}
+            {props.navigationElement ?
+            <div class='mdc-top-app-bar__navigation-icon' onClick={this.onNavigation}>
+              {typeof props.navigationElement === 'string' ? htmlToVdom(props.navigationElement) : props.navigationElement}
+            </div> :
+            props.navigation &&
+            (typeof props.navigation === 'string' ?
+            <m-icon-button class='mdc-top-app-bar__navigation-icon' icon={props.navigation} onClick={this.onNavigation}></m-icon-button> :
+            this.isArray(props.navigation) ?
+            <m-icon-button class='mdc-top-app-bar__navigation-icon' icons={props.navigation} onClick={this.onNavigation}></m-icon-button> :
+            (props.navigation.mIconButton ?
+            <m-icon-button class='mdc-top-app-bar__navigation-icon' {...props.navigation.mIconButton} onClick={this.onNavigation}>
+              {(props.children && props.children[0] && props.children[1]) && props.children}
+            </m-icon-button> :
+            props.navigation.mIcon ?
+            <button class='mdc-top-app-bar__navigation-icon' onClick={this.onNavigation}>
+              {(props.navigation.mIcon.path || props.navigation.mIcon.paths) ?
+              <m-icon {...props.navigation.mIcon}></m-icon> : props.navigation.text}
+            </button> :
+            <button class='mdc-top-app-bar__navigation-icon' onClick={this.onNavigation}>
+              {props.navigation.text}
+            </button>))}
+            {props.heading && <span class='mdc-top-app-bar__title'>{props.heading}</span>}
+          </section>}
+          {(props.actionItems || props.actionElements) &&
+          <section class='mdc-top-app-bar__section mdc-top-app-bar__section--align-end'>
+            {/* Support for custom elements, development completed (支持自定义元素，开发完成) */}
+            {props.actionElements ?
+            props.actionElements.map((item, index) => {
+              return <div accessKey={index.toString()} class='mdc-top-app-bar__action-item' onClick={this.onAction}>
+                {typeof item === 'string' ? htmlToVdom(item) : item}
+              </div>
+            }) :
+            props.actionItems &&
+            (typeof props.actionItems === 'string' ?
+            <m-icon-button accessKey='0' class='mdc-top-app-bar__action-item' icon={props.actionItems} onClick={this.onAction}></m-icon-button> :
+            props.actionItems.map((item, index) => {
+              return typeof item === 'string' ?
+              <m-icon-button accessKey={index.toString()} class='mdc-top-app-bar__action-item' icon={item} onClick={this.onAction}></m-icon-button> :
+              this.isArray(item) ?
+              <m-icon-button accessKey={index.toString()} class='mdc-top-app-bar__action-item' icons={item} onClick={this.onAction}></m-icon-button> :
+              item.mIconButton ?
+              <m-icon-button accessKey={index.toString()} class='mdc-top-app-bar__action-item' {...item.mIconButton} onClick={this.onAction}></m-icon-button> :
+              item.mIcon ?
+              <button accessKey={index.toString()} class='mdc-top-app-bar__action-item' onClick={this.onAction}>
+                {(item.mIcon.path || item.mIcon.paths) ? <m-icon accessKey={index.toString()} {...item.mIcon}></m-icon> : item.text}
               </button> :
-              <button class='mdc-top-app-bar__navigation-icon' onClick={this.onNavigation}>
-                {props.navigation.text}
-              </button>))}
-              {props.heading && <span class='mdc-top-app-bar__title'>{props.heading}</span>}
-            </section>}
-            {(props.actionItems || props.actionElements) &&
-            <section class='mdc-top-app-bar__section mdc-top-app-bar__section--align-end'>
-              {/* Support for custom elements, development completed (支持自定义元素，开发完成) */}
-              {props.actionElements ?
-              props.actionElements.map((item, index) => {
-                return <div accessKey={index.toString()} class='mdc-top-app-bar__action-item' onClick={this.onAction}>
-                  {typeof item === 'string' ? htmlToVdom(item) : item}
-                </div>
-              }) :
-              props.actionItems &&
-              (typeof props.actionItems === 'string' ?
-              <m-icon-button accessKey='0' class='mdc-top-app-bar__action-item' icon={props.actionItems} onClick={this.onAction}></m-icon-button> :
-              props.actionItems.map((item, index) => {
-                return typeof item === 'string' ?
-                <m-icon-button accessKey={index.toString()} class='mdc-top-app-bar__action-item' icon={item} onClick={this.onAction}></m-icon-button> :
-                this.isArray(item) ?
-                <m-icon-button accessKey={index.toString()} class='mdc-top-app-bar__action-item' icons={item} onClick={this.onAction}></m-icon-button> :
-                item.mIconButton ?
-                <m-icon-button accessKey={index.toString()} class='mdc-top-app-bar__action-item' {...item.mIconButton} onClick={this.onAction}></m-icon-button> :
-                item.mIcon ?
-                <button accessKey={index.toString()} class='mdc-top-app-bar__action-item' onClick={this.onAction}>
-                  {(item.mIcon.path || item.mIcon.paths) ? <m-icon accessKey={index.toString()} {...item.mIcon}></m-icon> : item.text}
-                </button> :
-                <button accessKey={index.toString()} class='mdc-top-app-bar__action-item' onClick={this.onAction}>
-                  {item.text}
-                </button>
-              }))}
-            </section>}
-          </div>
-        </header>
-        {props.adjust &&
-        <div {...extractClass(props,
-          (props.short || props.shortCollapsed) ? 'mdc-top-app-bar--short-fixed-adjust' :
-          (props.dense && props.prominent) ? 'mdc-top-app-bar--dense-prominent-fixed-adjust' :
-          props.dense ? 'mdc-top-app-bar--dense-fixed-adjust' :
-          props.prominent ? 'mdc-top-app-bar--prominent-fixed-adjust' : 'mdc-top-app-bar--fixed-adjust'
-        )}></div>}
-      </div>
-    )
+              <button accessKey={index.toString()} class='mdc-top-app-bar__action-item' onClick={this.onAction}>
+                {item.text}
+              </button>
+            }))}
+          </section>}
+        </div>
+      </header>,
+      (props.adjust &&
+      <div {...extractClass(props,
+        (props.short || props.shortCollapsed) ? 'mdc-top-app-bar--short-fixed-adjust' :
+        (props.dense && props.prominent) ? 'mdc-top-app-bar--dense-prominent-fixed-adjust' :
+        props.dense ? 'mdc-top-app-bar--dense-fixed-adjust' :
+        props.prominent ? 'mdc-top-app-bar--prominent-fixed-adjust' : 'mdc-top-app-bar--fixed-adjust'
+      )}></div>)
+    ]
   }
 }
