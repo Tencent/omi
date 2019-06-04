ea513987f1838f0912caa3c18789f0306fc51d14
dntzhang <dntzhang@qq.com>
2019-06-04 09:41:17 +0800

    omim  - update docs

--- packages/omim/docs/src/elements/my-demo/index.js
+++ packages/omim/docs/src/elements/my-demo/index.js
@@ -33,7 +33,7 @@ define('my-demo', class extends WeElement {
     if (!this.demo) return
     return (
       <div>
-        <iframe style={`height:${window.innerHeight - 59}px;visibility:${this.show ? 'visible' : 'hidden'};`} src={this.demo} ></iframe>
+        <iframe style={`width:${window.innerWidth - 1180}px;height:${window.innerHeight - 59}px;visibility:${this.show ? 'visible' : 'hidden'};`} src={this.demo} ></iframe>
 
         <div class="icon" onClick={this.toggle}>
           <svg viewBox="64 64 896 896" class="" style={`left:${this.show ? '14px' : '0px'};`} data-icon="left" width="1.2em" height="1.2em" fill="currentColor" aria-hidden="true" focusable="false"><path d="M724 218.3V141c0-6.7-7.7-10.4-12.9-6.3L260.3 486.8a31.86 31.86 0 0 0 0 50.3l450.8 352.1c5.3 4.1 12.9.4 12.9-6.3v-77.3c0-4.9-2.3-9.6-6.1-12.6l-360-281 360-281.1c3.8-3 6.1-7.7 6.1-12.6z"></path></svg>
