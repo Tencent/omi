b6b50271dd43dbf81a2dfd727491351d2926992a
dntzhang <dntzhang@qq.com>
2019-06-06 09:47:30 +0800

    omim - init test-react project

---
+++ packages/omim/test-react/src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import '@omim/core/button'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <a
+          className="App-link"
+          href="https://github.com/Tencent/omi"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Omi and Omim
+        </a>
+        <m-button>Omim Button</m-button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
