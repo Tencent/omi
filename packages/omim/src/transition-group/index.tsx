4f89a9726316e30a9dc701880acdddb55eb9aba4
dntzhang <dntzhang@qq.com>
2019-06-07 20:18:34 +0800

    omim - update transition

--- packages/omim/src/transition-group/index.tsx
+++ packages/omim/src/transition-group/index.tsx
@@ -1,6 +1,6 @@
 //todo duration and delay support
 
-import { tag, WeElement, h, getHost } from 'omi'
+import { tag, WeElement, h, getHost, render } from 'omi'
 
 interface Props {
   appear?: boolean,
@@ -33,155 +33,118 @@ export default class TransitionGroup extends WeElement<Props, Data>{
   install() {
     //@ts-ignore 不是 slot，所以需要共享一下 host 的 css
     this.css = getHost(this).constructor.css
-    if (this.props.appear) {
-      this.appear()
-      this.props.show = true
-    }
-  }
-
-  toggle() {
-    this.props.show = !this.props.show
-    if (this.props.show)
-      this.enter()
-    else
-      this.leave()
   }
 
   callback: () => void
 
   receiveProps() {
-    console.log('receiveProps')
     //find the leave item
-
     let el
-    // bind end event and trigger this.update()
-    this.callback = function () {
-      el.classList.remove(this.props.name + '-leave-to')
-      el.classList.remove(this.props.name + '-leave-active')
-      this.update()
-    }.bind(this)
+    let vel
+    let insertIndex
+
 
     const arr = []
-    this.shadowRoot.childNodes.forEach(node=>{
-      if(node['__omiattr_'] && node['__omiattr_'].hasOwnProperty('key')){
-        //map[node['__omiattr_'].key] = node
+    this.shadowRoot.childNodes.forEach(node => {
+      if (node['__omiattr_'] && node['__omiattr_'].hasOwnProperty('key')) {
         arr.push(node)
       }
     })
-    
-    
 
     const vnodes = this.render(this.props)
     const len = vnodes.length
-    console.log(len)
-    console.log(arr)
+    //@ts-ignore
+    window.xx = this
     //insert
-    if(len > arr.length){
-      for(let i=0;i<len;i++){
-        if(vnodes[i].attributes.key!== arr[i]['__omiattr_'].key){
-          console.log(vnodes[i])
+    if (len > arr.length) {
+      for (let i = 0; i < len; i++) {
+        if (i === len - 1) {
+          vel = vnodes[i]
+          insertIndex = i
+          break
+        } else if (vnodes[i].attributes.key !== arr[i]['__omiattr_'].key) {
+          vel = vnodes[i]
+          insertIndex = i
           break
         }
       }
-    }else if(len < arr.length){ //delete
-      for(let i=0;i<arr.length;i++){
-        if(i === arr.length-1){
+    } else if (len < arr.length) { //delete
+      for (let i = 0; i < arr.length; i++) {
+        if (i === arr.length - 1) {
           el = arr[i]
-        } else if(vnodes[i].attributes.key!== arr[i]['__omiattr_'].key){
-          console.log(111)
+        } else if (vnodes[i].attributes.key !== arr[i]['__omiattr_'].key) {
           el = arr[i]
           break
         }
       }
     }
-    
+
+    if (el) {
+
+      // bind end event and trigger this.update()
+      this.callback = function () {
+        el.parentNode.removeChild(el)
+        this.update()
+      }.bind(this)
+      this.elOnce(el, 'transitionend', this.callback)
+      this.elOnce(el, 'animationend', this.callback)
+      // add leave class
+      el.classList.add(this.props.name + '-leave')
+      el.classList.add(this.props.name + '-leave-active')
 
 
-    this.elOnce(el, 'transitionend', this.callback)
-    this.elOnce(el, 'animationend', this.callback)
-    // add leave class
-    el.classList.add(this.props.name + '-leave')
-    el.classList.add(this.props.name + '-leave-active')
-    
+      window.setTimeout(() => {
+        el.classList.remove(this.props.name + '-leave')
+        el.classList.add(this.props.name + '-leave-to')
+      }, 0)
+    } else {
 
-    window.setTimeout( ()=> {
-      el.classList.remove(this.props.name + '-leave')
-      el.classList.add(this.props.name + '-leave-to')
-    }, 0)
+      console.log(vel)
+      let iel = render(vel, null)
+
+      // bind end event and trigger this.update()
+      this.callback = function () {
+        console.log(111)
+        //@ts-ignore
+        this.shadowRoot.removeChild(iel)
+        setTimeout(()=>{
+          console.log(444)
+          this.update()
+        },1000)
+      }.bind(this)
+      this.elOnce(iel, 'transitionend', this.callback)
+      this.elOnce(iel, 'animationend', this.callback)
+
+
+      if (insertIndex === len - 1) {
+        console.log('appendChild')
+        //@ts-ignore
+        this.shadowRoot.appendChild(iel)
+      } else {
+        console.log('insertBefore')
+        //@ts-ignore
+        this.shadowRoot.insertBefore(iel, this.children[insertIndex])
+      }
+      //@ts-ignore
+      iel.classList.add(this.props.name + '-enter')
+      //@ts-ignore
+      iel.classList.add(this.props.name + '-enter-active')
+
+      window.setTimeout(() => {
+        //@ts-ignore
+        iel.classList.remove(this.props.name + '-enter')
+        //@ts-ignore
+        iel.classList.add(this.props.name + '-enter-to')
+      }, 0)
+
+    }
+
+
+
+
     return false
   }
 
-
-  appear() {
-    this.fire('before-appear')
-    this.classList.add(this.props.name + '-appear')
-    this.classList.add(this.props.name + '-appear-active')
-
-    this.callback = function () {
-      this.classList.remove(this.props.name + '-appear-to')
-      this.classList.remove(this.props.name + '-appear-active')
-      this.fire('after-appear')
-    }.bind(this)
-    this.once('transitionend', this.callback)
-    this.once('animationend', this.callback)
-
-    window.setTimeout(function () {
-      this.classList.remove(this.props.name + '-appear')
-      this.classList.add(this.props.name + '-appear-to')
-      this.fire('appear')
-    }.bind(this), 0)
-  }
-
-  _tempNode: HTMLElement
-
-  enter() {
-    if (this.props.remove && this.children.length == 0) {
-      this.appendChild(this._tempNode)
-    }
-    this.fire('before-enter')
-    this.classList.remove(this.props.name + '-leave-active')
-    this.classList.remove(this.props.name + '-leave-to')
-    this.classList.add(this.props.name + '-enter')
-    this.classList.add(this.props.name + '-enter-active')
-
-    this.callback = function () {
-      this.classList.remove(this.props.name + '-enter-active')
-      this.fire('after-enter')
-    }.bind(this)
-    this.once('transitionend', this.callback)
-    this.once('animationend', this.callback)
-
-    window.setTimeout(function () {
-      this.classList.remove(this.props.name + '-enter')
-      this.classList.add(this.props.name + '-enter-to')
-      this.fire('enter')
-    }.bind(this), 0)
-  }
-
-  leave() {
-    this.fire('before-leave')
-    this.classList.remove(this.props.name + '-enter-active')
-    this.classList.remove(this.props.name + '-enter-to')
-    this.classList.add(this.props.name + '-leave')
-    this.classList.add(this.props.name + '-leave-active')
-
-    this.callback = function (e) {
-      this.classList.remove(this.props.name + '-leave-active')
-      this.fire('after-leave')
-      this._tempNode = this.children[0]
-      if (this.props.remove) {
-        this._tempNode.parentNode.removeChild(this._tempNode)
-      }
-    }.bind(this)
-    this.once('transitionend', this.callback)
-    this.once('animationend', this.callback)
-
-    window.setTimeout(function () {
-      this.classList.remove(this.props.name + '-leave')
-      this.classList.add(this.props.name + '-leave-to')
-      this.fire('leave')
-    }.bind(this), 0)
-  }
 
 
   once(name, callback) {
