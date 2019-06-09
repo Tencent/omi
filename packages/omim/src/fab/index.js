d79f4e9e32c79294c1e09c78b1049aaee27c0ba6
dntzhang <dntzhang@qq.com>
2019-06-09 12:40:48 +0800

    omim - build src

--- packages/omim/src/fab/index.js
+++ packages/omim/src/fab/index.js
@@ -1939,20 +1939,26 @@ exports.default = Fab;
           \**********************/
         /*! no static exports found */
         /***/ (function (module, exports) {
-            if (!document.body.style.getPropertyValue('--mdc-theme-primary')) {
-                document.body.style.setProperty('--mdc-theme-primary', '#0072d9');
-                document.body.style.setProperty('--mdc-theme-secondary', '#2170b8');
-                document.body.style.setProperty('--mdc-theme-error', '#f5222d');
-                document.body.style.setProperty('--mdc-theme-surface', '#ffffff');
-                document.body.style.setProperty('--mdc-theme-on-primary', '#ffffff');
-                document.body.style.setProperty('--mdc-theme-on-secondary', '#ffffff');
-                document.body.style.setProperty('--mdc-theme-on-error', '#ffffff');
-                document.body.style.setProperty('--mdc-theme-on-surface', '#000000');
-                document.body.style.setProperty('--mdc-theme-background', '#ffffff');
-                document.body.style.setProperty('--mdc-shape-small-component-radius', '4px');
-                document.body.style.setProperty('--mdc-shape-medium-component-radius', '4px');
-                document.body.style.setProperty('--mdc-shape-large-component-radius', '0px');
-                document.body.style.setProperty('--mdc-typography--font-family', 'Roboto, sans-serif');
+            theme();
+            document.addEventListener('DOMContentLoaded', function () {
+                theme();
+            });
+            function theme() {
+                if (document.body && !document.body.style.getPropertyValue('--mdc-theme-primary')) {
+                    document.body.style.setProperty('--mdc-theme-primary', '#0072d9');
+                    document.body.style.setProperty('--mdc-theme-secondary', '#2170b8');
+                    document.body.style.setProperty('--mdc-theme-error', '#f5222d');
+                    document.body.style.setProperty('--mdc-theme-surface', '#ffffff');
+                    document.body.style.setProperty('--mdc-theme-on-primary', '#ffffff');
+                    document.body.style.setProperty('--mdc-theme-on-secondary', '#ffffff');
+                    document.body.style.setProperty('--mdc-theme-on-error', '#ffffff');
+                    document.body.style.setProperty('--mdc-theme-on-surface', '#000000');
+                    document.body.style.setProperty('--mdc-theme-background', '#ffffff');
+                    document.body.style.setProperty('--mdc-shape-small-component-radius', '4px');
+                    document.body.style.setProperty('--mdc-shape-medium-component-radius', '4px');
+                    document.body.style.setProperty('--mdc-shape-large-component-radius', '0px');
+                    document.body.style.setProperty('--mdc-typography--font-family', 'Roboto, sans-serif');
+                }
             }
             /***/ 
         }),
