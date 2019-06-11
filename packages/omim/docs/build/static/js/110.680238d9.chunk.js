db2594627aed4b2f1d19541ce0371f4c04dd96f0
dntzhang <dntzhang@qq.com>
2019-06-11 19:07:23 +0800

    omim - build docs

---
+++ packages/omim/docs/build/static/js/110.680238d9.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([110],{42:function(n,e){n.exports="## Breadcrumb\n\nBreadcrumb.\n\n## Usage\n\n```html\n<m-breadcrumb split=\"/\" items=\"[\n  { text: 'Home' },\n  { text: 'Helper' },\n  { text: 'About' }\n]\"></m-breadcrumb>\n\n\n<m-breadcrumb  split=\"/\" items=\"[\n  { text: 'Home', href:'https://tencent.github.io/omi/', icon:'home' },\n  { text: 'Helper', icon:'help' },\n  { text: 'About', icon: 'people' }\n]\"></m-breadcrumb>\n```\n\n## API\n\n### Props\n\n```jsx\n{\n  items: object,\n  split: string\n}\n```"}});
+//# sourceMappingURL=110.680238d9.chunk.js.map
