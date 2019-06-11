7c5eb368fc2b78dac31ffdb9f2290779f7b2e580
dntzhang <dntzhang@qq.com>
2019-06-11 09:43:50 +0800

    omi-cli - update template

--- packages/omi-cli/template/app/src/elements/app-omil/index.js
+++ packages/omi-cli/template/app/src/elements/app-omil/index.js
@@ -26,37 +26,38 @@ const appOmil = class extends WeElement {
   static css =
     `
 .weui-btn {
-    position: relative;
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    padding-left: 14px;
-    padding-right: 14px;
-    box-sizing: border-box;
-    font-size: 18px;
-    text-align: center;
-    text-decoration: none;
-    color: #FFFFFF;
-    line-height: 2.55555556;
-    border-radius: 3px;
-    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
-    overflow: hidden;
-    border-width: 0;
-    width: 100%;
-    border-width: 0;
-    outline: 0;
-    -webkit-appearance: none;
+  position: relative;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: 14px;
+  padding-right: 14px;
+  box-sizing: border-box;
+  font-size: 18px;
+  text-align: center;
+  text-decoration: none;
+  color: #FFFFFF;
+  line-height: 2.55555556;
+  border-radius: 3px;
+  -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  overflow: hidden;
+  border-width: 0;
+  width: 100%;
+  border-width: 0;
+  outline: 0;
+  -webkit-appearance: none;
 }
 ` +
     style +
     `
-        button{
-            background-color: #58bc58;
-        }
-    `;
+    button{
+      background-color: #58bc58;
+      cursor: pointer;
+    }
+  `;
   install() {
     this.data = {
-      title: "I am omil button."
+      title: "Install Omi Snippets!"
     };
   }
   omiSnippets() {
