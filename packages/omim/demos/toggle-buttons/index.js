c15d3782e4db96598621b1331b37f46ef10d429a
dntzhang <dntzhang@qq.com>
2019-06-18 14:51:18 +0800

    omim - fix toggle buttons

--- packages/omim/demos/toggle-buttons/index.js
+++ packages/omim/demos/toggle-buttons/index.js
@@ -15,29 +15,18 @@ function onChange(item) {
 
 render(
   <div>
-    <m-toggle-buttons multiple-selection items={[
-
-    { selected: true, icon: 'account_balance' },
-    { icon: 'stars'  },
-    { icon: 'av_timer'  },
-    { disabled: true, icon: 'equalizer'  }
-    ]}></m-toggle-buttons>
-    <br /><br />
-
-    <m-toggle-buttons multiple-selection items={[
-
-      { selected: true, path: 'M15 15H3v2h12v-2zm0-8H3v2h12V7zM3 13h18v-2H3v2zm0 8h18v-2H3v2zM3 3v2h18V3H3z' },
-      { path: 'M7 15v2h10v-2H7zm-4 6h18v-2H3v2zm0-8h18v-2H3v2zm4-6v2h10V7H7zM3 3v2h18V3H3z' },
-      { path: 'M3 21h18v-2H3v2zm6-4h12v-2H9v2zm-6-4h18v-2H3v2zm6-4h12V7H9v2zM3 3v2h18V3H3z' },
-      { disabled: true, path: 'M3 21h18v-2H3v2zm0-4h18v-2H3v2zm0-4h18v-2H3v2zm0-4h18V7H3v2zm0-6v2h18V3H3z' }
-    ]}></m-toggle-buttons>
-    <br /><br />
     <m-toggle-buttons items={[
-
       { selected: true, path: 'M15 15H3v2h12v-2zm0-8H3v2h12V7zM3 13h18v-2H3v2zm0 8h18v-2H3v2zM3 3v2h18V3H3z' },
       { path: 'M7 15v2h10v-2H7zm-4 6h18v-2H3v2zm0-8h18v-2H3v2zm4-6v2h10V7H7zM3 3v2h18V3H3z' },
       { path: 'M3 21h18v-2H3v2zm6-4h12v-2H9v2zm-6-4h18v-2H3v2zm6-4h12V7H9v2zM3 3v2h18V3H3z' },
       { disabled: true, path: 'M3 21h18v-2H3v2zm0-4h18v-2H3v2zm0-4h18v-2H3v2zm0-4h18V7H3v2zm0-6v2h18V3H3z' }
+    ]}></m-toggle-buttons>
+    <br /><br />
+    <m-toggle-buttons multiple-selection items={[
+      { selected: true, icon: 'account_balance', value: 0 },
+      { icon: 'stars', value: 1 },
+      { icon: 'av_timer', value: 2 },
+      { disabled: true, icon: 'equalizer', value: 3 }
     ]}></m-toggle-buttons>
   </div>
   , 'body')
--- packages/omim/src/toggle-buttons/index.tsx
+++ packages/omim/src/toggle-buttons/index.tsx
@@ -6,6 +6,8 @@ import '../theme.ts'
 import { MDCRipple } from '@material/ripple'
 
 interface Props {
+  multipleSelection?: boolean
+  items: any[] 
 }
 
 @tag('m-toggle-buttons')
@@ -15,26 +17,43 @@ class ToggleButtons extends WeElement<Props, {}> {
   }
 
   static propTypes = {
+    multipleSelection: Boolean,
+    items: Array 
   }
 
   static css = css
 
-	installed() {
-		this.shadowRoot.querySelectorAll('.ripple').forEach(dom => {
-      console.log(123)
-			new MDCRipple(dom)
-		})
-	}
+  installed() {
+    this.shadowRoot.querySelectorAll('.ripple').forEach(dom => {
+      new MDCRipple(dom)
+    })
+  }
+
+  clickHandler = (item)=> {
+    if(item.disabled) return
+    if(this.props.multipleSelection){
+      item.selected = !item.selected
+      this.fire('change', this.props.items)
+    } else if(!item.selected){
+      this.props.items.forEach(item=> item.selected = false)
+      item.selected = true
+      this.fire('change', item)
+    }
+    this.update(true)
+  }
 
   render(props) {
     return <div class="m-root">
-      {props.items.map(item => <button class={classNames("mb-root mtb-root", {
-        'selected': item.selected,
-        'disabled': item.disabled
-      })} tabindex="0"
+      {props.items.map(item => <button
+        class={classNames("mb-root mtb-root", {
+          'selected': item.selected,
+          'disabled': item.disabled
+        })}
+        tabindex="0"
         disabled={item.disabled}
-        type="button" value="left">
-       {typeof item.icon === 'string'?<i class='material-icons'>{item.icon}</i>: <span class="mtb-label">
+        onClick={_=>this.clickHandler(item)}
+        type="button">
+        {typeof item.icon === 'string' ? <i class='material-icons'>{item.icon}</i> : <span class="mtb-label">
           <svg class="msi-root" focusable="false"
             viewBox="0 0 24 24" aria-hidden="true" role="presentation">
             <path d={item.path}></path>
