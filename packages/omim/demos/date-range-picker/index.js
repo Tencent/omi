ea48d506985cda400f69e6c4f37744dc4e028988
dntzhang <dntzhang@qq.com>
2019-06-21 11:28:57 +0800

    omim - init date range picker

---
+++ packages/omim/demos/date-range-picker/index.js
@@ -0,0 +1,11 @@
+
+
+import '../../src/date-range-picker/index.tsx'
+import { render, h } from 'omi'
+
+render(
+  <div>
+   <m-date-range-picker show={true}></m-date-range-picker>
+  </div>
+  , 'body')
+
---
+++ packages/omim/src/date-range-picker/moment.js
@@ -0,0 +1,5 @@
+export default {
+  defineLocale: function (locale, data) {
+    return data
+  }
+}
