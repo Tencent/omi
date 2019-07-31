ee2a2d02f48b93efa2eabfb4cdd9c040bb123d13
dntzhang <dntzhang@qq.com>
2019-07-31 11:13:35 +0800

    omi - add vue example

---
+++ packages/omi/examples/vue/app.vue
@@ -0,0 +1,41 @@
+<script>
+import { define, WeElement, html } from 'omi'
+
+define('my-ele', class extends WeElement {
+  static propTypes = {
+    name: String
+  }
+
+  clickHandle = ()=>{
+   
+  //  this.fire('myEvent')
+
+    this.dispatchEvent(new CustomEvent('myEvent'), { detail: 111 })
+  }
+
+  render(props) {
+    return (
+      html`<div onClick=${this.clickHandle}>Hello, ${props.name}! </div>`
+    )
+  }
+})
+
+export default{
+
+  methods:{
+    myEvent(){
+ console.log('abc')
+
+    }
+  }
+
+}
+
+</script>
+
+
+<template>
+  <div>
+    <my-ele name="Omi" @myEvent="myEvent" />
+  </div>
+</template>
