0c272af8bf3d2a61e7c05436160ff7c16ba0b936
dntzhang <dntzhang@qq.com>
2019-08-01 18:08:43 +0800

    omip-new - generate testing

--- packages/omip-new/index.js
+++ packages/omip-new/index.js
@@ -1,6 +1,7 @@
 const parser = require("@babel/parser")
 const traverse = require("@babel/traverse").default
 const t = require("@babel/types")
+const generate = require("@babel/generator").default
 
 const code = `
 import { render, h } from 'omis'
@@ -44,6 +45,32 @@ traverse(ast, {
   },
   JSXElement(p) {
     //p.stop();
+    p.node.openingElement.attributes.push(t.jsxAttribute(t.JSXIdentifier('a'), t.StringLiteral('1')))
     console.log(p)
   }
-});
+})
+
+console.log(generate(ast).code)
+
+
+//输出
+// import { render, h } from 'omis';
+
+// const Index = (props, store) => {
+//   return <view a="1" a="1">
+//       <text a="1"> {store.item.index}}: {store.item.msg} </text>
+//       <text a="1"> Time: {store.item.time} </text>
+//     </view>;
+// };
+
+// Index.store = _ => {
+//   return {
+//     item: {
+//       index: 0,
+//       msg: 'this is a template',
+//       time: '2016-09-15'
+//     }
+//   };
+// };
+
+// render(<Index a="1" />, 'body');
