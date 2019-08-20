b9ea8fa2e974dc87b14c0a51e578ee36dd58b2c6
dntzhang <dntzhang@qq.com>
2019-08-20 10:12:45 +0800

    text: kbone canvas testing

--- packages/omi-kbone/src/log.js
+++ packages/omi-kbone/src/log.js
@@ -1,11 +1,16 @@
 import { render, h } from 'omis'
 
+const Comp = <canvas ref="canvas" class="canvas" ref="canvas" canvas-id="canvas" width="300" height="200"></canvas>
+
 export default function createApp() {
   const container = document.createElement('div')
   container.id = 'app'
   document.body.appendChild(container)
 
-  render(<div >我是log页面</div>, '#app')
+  render(<div >
+    <h1>我是log页面</h1>
+    <canvas class="canvas" canvas-id="canvas" width="300" height="200"></canvas>
+  </div>, '#app')
 }
 
 "undefined" != typeof wx && wx.getSystemInfoSync || createApp()
