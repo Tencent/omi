49379e401377c3990104e29bfd756e7476cdd91c
dntzhang <dntzhang@qq.com>
2019-10-09 12:04:22 +0800

    refactor: remove dead code

--- packages/omis/react/src/omis/react.js
+++ packages/omis/react/src/omis/react.js
@@ -2,45 +2,26 @@ import * as React from 'react'
 import { obaa } from './obaa'
 import { getPath, needUpdate, fixPath } from './path'
 
-const components = []
-const updateSelfComponents = []
-
 let isSelf = false
 let currentComponent = null
+let isMultiStore = false
 
 export function $(options) {
   if (options.store) {
     $.store = options.store
-    obaa($.store.data, (prop, val, old, path) => {
-      const patch = {}
-
-      patch[fixPath(path + '-' + prop)] = true
-      components.forEach(component => {
-        if (
-          component.__$updatePath_ &&
-          needUpdate(patch, component.__$updatePath_)
-        ) {
-          component.setState({ __$id_: component.state.__$id_++ })
-
-          isSelf = false
+    if ($.store.data) {
+      observe($.store)
+    } else {
+      isMultiStore = true
+      for (let key in $.store) {
+        if ($.store[key].data) {
+          observe($.store[key], key)
         }
-      })
+      }
+    }
 
-      updateSelfComponents.forEach(component => {
-        if (
-          component.__$updateSelfPath_ &&
-          needUpdate(patch, component.__$updateSelfPath_)
-        ) {
-          component.setState({ __$id_: component.state.__$id_++ })
-          isSelf = true
-          currentComponent = component
-        }
-      })
-    })
   }
 
-  const updatePath = options.use && getPath(options.use)
-  const updateSelfPath = options.useSelf && getPath(options.useSelf)
 
   return class extends React.Component {
     state = {
@@ -51,14 +32,42 @@ export function $(options) {
 
     constructor(props) {
       super(props)
-      if (updatePath) {
-        components.push(this)
-        this.__$updatePath_ = updatePath
+
+
+      if (isMultiStore) {
+
+        if (options.use) {
+          let updatePath = {}
+          for (let storeName in options.use) {
+            getPath(options.use[storeName], updatePath, storeName)
+            $.store[storeName].components.push(this)
+          }
+          this.__$updatePath_ = updatePath
+
+
+        }
+
+        if (options.useSelf) {
+          let updateSelfPath = {}
+          for (let storeName in options.useSelf) {
+            getPath(options.useSelf[storeName], updateSelfPath, storeName)
+            $.store[storeName].updateSelfComponents.push(this)
+          }
+          this.__$updateSelfPath_ = updateSelfPath
+        }
+
+
+      } else {
+        if (options.use) {
+          $.store.components.push(this)
+          this.__$updatePath_ = getPath(options.use)
+        }
+        if (options.useSelf) {
+          $.store.updateSelfComponents.push(this)
+          this.__$updateSelfPath_ = getPath(options.useSelf)
+        }
       }
-      if (updateSelfPath) {
-        updateSelfComponents.push(this)
-        this.__$updateSelfPath_ = updateSelfPath
-      }
+
     }
 
     shouldComponentUpdate() {
@@ -67,23 +76,53 @@ export function $(options) {
     }
 
     componentWillUnmount() {
-      for (let i = 0, len = components.length; i < len; i++) {
-        if (components[i] === this) {
-          components.splice(i, 1)
-          break
-        }
-      }
-
-      for (let i = 0, len = updateSelfComponents.length; i < len; i++) {
-        if (updateSelfComponents[i] === this) {
-          updateSelfComponents.splice(i, 1)
-          break
-        }
-      }
-    }  
+      removeItem(this, $.store.components)
+      removeItem(this, $.store.updateSelfComponents)
+    }
 
     render() {
       return options.render.apply(this, arguments)
     }
   }
+}
+
+function removeItem(item, arr) {
+  for (let i = 0, len = arr.length; i < len; i++) {
+    if (arr[i] === item) {
+      arr.splice(i, 1)
+      break
+    }
+  }
+}
+
+function observe(store, storeName) {
+
+  store.components = []
+  store.updateSelfComponents = []
+  obaa(store.data, (prop, val, old, path) => {
+    const patch = {}
+
+    patch[fixPath(path + '-' + prop)] = true
+    store.components.forEach(component => {
+      if (
+        component.__$updatePath_ &&
+        needUpdate(patch, component.__$updatePath_)
+      ) {
+        component.setState({ __$id_: component.state.__$id_++ })
+
+        isSelf = false
+      }
+    })
+
+    store.updateSelfComponents.forEach(component => {
+      if (
+        component.__$updateSelfPath_ &&
+        needUpdate(patch, component.__$updateSelfPath_)
+      ) {
+        component.setState({ __$id_: component.state.__$id_++ })
+        isSelf = true
+        currentComponent = component
+      }
+    })
+  })
 }
