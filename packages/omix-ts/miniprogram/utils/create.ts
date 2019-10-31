799d595f541a0ef79042695451c5a4ee560733b3
dntzhang <dntzhang@qq.com>
2019-10-31 10:55:55 +0800

    chore: update type declaration of  create.ts

--- packages/omix-ts/miniprogram/utils/create.ts
+++ packages/omix-ts/miniprogram/utils/create.ts
@@ -62,12 +62,20 @@ interface ComponentOption {
   [key: string]: any
 }
 
+interface Computed {
+  [key: string]: any
+}
+
 interface PageOption {
     [key: string]: any,
     /**
      * 依赖 store 上的 path
      */
     use?: Array<string | object>
+    /**
+     * 计算属性
+     */
+    computed?: Computed,
     /** 生命周期回调—监听页面加载
      *
      * 页面加载时触发。一个页面只会调用一次，可以在 onLoad 的参数中获取打开当前页面路径中的参数。
