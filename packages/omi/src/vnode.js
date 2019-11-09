1ce5888f4db99e662ef9d38743f9db7e32ee5365
mrjzhang <mrjzhang@tencent.com>
2019-11-09 22:06:54 +0800

    chore(vnode): use object literal to create    vnode

--- packages/omi/src/vnode.js
+++
@@ -1,7 +0,0 @@
-/** Virtual DOM Node */
-export function VNode(nodeName, attributes, children) {
-  this.nodeName = nodeName
-  this.children = children
-  this.attributes = attributes == null ? undefined : attributes
-  this.key = attributes == null ? undefined : attributes.key
-}
