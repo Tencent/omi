85e7cb7ceba1aa2ab7c4ac00fdcf2b708b59c2ca
mrjzhang <mrjzhang@tencent.com>
2019-11-14 20:57:23 +0800

    test(omiv): add new test case

---
+++ packages/omiv/test/browser/components/nest3.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Child />
+  </div>
+</template>
+
+<script>
+import Child from './child-for-nest3.vue'
+
+export default {
+  components: {
+    Child
+  },
+  store: new (class {
+    data = {
+      count: 1
+    }
+    sub = () => {
+      this.data.count--
+    }
+    add = () => {
+      this.data.count++
+    }
+  })()
+}
+</script>
