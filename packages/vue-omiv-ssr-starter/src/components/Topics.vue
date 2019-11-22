cb1ebfb2f515b1718bd7b46303cffe2724009e52
mrjzhang <mrjzhang@tencent.com>
2019-11-22 20:28:26 +0800

    chore(omiv): 删除不必要文件

--- packages/vue-omiv-ssr-starter/src/components/Topics.vue
+++ packages/vue-omiv-ssr-starter/src/components/Topics.vue
@@ -10,14 +10,7 @@
 <script>
 export default {
   useSelf: ["topics"],
-  mounted() {
-    console.log("mounted ===== ", this.$state.topics);
-  },
-  beforeCreate() {
-    console.log("beforeCreate ===== ", this.$state && this.$state.topics);
-  },
   asyncData({ store }) {
-    console.log("asyncData ", this.$state && this.$state.topics);
     return store.getTopics();
   }
 };
