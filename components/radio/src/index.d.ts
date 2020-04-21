e6b25cbbb26e7c487e15e1ff15351589bb56835f
dntzhang <dntzhang@qq.com>
2020-04-21 16:16:16 +0800

    feat(omiu): add radio

---
+++ components/radio/src/index.d.ts
@@ -0,0 +1,26 @@
+import { WeElement } from 'omi';
+import '@omiu/common/theme.ts';
+interface Props {
+    label?: string;
+    disabled?: boolean;
+    checked?: boolean;
+    value: string;
+    name?: string;
+}
+export default class Radio extends WeElement<Props> {
+    static css: any;
+    static propTypes: {
+        label: StringConstructor;
+        disabled: BooleanConstructor;
+        checked: BooleanConstructor;
+        value: StringConstructor;
+        name: StringConstructor;
+    };
+    group: Radio[];
+    installed(): void;
+    isChecked: boolean;
+    clickHandler: () => void;
+    render(props: any): JSX.Element;
+    getScopeRoot(current: any): any;
+}
+export {};
