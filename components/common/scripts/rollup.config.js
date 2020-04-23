076c6c3fcc142165feab0d9bffa1ad2c8d56fd33
dntzhang <dntzhang@qq.com>
2020-04-23 08:41:47 +0800

    publish(@omiu/common): v0.0.5, build umd js

---
+++ components/common/scripts/rollup.config.js
@@ -0,0 +1,29 @@
+import nodeResolve from "rollup-plugin-node-resolve";
+
+import commonjs from '@rollup/plugin-commonjs';
+
+const license = require("rollup-plugin-license");
+const pkg = require("../package.json");
+const licensePlugin = license({
+  banner: `${pkg.name} v${pkg.version} http://omijs.org\r\nFront End Cross-Frameworks Framework.\r\nBy dntzhang https://github.com/dntzhang \r\n Github: https://github.com/Tencent/omi\r\n MIT Licensed.`
+});
+
+export default {
+  input: "./src/index.js",
+  output: {
+    format: "umd",
+    file: "./index.umd.js",
+    name: pkg.name,
+    sourcemap: true,
+    strict: true
+  },
+  plugins: [
+    nodeResolve({
+    	main: true
+    }),
+    commonjs(),
+
+    licensePlugin
+  ],
+  external: ['omi']
+};
