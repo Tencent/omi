f925d39bbd53723cb1e674e93bbf8a1926c5a9c6
dntzhang <dntzhang@qq.com>
2020-04-25 10:10:43 +0800

    chore: auto docs gen

--- components/input/scripts/docs-gen.js
+++ components/input/scripts/docs-gen.js
@@ -3,8 +3,22 @@ const fs = require('fs')
 
 const content = fs.readFileSync('./src/index.tsx', 'utf-8')
 
-const res = content.match(new RegExp('interface Props \\{[\\s\\S]*?}'))[0].replace('interface Props ', '')
+const props = content.match(new RegExp('interface Props \\{[\\s\\S]*?}'))[0].replace('interface Props ', '')
 
-const res2 = content.match(new RegExp('static defaultProps = \\{[\\s\\S]*?}'))[0].replace('static defaultProps = ', '').replace(/    /g, '  ').replace(/  }/g, ')')
+const defaultProps = content.match(new RegExp('static defaultProps = \\{[\\s\\S]*?}'))[0].replace('static defaultProps = ', '').replace(/    /g, '  ').replace(/  }/g, ')')
 
-console.log(res2)
+
+const eventContexts = content.match(new RegExp('this.fire\\([\\s\\S]*?,', 'g'))
+
+//fire 附近打标标记 event.detail 类型？
+const events = eventContexts.map(event => {
+  return event.replace('this.fire(\'', '').replace('\',', '')
+})
+const eventMap = {}
+events.forEach(event => {
+  eventMap[event] = 1
+})
+
+console.log(props)
+console.log(defaultProps)
+console.log(Object.keys(eventMap))
