56dfb55dff545aff568c0655df636db49e5043da
dntzhang <dntzhang@qq.com>
2020-04-26 16:05:01 +0800

    publish(@omiu/link)

--- components/link/src/index.esm.js
+++ components/link/src/index.esm.js
@@ -1,5 +1,5 @@
 /**
- * @omiu/link v0.0.3 http://omijs.org
+ * @omiu/link v0.0.4 http://omijs.org
  * Front End Cross-Frameworks Framework.
  * By dntzhang https://github.com/dntzhang
  * Github: https://github.com/Tencent/omi
