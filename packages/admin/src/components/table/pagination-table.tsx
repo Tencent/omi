ec78562c554754cc4746fd305abda0ea3c3834c8
yuqing521 <1581446178@qq.com>
2021-06-26 01:14:20 +0800

    fix(admin): add 分页列表 处理逻辑

--- packages/admin/src/components/table/pagination-table.tsx
+++ packages/admin/src/components/table/pagination-table.tsx
@@ -21,8 +21,55 @@ declare global {
 export default class extends WeElement<Props> {
     static css = sheet.target
 
-
     dataSource = [{
+        id: 1,
+        name: 'xwang',
+        age: 18,
+        address: 'Tencent'
+    }, {
+        id: 2,
+        name: 'dntzhang',
+        age: 12,
+        address: 'Tencent',
+        $config: {
+            bgColor: 'rgb(247 176 176 / 32%)'
+        }
+    },
+    {
+        id: 3,
+        name: 'lucy',
+        age: 12,
+        address: 'Tencent',
+
+    }, {
+        id: 4,
+        name: 'john',
+        age: 12,
+        address: 'Tencent',
+        $config: {
+            bgColor: 'rgb(230 162 60 / 34%)'
+        }
+    }, {
+        id: 5,
+        name: 'tim',
+        age: 12,
+        address: 'Tencent'
+    }, {
+        id: 6,
+        name: 'tim',
+        age: 12,
+        address: 'Tencent'
+    }, {
+        id: 7,
+        name: 'tim',
+        age: 12,
+        address: 'Tencent'
+    }, {
+        id: 8,
+        name: 'tim',
+        age: 12,
+        address: 'Tencent'
+    }, {
         id: 1,
         name: 'xwang',
         age: 18,
@@ -94,19 +141,92 @@ export default class extends WeElement<Props> {
         )
     }]
 
-    onClick = (evt) => {
+    paging = true
+    pageSize = 5
+    currentPage = 1
+
+    data = {
+        allData: this.dataSource,
+        filterData: [],
+        start: 1,
+        end: 1,
+        totalSize: this.dataSource.length,
+        currPageSize: this.paging ? this.pageSize : Number.MAX_SAFE_INTEGER,
+        currentPage: this.currentPage
+    }
+
+    handleSizeChange(value) {
+        this.currentPage = 1;
+        this.data.currPageSize = value;
+        this.handleChange(false);
+    }
+
+    handleCurrentChange(value) {
+        this.currentPage = value;
+        this.handleChange(false);
+    }
+
+    handleChange(reset) {
+        if (reset) {
+            this.currentPage = 1;
+        }
+        this.renderTable();
+    }
+
+    renderTable() {
+        if (this.data.totalSize > this.data.currPageSize) {
+            let startIndex = (this.currentPage - 1) * this.data.currPageSize,
+                endIndex = this.currentPage * this.data.currPageSize;
+            this.data.start = startIndex + 1;
+            this.data.end = this.data.totalSize < endIndex ? this.data.totalSize : endIndex;
+            this.data.filterData = this.data.allData.slice(startIndex, endIndex);
+        } else if (this.data.totalSize > 0) {
+            this.currentPage = 1;
+            this.data.start = 1;
+            this.data.end = this.data.totalSize;
+            this.data.filterData = this.data.allData;
+        } else {
+            this.currentPage = 1;
+            this.data.start = 0;
+            this.data.end = 0;
+            this.data.filterData = [];
+        }
+        this.update()
+    }
+
+    setData(value) {
+        this.data.allData = value;
+        this.data.totalSize = value.length;
+        this.renderTable();
+    }
+
+    change(){
+        this.renderTable();
+    }
+
+    onClick = (evt: { currentTarget: { dataset: { itemId: number } } }) => {
         console.log(Number(evt.currentTarget.dataset.itemId))
         this.deleteItemById(Number(evt.currentTarget.dataset.itemId))
     }
 
-    deleteItemById(id) {
+    deleteItemById(id: number) {
         const index = this.dataSource.indexOf(this.dataSource.find(item => item.id === id))
         if (index !== -1) {
             this.dataSource.splice(index, 1)
-            this.update()
         }
-
+        this.update()
     }
+
+    install() {
+        this.setData(this.dataSource)
+        console.log(this.data)
+        console.log(this.currentPage)
+    }
+
+    update() {
+        console.log("object", this.data)
+    }
+
 
     render(props) {
         return <div>
@@ -118,8 +238,15 @@ export default class extends WeElement<Props> {
                     stripe={false}
                     border={false}
                     compact={false}
-                    columns={this.columns} dataSource={this.dataSource}></o-table>
-                <o-pagination total="25" current-page="1" page-size="5" class={tw`pt-3`}></o-pagination>
+                    columns={this.columns} dataSource={this.data.filterData}></o-table>
+
+                <div class={tw`mt-3`}>
+                    <div class={tw`mb-2`}>
+                        <span v-if="totalSize > 1">显示第 {this.data.start} 条到第 {this.data.end} 条的数据，</span>
+                        共{this.data.totalSize} 条数据
+                    </div>
+                    <o-pagination total={this.data.totalSize} current-page={this.currentPage} page-size={this.pageSize} onChange={this.change} ></o-pagination>
+                </div>
             </div>
 
         </div>
