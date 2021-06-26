7d68296a7085db4a047938839931fed6d9f2ddf4
yuqing521 <1581446178@qq.com>
2021-06-26 11:51:34 +0800

    feat(omi-cli): 支持多入口及多页面应用

--- packages/omi-cli/template/vite/vite.config.js
+++ packages/omi-cli/template/vite/vite.config.js
@@ -1,6 +1,16 @@
+import { resolve } from "path";
+
 export default {
   esbuild: {
-    jsxFactory: 'h',
-    jsxFragment: 'Fragment'
+    jsxFactory: "h",
+    jsxFragment: "Fragment"
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        src: resolve(__dirname, "src/index.html")
+      }
+    }
   }
-}
+};
