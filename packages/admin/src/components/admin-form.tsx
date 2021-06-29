ed5e7c269c0d0b4d82f76135a40ca2ca6daa96e6
dntzhang <dntzhang@qq.com>
2021-06-29 09:05:33 +0800

    feat(admin): add datepicker

--- packages/admin/src/components/admin-form.tsx
+++ packages/admin/src/components/admin-form.tsx
@@ -5,7 +5,7 @@ import '@omiu/select'
 import '@omiu/switch'
 import '@omiu/radio'
 import '@omiu/checkbox'
-
+import '@omiu/date-picker'
 
 interface Props { }
 
@@ -25,17 +25,20 @@ export default class extends WeElement<Props> {
   items = [{ label: '广东省-深圳市', value: 1 }, { label: '江西省-抚州市', value: 2 }, { label: '河北省-石家庄', value: 3 }]
 
   render() {
+    const formItemClass = tw`leading-8 mb-6`
+    const lableClass = tw`w-16 float-left align-middle`
+
     return (
       <div style={{ width: '480px' }} class={tw`text-sm px-10 py-10 text-gray-500`}>
-        <div class={tw`leading-8 mb-6`}>
-          <label class={tw`w-16 float-left align-middle`} >用户姓名</label>
+        <div class={formItemClass}>
+          <label class={lableClass} >用户姓名</label>
           <div class={tw`leading-8 ml-16`}>
             <o-input size="small" block></o-input>
           </div>
         </div>
 
-        <div class={tw`leading-8 mb-6`}>
-          <label class={tw`w-16 float-left align-middle`} >出生地址</label>
+        <div class={formItemClass}>
+          <label class={lableClass} >出生地址</label>
           <div class={tw`leading-8 ml-16`}>
             <o-select css={`
             .o-select .o-input__inner {
@@ -46,19 +49,23 @@ export default class extends WeElement<Props> {
           </div>
         </div>
 
+        <div class={formItemClass}>
+          <label class={lableClass} >出生日期</label>
+          <div class={tw`leading-8 ml-10`}>
+            <o-date-picker locale="zh"> </o-date-picker>
+          </div>
+        </div>
 
-        <div class={tw`leading-8 mb-6`}>
-          <label class={tw`w-16 float-left align-middle`} >是否结婚</label>
+        <div class={formItemClass}>
+          <label class={lableClass} >是否结婚</label>
           <div class={tw`leading-8 ml-16 pt-1`}>
             <o-switch id="switchA" checked="0">
             </o-switch>
           </div>
         </div>
 
-
-
-         <div class={tw`leading-8 mb-6`}>
-          <label class={tw`w-16 float-left align-middle`} >喜爱水果</label>
+        <div class={formItemClass}>
+          <label class={lableClass} >喜爱水果</label>
           <div class={tw`leading-8 ml-10`}>
             <o-checkbox class={tw``} label='苹果🍎'> </o-checkbox>
             <o-checkbox class={tw` ml-6`} label='草莓🍓'> </o-checkbox>
@@ -69,33 +76,28 @@ export default class extends WeElement<Props> {
           </div>
         </div>
 
-        <div class={tw`leading-8 mb-6`}>
-          <label class={tw`w-16 float-left align-middle`} >用户性别</label>
+        <div class={formItemClass}>
+          <label class={lableClass} >用户性别</label>
           <div class={tw`leading-8 ml-16`}>
             <o-radio name="sex" checked value="0">男</o-radio>
             <o-radio name="sex" value="1" class={tw`ml-2`}>女</o-radio>
           </div>
         </div>
 
-
-        <div class={tw`leading-8 mb-6`}>
-          <label class={tw`w-16 float-left align-middle`} >个性签名</label>
+        <div class={formItemClass}>
+          <label class={lableClass} >个性签名</label>
           <div class={tw`leading-8 ml-16`}>
             <o-input type="textarea" block></o-input>
           </div>
         </div>
 
-
-
-        <div class={tw`leading-8 mb-6`}>
-          <label class={tw`w-16 float-left align-middle`} ></label>
+        <div class={formItemClass}>
+          <label class={lableClass} ></label>
           <div class={tw`leading-8 ml-16`}>
-          <o-button size="small" type="primary">创建</o-button>
-
-          <o-button size="small" class={tw`ml-6`}>重置</o-button>
+            <o-button size="small" type="primary">创建</o-button>
+            <o-button size="small" class={tw`ml-6`}>重置</o-button>
           </div>
         </div>
-
 
       </div>
     )
