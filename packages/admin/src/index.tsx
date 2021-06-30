06d3a089c75bd8f07b7a350a3d1110647c080a22
dntzhang <dntzhang@qq.com>
2021-06-30 23:06:00 +0800

    feat(omi-admin): add route transition effect

--- packages/admin/src/index.tsx
+++ packages/admin/src/index.tsx
@@ -9,6 +9,8 @@ import '@omiu/icon/warning'
 import '@omiu/icon/sentiment-satisfied-alt'
 import '@omiu/icon/table-chart'
 
+import '@omiu/transition'
+
 import './index.css'
 
 import './layouts/basic-layout'
@@ -18,66 +20,81 @@ import './components/admin-main-welcome'
 import { tw, sheet } from 'omi-twind'
 import store from './store'
 
+const fadeCSS = `.fade-leave-to,
+.fade-enter {
+  opacity: 0;
+  transform: translateX(15px);
+}
+
+.fade-leave-active,
+.fade-enter-active {
+  transition: all 500ms ease-in;
+}
+`
+
 @tag('my-app')
 export default class extends WeElement {
-  static css = sheet.target
+  static css = [sheet.target, fadeCSS]
 
   data = {
     tagName: 'admin-main-welcome'
   }
 
-  install() {
+  transition
+
+  transitionTo(tagName) {
+    this.transition.leave()
+
+    setTimeout(() => {
+      this.data.tagName = tagName
+      this.update()
+      this.transition.enter()
+    }, 500)
+  }
+
+  installed() {
+
     route('/', () => {
       this.update()
     })
 
     route('/welcome', () => {
-      this.data.tagName = 'admin-main-welcome'
-      this.update()
+      this.transitionTo('admin-main-welcome')
     })
 
     route('/table/basic', () => {
       //lazy load
-      import('./components/table/basic-table')
-      this.data.tagName = 'basic-table'
-      this.update()
+      import('./components/table/basic-table').then(() => this.transitionTo('basic-table')
+      )
+
     })
 
     route('/table/pagination', () => {
       //lazy load
-      import('./components/table/pagination-table')
-      this.data.tagName = 'pagination-table'
-      this.update()
+      import('./components/table/pagination-table').then(() => this.transitionTo('pagination-table')
+      )
     })
 
     route('/form', () => {
       //lazy load
-      import('./components/admin-form')
-      this.data.tagName = 'admin-form'
-      this.update()
+      import('./components/admin-form').then(() => this.transitionTo('admin-form'))
     })
 
 
     route('/icon', () => {
       //lazy load
-      import('./components/admin-icon')
-      this.data.tagName = 'admin-icon'
-      this.update()
+      import('./components/admin-icon').then(() => this.transitionTo('admin-icon'))
     })
 
 
     route('/error', () => {
       //lazy load
-      import('./components/status/status-error')
-      this.data.tagName = 'status-error'
-      this.update()
+      import('./components/status/status-error').then(() => this.transitionTo('status-error'))
     })
 
     route('/warning', () => {
       //lazy load
-      import('./components/status/status-warning')
-      this.data.tagName = 'status-warning'
-      this.update()
+      import('./components/status/status-warning').then(() => this.transitionTo('status-warning'))
     })
 
     route('*', function () {
@@ -88,7 +105,9 @@ export default class extends WeElement {
   render(props) {
     return (
       <basic-layout>
-        <this.data.tagName></this.data.tagName>
+        <o-transition ref={_ => this.transition = _} appear name="fade">
+          <this.data.tagName class={tw`block`}></this.data.tagName>
+        </o-transition>
       </basic-layout>
     )
   }
