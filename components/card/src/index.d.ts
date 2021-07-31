b103e1d02ae5e948bb5b031cf020871b13dbed90
ringosix <dyxzzt1108@163.com>
2021-07-31 18:58:16 +0800

    feat(card): add props: hoverable & more example

--- components/card/src/index.d.ts
+++ components/card/src/index.d.ts
@@ -1,23 +1,27 @@
 import { WeElement } from 'omi';
 interface Props {
     title?: string;
+    hoverable?: string;
     extra?: string;
     actions: any[];
     size?: 'medium' | 'small';
 }
 export default class Card extends WeElement<Props> {
-    static css: any;
+    css: any;
     static defaultProps: {
         title: string;
+        hoverable: string;
         extra: string;
     };
     static propTypes: {
         title: StringConstructor;
+        hoverable: StringConstructor;
         extra: StringConstructor;
         actions: ArrayConstructor;
         size: StringConstructor;
     };
     clickHandler: (item: any) => void;
+    handleMousemove: (item: any) => void;
     _iconTag: any;
     render(props: any): JSX.Element;
 }
