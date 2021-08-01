6c4f1b4ef3e4dfeea4d4ac5fe05a57ac1f7537cb
dntzhang <dntzhang@qq.com>
2021-08-01 19:11:15 +0800

    feat(@omiu/chart-card): support content, data and labels props

--- components/chart-card/src/index.tsx
+++ components/chart-card/src/index.tsx
@@ -5,6 +5,10 @@ import * as css from './index.scss'
 import '@omiu/chart'
 export interface Props {
   color: string
+  content: string | number
+  describe: string
+  labels: string[]
+  data: number[]
 }
 
 @tag('o-chart-card')
@@ -16,14 +20,18 @@ export default class ChartCard extends WeElement<Props> {
   }
 
   static propTypes = {
-    color: String
+    color: String,
+    labels: Array,
+    data: Array,
+    content: String,
+    describe: String
   }
 
   chartData = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: null,
     datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
+      // label: '# of Votes',
+      data: null,
       segment: {
         borderColor: 'white',
       },
@@ -38,6 +46,8 @@ export default class ChartCard extends WeElement<Props> {
         display: false
       }
     },
+    //  https://www.chartjs.org/docs/latest/configuration/responsive.html
+    // Note that in order for the above code to correctly resize the chart height, the maintainAspectRatio option must also be set to false. 
     maintainAspectRatio: false,
     scales: {
       yAxes: { display: false },
@@ -47,21 +57,19 @@ export default class ChartCard extends WeElement<Props> {
 
   render(props: Props) {
 
+    this.chartData.labels = props.labels
+    this.chartData.datasets[0].data = props.data
     return <div class="card text-white" style={{ backgroundColor: props.color }}>
       <div class="card-body card-body pb-0 d-flex justify-content-between align-items-start">
         <div>
-          <div class="text-value-lg">9823</div>
-          <div>Members online</div>
+          <div class="text-value-lg">{props.content}</div>
+          <div>{props.describe}</div>
         </div>
         <div class="icon">
           <slot>ICON</slot>
         </div>
       </div>
       <div class="c-chart-wrapper mt-3 mx-3" >
-        {/* 
-        https://www.chartjs.org/docs/latest/configuration/responsive.html
-        Note that in order for the above code to correctly resize the chart height, the maintainAspectRatio option must also be set to false. 
-        */}
         <o-line ignoreAttrs={true} height="70" data={this.chartData} options={this.chartOptions}></o-line>
 
       </div>
@@ -72,6 +80,6 @@ export default class ChartCard extends WeElement<Props> {
 
 declare global {
   interface HTMLElementTagNameMap {
-    'o-cascader': ChartCard
+    'o-chart-card': ChartCard
   }
 }
