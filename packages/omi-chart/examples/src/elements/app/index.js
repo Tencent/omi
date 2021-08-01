c9fd4ef651814443f6294f973c94c3c4e2ef4c93
dntzhang <dntzhang@qq.com>
2021-08-01 10:38:43 +0800

    chore(omi-chart): sync v6 version

---
+++ packages/omi-chart/examples/src/elements/app/index.js
@@ -0,0 +1,92 @@
+import { define, WeElement } from 'omi'
+import logo from './logo.svg'
+import style from './_index.css'
+import '../../chart'
+
+define('my-app', class extends WeElement {
+
+  install(){
+    this.chartData = {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    }
+    
+    this.chartOptions = {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  }
+
+  css() {
+    return style
+  }
+
+  onClick = ()=>{
+    this.chartData.datasets[0].data[0] = Math.random()*100
+    this.chartData.datasets[0].data[1] = Math.random()*100
+    this.chartData.datasets[0].data[2] = Math.random()*100
+    this.chartData.datasets[0].data[3] = Math.random()*100
+    this.chartData.datasets[0].data[4] = Math.random()*100
+    this.chartData.datasets[0].data[5] = Math.random()*100
+    this.update()
+  }
+
+
+  addDataset = ()=>{
+    this.chartData.datasets.push({
+      label: '# of Votes',
+      data: [Math.random()*100, Math.random()*100, Math.random()*100, Math.random()*100, Math.random()*100, Math.random()*100],
+      backgroundColor: 'rgba(25,202,173,0.2)',
+			borderColor: '#19CAAD',
+      borderWidth: 1
+    })
+    this.update()
+  }
+
+  render() {
+    return (
+      <div class="app">
+        <header class="app-header">
+          <img
+            src={logo}
+            class="app-logo"
+            alt="logo"
+          />
+          <h1 class="app-title">Welcome to omi-chart</h1>
+        </header>
+        <chart-bar width={375} data={this.chartData} options={this.chartOptions} />
+        <chart-line width={375} data={this.chartData} options={this.chartOptions} />
+        <chart-radar width={375} data={this.chartData} options={this.chartOptions} />
+        <div>
+          <button onClick={this.onClick}>random data</button>
+          <button onClick={this.addDataset}>add dataset</button>
+        </div>
+      </div>
+    )
+  }
+})
---
+++ packages/omi-chart/index.js
@@ -0,0 +1,100 @@
+import { WeElement, define } from 'omi'
+import Chart from 'chart.js'
+
+class ChartBase extends WeElement {
+  receiveProps(props) {
+    this.chart.data = props.data
+    this.chart.options = props.options
+    this.chart.update()
+  }
+
+  render(props) {
+    return (
+      <div style={{ width: props.width + 'px', height: props.height + 'px' }}>
+        <canvas ref={(e) => { this.canvas = e }}>
+        </canvas>
+      </div>
+    )
+  }
+}
+
+define('chart-bar', class extends ChartBase {
+  installed() {
+    this.chart = new Chart(this.canvas.getContext('2d'), {
+      type: this.props.horizontal ? 'horizontalBar' : 'bar',
+      data: this.props.data,
+      options: this.props.options
+    })
+  }
+})
+
+
+define('chart-line', class extends ChartBase {
+  installed() {
+    this.chart = new Chart(this.canvas.getContext('2d'), {
+      type: 'line',
+      data: this.props.data,
+      options: this.props.options
+    })
+  }
+})
+
+
+define('chart-radar', class extends ChartBase {
+  installed() {
+    this.chart = new Chart(this.canvas.getContext('2d'), {
+      type: 'radar',
+      data: this.props.data,
+      options: this.props.options
+    })
+  }
+})
+
+define('chart-scatter', class extends ChartBase {
+  installed() {
+    this.chart = new Chart.Scatter(this.canvas.getContext('2d'), {
+      data: this.props.data,
+      options: this.props.options
+    })
+  }
+})
+
+define('chart-doughnut', class extends ChartBase {
+  installed() {
+    this.chart = new Chart(this.canvas.getContext('2d'), {
+      type: 'doughnut',
+      data: this.props.data,
+      options: this.props.options
+    })
+  }
+})
+
+define('chart-pie', class extends ChartBase {
+  installed() {
+    this.chart = new Chart(this.canvas.getContext('2d'), {
+      type: 'pie',
+      data: this.props.data,
+      options: this.props.options
+    })
+  }
+})
+
+define('chart-polar-area', class extends ChartBase {
+  installed() {
+    this.chart = new Chart.PolarArea(this.canvas.getContext('2d'), {
+      data: this.props.data,
+      options: this.props.options
+    })
+  }
+})
+
+define('chart-bubble', class extends ChartBase {
+  installed() {
+    this.chart = new Chart(this.canvas.getContext('2d'), {
+      type: 'bubble',
+      data: this.props.data,
+      options: this.props.options
+    })
+  }
+})
+
