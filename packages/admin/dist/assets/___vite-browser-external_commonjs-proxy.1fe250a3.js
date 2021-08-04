b3535be89377a1c19b17299825481b3080c68ad6
dntzhang <dntzhang@qq.com>
2021-08-04 19:21:30 +0800

    feat(@omiu/table): support right column freezing

--- packages/admin/dist/assets/___vite-browser-external_commonjs-proxy.1fe250a3.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./vendor.ef3c1a07.js";var o=e(Object.freeze({__proto__:null,[Symbol.toStringTag]:"Module",default:{}}));export{o as r};
---
+++ packages/admin/dist/assets/___vite-browser-external_commonjs-proxy.c4d229e3.js
@@ -0,0 +1,1 @@
+import{i as o}from"./vendor.c903f3c9.js";var r=o(Object.freeze({__proto__:null,[Symbol.toStringTag]:"Module",default:{}}));export{r};
