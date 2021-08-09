45e9804f8d9a4a55a48c518c71d1f3cec5d3f208
dntzhang <dntzhang@qq.com>
2021-08-09 12:04:50 +0800

    chore(admin): rebuild

---
+++ packages/admin/dist/assets/___vite-browser-external_commonjs-proxy.3bd6201a.js
@@ -0,0 +1,1 @@
+import{l as e}from"./vendor.3de5fe22.js";var o=e(Object.freeze({__proto__:null,[Symbol.toStringTag]:"Module",default:{}}));export{o as r};
--- packages/admin/dist/assets/___vite-browser-external_commonjs-proxy.48053076.js
+++
@@ -1,1 +0,0 @@
-import{l as o}from"./vendor.b428748a.js";var r=o(Object.freeze({__proto__:null,[Symbol.toStringTag]:"Module",default:{}}));export{r};
