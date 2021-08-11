750858b9648b9cbbe97ad2e2ff6233d7a6ffe0ac
dntzhang <dntzhang@qq.com>
2021-08-11 18:59:44 +0800

    fix(admin): fix border error

--- packages/admin/src/layouts/basic-layout.tsx
+++ packages/admin/src/layouts/basic-layout.tsx
@@ -92,7 +92,7 @@ export default class extends WeElement {
         <div class={tw`flex flex-row`}>
           <layout-left-panel
             class={tw`${this.store.isLeftPanelClosed ? 'sm:w-0 -translate-x-full' : 'sm:w-64 w-3/4 translate-x-0'
-              } flex-none overflow-x-hidden overflow-y-auto bg-white z-50 transition-all duration-500 ease-in-out  sm:relative fixed `}
+              } flex-none border-r-1 overflow-x-hidden overflow-y-auto bg-white z-50 transition-all duration-500 ease-in-out  sm:relative fixed `}
           ></layout-left-panel>
           <layout-container class={tw`flex-1 flex-grow`}>
             <div class={tw`overflow-auto flex pt-0.5`} style={{ width: this.getMainContentWidth() }}>
