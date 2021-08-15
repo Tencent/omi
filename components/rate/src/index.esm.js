8a0a30ec27db04ec4933f13ae32981beda6c3784
dntzhang <dntzhang@qq.com>
2021-08-15 13:08:21 +0800

    chore(@omiu/rate): rebuild

--- components/rate/src/index.esm.js
+++ components/rate/src/index.esm.js
@@ -1,13 +1,12 @@
 /**
- * @omiu/button v0.0.9 http://omijs.org
+ * @omiu/rate v0.0.10 http://omijs.org
  * Front End Cross-Frameworks Framework.
  * By dntzhang https://github.com/dntzhang
  * Github: https://github.com/Tencent/omi
  * MIT Licensed.
  */
 
-import { h, tag, render, WeElement } from 'omi';
-import '@omiu/o-icon';
+import { h, extractClass, classNames, tag, WeElement } from 'omi';
 
 /*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
@@ -25,6 +24,58 @@ PERFORMANCE OF THIS SOFTWARE.
 ***************************************************************************** */
 /* global Reflect, Promise */
 
+var extendStatics$1 = function(d, b) {
+    extendStatics$1 = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return extendStatics$1(d, b);
+};
+
+function __extends$1(d, b) {
+    extendStatics$1(d, b);
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+}
+
+function __decorate$1(decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+}
+
+function __spreadArrays() {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+}
+
+/**
+ * @omiu/o-icon v0.0.1 http://omijs.org
+ * Front End Cross-Frameworks Framework.
+ * By dntzhang https://github.com/dntzhang
+ * Github: https://github.com/Tencent/omi
+ * MIT Licensed.
+ */
+
+/*! *****************************************************************************
+Copyright (c) Microsoft Corporation. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+this file except in compliance with the License. You may obtain a copy of the
+License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED
+WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions
+and limitations under the License.
+***************************************************************************** */
+/* global Reflect, Promise */
+
 var extendStatics = function(d, b) {
     extendStatics = Object.setPrototypeOf ||
         ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
@@ -38,12 +89,101 @@ function __extends(d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 }
 
+var __assign = function() {
+    __assign = Object.assign || function __assign(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+
 function __decorate(decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 }
+
+
+var css$1 = `:host {
+  display: inline-block; }
+
+.o-icon {
+  text-align: center;
+  display: inline-block; }
+
+.o-icon svg {
+  display: inline-block;
+  vertical-align: top; }
+
+.rotate {
+  display: inline-block;
+  -webkit-animation: rotate 1s infinite linear;
+  animation: rotate 1s infinite linear; }
+
+i div {
+  font-style: normal; }
+
+@-webkit-keyframes rotate {
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg); } }
+
+@keyframes rotate {
+  100% {
+    -webkit-transform: rotate(360deg);
+    transform: rotate(360deg); } }
+`;
+
+
+/** @class */ ((function (_super) {
+    __extends(Icon, _super);
+    function Icon() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Icon.prototype.render = function (props) {
+        return (h("i", __assign({}, extractClass(props, 'o-icon')),
+            h("svg", { viewBox: '0 0 ' + props.view + ' ' + props.view, class: classNames({ rotate: props.rotate }), width: props.scale + 'em', height: props.scale + 'em', fill: props.color, "aria-hidden": "true" }, props.paths ? (props.paths.map(function (item) {
+                var attrs = { d: item.path, fill: props.color || 'black' };
+                if (item.color)
+                    attrs.fill = item.color;
+                return h("path", __assign({}, attrs));
+            })) : h("path", { d: props.path })),
+            props.children && (h("div", { style: "color:" + (props.color || 'black') + ";" }, props.children[0]))));
+    };
+    Icon.css = css$1;
+    Icon.defaultProps = {
+        view: 1024,
+        scale: 2
+    };
+    Icon.propTypes = {
+        path: String,
+        paths: Object,
+        view: Number,
+        scale: Number,
+        color: String,
+        rotate: Boolean
+    };
+    Icon = __decorate([
+        tag('o-icon')
+    ], Icon);
+    return Icon;
+})(WeElement));
+
+
+var css = `.rate {
+  position: absolute;
+  left: 30px;
+  top: 10px; }
+
+.rating {
+  overflow: hidden;
+  float: left; }
+`
+
 
 var state = function (data, base) { return new Proxy(data, {
     set: function (target, propKey, value, receiver) {
@@ -52,69 +192,76 @@ var state = function (data, base) { return new Proxy(data, {
         return true;
     }
 }); };
-
 var Rate = /** @class */ (function (_super) {
-    __extends(Rate, _super);
+    __extends$1(Rate, _super);
     function Rate() {
-        var _this = _super.call(this) || this;
-        _this.onMouseover = function (evt) {
-            _this.state.value = evt.target.dataset['rate'];
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.handleMousemove = function (evt) {
+            if (!_this.props.disabled && !_this.props.readonly) {
+                _this.state.value = evt.target.dataset['rate'];
+            }
         };
-        _this.onMouseout = function () {
-            _this.state.value = _this.props.value;
+        _this.handleMouseleave = function (evt) {
+            if (!_this.props.disabled && !_this.props.readonly) {
+                _this.state.value = _this.props.value;
+            }
         };
-        _this.state = state({
-            value: _this.props.value
-        }, _this);
         return _this;
     }
+    Rate.prototype.install = function () {
+        this.state = state({
+            value: this.props.value
+        }, this);
+    };
     Rate.prototype.submit = function (index) {
-        this.updateProps({
-            value: index
-        });
+        if (!this.props.disabled) {
+            this.updateProps({
+                value: index,
+                readonly: !this.props.readonly
+            });
+        }
     };
     Rate.prototype.render = function (props) {
         var _this = this;
         var value = this.state.value;
-        var emptynum = Array.from({ length: Math.floor(props.max) }, function (v, k) { return k + 1; });
+        var arr = Array.from({ length: props.max * 2 - 1 }).reduce(function (pre, cur) { return __spreadArrays(pre, [pre[pre.length - 1] + 0.5]); }, [0.5]);
+        var that = this;
         function getCls(value, max) {
             if (value <= max)
-                return '#f7e620';
+                return that.props.color;
             if (value > max)
                 return '#ccc';
-            // return 'half'
         }
-        return (h("div", { className: "rating" }, emptynum.map(function (rate, index) { return (h("o-icon", { view: '24', color: getCls(index, value), scale: '2', path: props.svg, onMouseover: _this.onMouseover, onMouseout: _this.onMouseout, onClick: _this.submit.bind(_this, index), "data-rate": index })); })));
+        return (h("div", { class: "rate" }, arr.map(function (rate, index) { return (h("div", { class: "rating", style: { width: props.width / 2, paddingRight: (index % 2 == 0 ? 0 : props.width / 4) + "px" }, onMousemove: _this.handleMousemove, onMouseleave: _this.handleMouseleave, onClick: _this.submit.bind(_this, rate), "data-rate": rate },
+            h("svg", { t: "1627617455331", class: "icon", viewBox: "0 0 1024 1024", version: "1.1", xmlns: "http://www.w3.org/2000/svg", "p-id": "1209", width: props.width, height: props.width, fill: getCls(rate, value), "data-rate": rate, style: { transform: "translateX(" + (index % 2 == 0 ? 0 : props.width / 2 * -1) + "px)" } },
+                h("path", { d: "M964.685714 330.4l-290.171428-42.171429L544.8 25.257143c-3.542857-7.2-9.371429-13.028571-16.571429-16.571429-18.057143-8.914286-40-1.485714-49.028571 16.571429L349.485714 288.228571l-290.171428 42.171429c-8 1.142857-15.314286 4.914286-20.914286 10.628571a36.628571 36.628571 0 0 0 0.685714 51.771429l209.942857 204.685714-49.6 289.028572a36.514286 36.514286 0 0 0 53.028572 38.514285L512 788.571429l259.542857 136.457142c7.085714 3.771429 15.314286 5.028571 23.2 3.657143 19.885714-3.428571 33.257143-22.285714 29.828572-42.171428l-49.6-289.028572 209.942857-204.685714c5.714286-5.6 9.485714-12.914286 10.628571-20.914286 3.085714-20-10.857143-38.514286-30.857143-41.485714z", "p-id": "1210", "data-rate": rate })))); })));
     };
-    Rate.css = undefined;
+    Rate.css = css;
     Rate.defaultProps = {
         size: 10,
-        value: 0,
-        max: 5,
-        precision: 1,
-        // readonly: false,
-        // disabled: false,
+        value: 1,
+        max: 10,
+        half: true,
+        disabled: false,
+        readonly: false,
         color: '#f7e620',
-        svg: 'M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z'
+        width: 32
     };
     Rate.propTypes = {
         value: Number,
         max: Number,
-        precision: Number,
-        // readonly: Boolean,
-        // disabled: Boolean,
+        half: Boolean,
+        disabled: Boolean,
+        readonly: Boolean,
         icon: String,
         color: String,
-        svg: String,
+        width: Number
     };
-    Rate = __decorate([
+    Rate = __decorate$1([
         tag('o-rate')
     ], Rate);
     return Rate;
 }(WeElement));
-render(h("o-rate", { value: 1 }), '#root', {
-    ignoreAttrs: true
-});
 
-export default Rate;
+export { Rate as default };
 //# sourceMappingURL=index.esm.js.map
