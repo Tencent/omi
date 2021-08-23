e869019684be9440bd014c6cec720a32fbfdd4fc
dntzhang <dntzhang@qq.com>
2021-08-23 21:11:07 +0800

    chore(admin): rebuild

---
+++ packages/admin/dist/assets/___vite-browser-external_commonjs-proxy.49d3e0d4.js
@@ -0,0 +1,1 @@
+import{i as o}from"./vendor.42431017.js";var r=o(Object.freeze({__proto__:null,[Symbol.toStringTag]:"Module",default:{}}));export{r};
--- packages/admin/dist/assets/___vite-browser-external_commonjs-proxy.e2cda3d0.js
+++
@@ -1,1 +0,0 @@
-import{i as o}from"./vendor.cff7a2c5.js";var r=o(Object.freeze({__proto__:null,[Symbol.toStringTag]:"Module",default:{}}));export{r};
