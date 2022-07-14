afc7db800f5b34aaddafff70cf04acf9eabdc437
dntzhang <dntzhang@tencent.com>
2022-07-14 14:28:57 +0800

    chore(admin): rebuild

---
+++ packages/admin/dist/assets/___vite-browser-external_commonjs-proxy.96aed09f.js
@@ -0,0 +1,1 @@
+import{g as e}from"./index.fc9addda.js";var r={},t=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"})),o=e(t);export{o as r};
--- packages/admin/dist/assets/___vite-browser-external_commonjs-proxy.ee515a19.js
+++
@@ -1,1 +0,0 @@
-import{g as e}from"./index.fb69cf45.js";var r={},t=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"})),o=e(t);export{o as r};
