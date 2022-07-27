993e5133f5b4cc7f26a3d7ee804b4f614385a59b
dntzhang <dntzhang@tencent.com>
2022-07-27 16:14:28 +0800

    chore(turorial): translate

--- packages/tutorial/src/sections/en/snake-game/app/snake-game.tsx
+++ packages/tutorial/src/sections/en/snake-game/app/snake-game.tsx
@@ -237,7 +237,7 @@ export default class extends WeElement {
             <div class="btn cm-btn cm-btn-dir down" onClick={store.turnDown}><i></i><em></em><span>Down</span></div>
             <div class="btn cm-btn cm-btn-dir left" onClick={store.turnLeft}><i></i><em></em><span >Left</span></div>
             <div class="btn cm-btn cm-btn-dir right" onClick={store.turnRight}><i></i><em></em><span >Right</span></div>
-            <div class="btn cm-btn space" onClick={store.toggleSpeed}><i></i><span >加速/减速</span></div>
+            <div class="btn cm-btn space" onClick={store.toggleSpeed}><i></i><span >Switch Accelerator</span></div>
             <div class="btn reset small" onClick={store.reset}><i ></i><span >Reset</span></div>
             <div class="btn pp small" onClick={store.pauseOrPlay}><i></i><span >{store.data.paused ? 'Play' : 'Pause'}</span></div>
           </div>
