0a7bea93a392a7c9efc05b4c95cfb914a63bcbfe
dntzhang <dntzhang@tencent.com>
2022-08-11 14:34:41 +0800

    feat(@omiu/message): using transition latest version

---
+++ components/message/src/demo.tsx
@@ -0,0 +1,7 @@
+import './index.tsx'
+
+function sucAlert(msg) {
+  document.body.innerHTML += '	<o-message type="success" center message="' + msg + '"></o-message>'
+}
+
+sucAlert('复制标签成功22 <xx-xx></xx-xx>')
