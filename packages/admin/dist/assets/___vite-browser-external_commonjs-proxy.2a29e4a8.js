ca628df401ad053a4e054fc9e6f22b56dba9011b
YunYouJun <me@yunyoujun.cn>
2022-08-29 02:02:49 +0800

    refactor: simplify router generate & fix loss dependencies

---
+++ packages/admin/dist/assets/___vite-browser-external_commonjs-proxy.2a29e4a8.js
@@ -0,0 +1,1 @@
+import{g as e}from"./index.02c94443.js";var r={},t=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"})),o=e(t);export{o as r};
--- packages/admin/dist/assets/radio-component.8b81db8c.js
+++
@@ -1,9 +0,0 @@
-import{W as l,h as o,t as n,s as m,a as p}from"./index.bf6785d8.js";import"./index.esm.9fc0db10.js";import"./admin-docs.35672bd1.js";import"./index.f7b137ab.js";import"./container.f851b825.js";import"./encode.47cdd865.js";import"./___vite-browser-external_commonjs-proxy.9dfe6bbb.js";var c=Object.defineProperty,x=Object.getOwnPropertyDescriptor,b=(i,a,u,r)=>{for(var e=r>1?void 0:r?x(a,u):a,t=i.length-1,d;t>=0;t--)(d=i[t])&&(e=(r?d(a,u,e):d(e))||e);return r&&e&&c(a,u,e),e};const f="radio-component";let s=class extends l{install(){}render(){return o("code-demo-container",null,o("code-demo",{title:"\u5355\u9009\u6846",describe:"\u7528\u4E8E\u5728\u591A\u4E2A\u9009\u9879\u4E2D\u9009\u62E9\u4E00\u4E2A",code:`
-\`\`\`jsx
-<o-radio name="sex" checked={true} value="0">\u7537</o-radio>
-<o-radio name="sex" value="1">\u5973</o-radio>
-
-<o-radio disabled={true} name="sex-b">\u7537</o-radio>
-<o-radio disabled={true} name="sex-b">\u5973</o-radio>
-\`\`\`
-          `},o("div",{slot:"demo",class:n`flex justify-around px-5 py-5`},o("o-radio",{name:"sex",checked:!0,value:"0"},"\u7537"),o("o-radio",{name:"sex",value:"1"},"\u5973"),o("o-radio",{disabled:!0,name:"sex-b"},"\u7537"),o("o-radio",{disabled:!0,name:"sex-b"},"\u5973"))))}};s.css=[m.target,"o-radio{margin-left:10px;}"];s=b([p(f)],s);export{s as default};
