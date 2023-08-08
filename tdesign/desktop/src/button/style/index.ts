80b5136ed960041955f6ce9da7252ff0ad948929
dntzhang <dntzhang@qq.com>
2023-08-09 06:36:48 +0800

    fix: less文件加载失败

--- tdesign/desktop/src/button/style/index.ts
+++ tdesign/desktop/src/button/style/index.ts
@@ -1,5 +1,4 @@
-
-import styles from '@common/style/web/components/button/_index.less'
-import theme from '@common/style/web/theme/_index.less'
+import styles from  '../../_common/style/web/components/button/_index.less'
+import theme from  '../../_common/style/web/theme/_index.less'
 
 export default theme + styles
