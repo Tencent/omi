a3f8f8d3ddbf7aa354337289ffdd1165d1d59a80
ziMu-66ccff <3553189793@qq.com>
2023-08-08 18:28:31 +0800

    chore: tdesign-omi初始化了基本配置 配置了组件库文档静态网站基于vitepress

---
+++ tdesign/desktop/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: ['standard-with-typescript', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {}
+}
