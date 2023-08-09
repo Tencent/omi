0059350c93c76246416c74e46ced88581957d22c
rex <rainbowMorel@163.com>
2023-08-09 23:00:31 +0800

    feat: 格式化代码

--- tdesign/desktop/src/button/types.ts
+++ tdesign/desktop/src/button/types.ts
@@ -1,20 +1,20 @@
-import { SizeEnum, TElement, TNode } from '../common';
+import { SizeEnum, TElement, TNode } from '../common'
 export type ButtonProps = {
-    block?: boolean;
-    disable?: boolean;
-    ghost?: boolean;
-    loading?: boolean;
+  block?: boolean
+  disable?: boolean
+  ghost?: boolean
+  loading?: boolean
 
-    href?: string;
-    icon?: string | TNode;
+  href?: string
+  icon?: string | TNode
 
-    size?: SizeEnum;
-    suffix?: TElement;
+  size?: SizeEnum
+  suffix?: TElement
 
-    shape?: 'rectangle' | 'square' | 'round' | 'circle';
-    tag?: 'button' | 'a' | 'div';
-    type?: 'submit' | 'reset' | 'button';
-    variant?: 'base' | 'outline' | 'dashed' | 'text';
-  
-    onClick?: (e: MouseEvent) => void;
+  shape?: 'rectangle' | 'square' | 'round' | 'circle'
+  tag?: 'button' | 'a' | 'div'
+  type?: 'submit' | 'reset' | 'button'
+  variant?: 'base' | 'outline' | 'dashed' | 'text'
+
+  onClick?: (e: MouseEvent) => void
 } & HTMLButtonElement
