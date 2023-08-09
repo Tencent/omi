85f81c3f3ac94e5ae3e2cce1294214c4ac91d59d
GuYith <48597370+GuYith@users.noreply.github.com>
2023-08-09 23:24:25 +0800

    feat: add and init link component

---
+++ tdesign/desktop/src/link/style/index.ts
@@ -0,0 +1,3 @@
+import linkStyle from '../../_common/style/web/components/link/_index.less'
+import theme from '../../_common/style/web/theme/_index.less'
+export default linkStyle + theme
---
+++ tdesign/desktop/src/link/theme.tsx
@@ -0,0 +1,25 @@
+import { h, tag, render, WeElement } from 'omi'
+
+import './link'
+
+@tag('link-theme')
+export default class LinkTheme extends WeElement<Props> {
+  static css = `
+    t-link{ margin: 0 5px }
+  `
+  render(props) {
+    return (
+      <div>
+        <t-link theme="default">跳转链接</t-link>
+        <t-link theme="primary">跳转链接</t-link>
+        <t-link theme="danger">跳转链接</t-link>
+        <t-link theme="warning">跳转链接</t-link>
+        <t-link theme="success">跳转链接</t-link>
+      </div>
+    )
+  }
+}
+
+render(<link-theme></link-theme>, 'body', {
+  ignoreAttrs: true
+})
