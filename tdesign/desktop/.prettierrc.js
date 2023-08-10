2bc7dbb357378438d5d342563caec9dac0525542
GuYith <48597370+GuYith@users.noreply.github.com>
2023-08-10 11:53:37 +0800

    style: modify .prettierrc.js

--- tdesign/desktop/.prettierrc.js
+++ tdesign/desktop/.prettierrc.js
@@ -5,8 +5,8 @@ module.exports = {
   tabWidth: 2,
   // 不使用缩进符，而使用空格
   useTabs: false,
-  // 行尾需要有分号
-  semi: true,
+  // 行尾不加分号
+  semi: false,
   // 使用单引号
   singleQuote: true,
   // 对象的 key 仅在必要时用引号
