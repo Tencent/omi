2b3b93857524a417a685df40bcb55f4c1307b3a5
dntzhang <dntzhang@tencent.com>
2023-10-09 16:17:57 +0800

    docs: add snake game example, simple signal demo

---
+++ packages/snake-game/src/model/game.ts
@@ -0,0 +1,137 @@
+import Snake from './snake'
+import { Signal } from 'omi'
+
+type SignalValueType = {
+  map: number[][],
+  paused: boolean
+}
+
+class Game extends Signal<SignalValueType>{
+  snake: Snake
+  size: number
+  loop: number
+  interval: number
+  _preDate: number
+  food: [number, number]
+
+  constructor() {
+    super({ map: [], paused: false })
+    this.size = 16
+    this.loop = null
+    this.interval = 500
+    this._preDate = Date.now()
+    this.init()
+  }
+
+  init() {
+    const { map } = this.value
+    this.snake = new Snake
+    for (let i = 0; i < this.size; i++) {
+      const row = []
+      for (let j = 0; j < this.size; j++) {
+        row.push(0)
+      }
+      map.push(row)
+    }
+  }
+
+  tick() {
+    this.makeFood()
+    const eating = this.eat()
+    this.snake.move(eating)
+    this.mark()
+    this.update()
+  }
+
+  mark() {
+    const { map } = this.value
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
+        map[i][j] = 0
+      }
+    }
+
+    for (let k = 0, len = this.snake.body.length; k < len; k += 2) {
+      this.snake.body[k + 1] %= this.size
+      this.snake.body[k] %= this.size
+
+      if (this.snake.body[k + 1] < 0) this.snake.body[k + 1] += this.size
+      if (this.snake.body[k] < 0) this.snake.body[k] += this.size
+      map[this.snake.body[k + 1]][this.snake.body[k]] = 1
+    }
+    if (this.food) {
+      map[this.food[1]][this.food[0]] = 1
+    }
+  }
+
+  start() {
+    this.loop = setInterval(() => {
+      if (Date.now() - this._preDate > this.interval) {
+        this._preDate = Date.now()
+        if (!this.value.paused) {
+          this.tick()
+        }
+      }
+    }, 16)
+  }
+
+  stop() {
+    clearInterval(this.loop)
+  }
+
+  pause() {
+    this.value.paused = true
+  }
+
+  play() {
+    this.value.paused = false
+  }
+
+  pauseOrPlay = () => { 
+    this.value.paused = !this.value.paused
+    this.update()
+  }
+
+  reset = () => {
+    this.value.paused = false
+    this.interval = 500
+    this.snake.body = [3, 1, 2, 1, 1, 1]
+    this.food = null
+    this.snake.dir = 'right'
+  }
+
+  toggleSpeed = () => {
+    this.interval === 500 ? (this.interval = 150) : (this.interval = 500)
+  }
+
+  makeFood() {
+    if (!this.food) {
+      this.food = [this._rd(0, this.size - 1), this._rd(0, this.size - 1)]
+      for (let k = 0, len = this.snake.body.length; k < len; k += 2) {
+        if (this.snake.body[k + 1] === this.food[1]
+          && this.snake.body[k] === this.food[0]) {
+          this.food = null
+          this.makeFood()
+          break
+        }
+
+      }
+    }
+  }
+
+  eat() {
+    for (let k = 0, len = this.snake.body.length; k < len; k += 2) {
+      if (this.snake.body[k + 1] === this.food[1]
+        && this.snake.body[k] === this.food[0]) {
+        this.food = null
+        return true
+      }
+    }
+  }
+
+  _rd(from, to) {
+    return from + Math.floor(Math.random() * (to + 1))
+  }
+}
+
+export default Game
