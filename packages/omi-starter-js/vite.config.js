1a66839bc5d2b2739e16135d1ed55a62f642228b
dntzhang <dntzhang@tencent.com>
2023-10-12 09:20:22 +0800

    feat(omi-starter-js): completed

---
+++ packages/omi-starter-js/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  esbuild: {
+    jsxFactory: "h",
+    jsxFragment: "h.f"
+  },
+})
