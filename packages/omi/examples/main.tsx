d79a59206ebc6c17169b9f3dc156356ab11b2d42
dntzhang <dntzhang@qq.com>
2023-10-16 07:11:40 +0800

    docs: add sharing-styles example

--- packages/omi/examples/main.tsx
+++ packages/omi/examples/main.tsx
@@ -5,6 +5,9 @@ switch (location.hash) {
   case '#signal':
     import('./signal')
     break
+  case '#sharing-styles':
+    import('./sharing-styles')
+    break
   default:
     import('./todo-app')
 }
