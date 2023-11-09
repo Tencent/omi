6f72e827975b19493481db32aad17468954ce0c3
dntzhang <dntzhang@tencent.com>
2023-11-09 09:22:17 +0800

    refactor(omi-transition): code optimization

--- packages/omi-elements/src/components/transition-directive.ts
+++ packages/omi-elements/src/components/transition-directive.ts
@@ -1,27 +1,12 @@
 import { registerDirective, Component } from 'omi'
 
-function debounce(func: (...args: any[]) => void, wait: number) {
-  let timeout: NodeJS.Timeout | null
-  return function (...args: any[]) {
-    if (timeout) {
-      clearTimeout(timeout)
-    }
-    timeout = setTimeout(() => {
-      // @ts-ignore
-      func.apply(this, args)
-    }, wait)
-  }
-}
 
 registerDirective('transition', (dom: HTMLElement | Component, options: { name: string; delay?: number }) => {
   const { name, delay = 0 } = options
   const observer = new MutationObserver((mutations) => {
     mutations.forEach((mutation) => {
       if (mutation.type === 'attributes' && mutation.attributeName === 'show') {
-        const show =
-          dom.getAttribute('show') === 'true' ||
-          dom.getAttribute('show') === '1' ||
-          (dom as Component<{ show: boolean }>).props?.show
+        const show = getShowAttribute(dom)
         updateClasses(dom, name, show, delay)
       }
     })
@@ -29,44 +14,58 @@ registerDirective('transition', (dom: HTMLElement | Component, options: { name: 
 
   observer.observe(dom, { attributes: true })
 
-  const onTransitionEnd = () => {
-    dom.classList.remove(name + '-enter-active')
-    dom.classList.remove(name + '-leave-active')
-  }
+  const onTransitionEnd = debounce(() => {
+    dom.classList.remove(`${name}-enter-active`)
+    dom.classList.remove(`${name}-leave-active`)
+  }, 0)
 
-  dom.addEventListener('transitionend', debounce(onTransitionEnd, 0))
-  dom.addEventListener('animationend', debounce(onTransitionEnd, 0))
+  dom.addEventListener('transitionend', onTransitionEnd)
+  dom.addEventListener('animationend', onTransitionEnd)
 
-  const show =
-    dom.getAttribute('show') === 'true' ||
-    dom.getAttribute('show') === '1' ||
-    (dom as Component<{ show: boolean }>).props?.show
-
+  const show = getShowAttribute(dom)
   updateClasses(dom, name, show, delay)
 })
 
+function getShowAttribute(dom: HTMLElement | Component<{ show: boolean }>): boolean {
+  return dom.getAttribute('show') === 'true' ||
+    dom.getAttribute('show') === '1' ||
+    dom.props?.show
+}
+
 function updateClasses(dom: HTMLElement, name: string, show: boolean, delay: number) {
   if (show) {
-    dom.classList.remove(name + '-leave-to')
-    dom.classList.remove(name + '-leave-active')
-    dom.classList.add(name + '-enter-from')
+    dom.classList.remove(`${name}-leave-to`)
+    dom.classList.remove(`${name}-leave-active`)
+    dom.classList.add(`${name}-enter-from`)
     requestAnimationFrame(() => {
       setTimeout(() => {
-        dom.classList.remove(name + '-enter-from')
-        dom.classList.add(name + '-enter-to')
-        dom.classList.add(name + '-enter-active')
+        dom.classList.remove(`${name}-enter-from`)
+        dom.classList.add(`${name}-enter-to`)
+        dom.classList.add(`${name}-enter-active`)
       }, 1 + delay)
     })
   } else {
-    dom.classList.remove(name + '-enter-to')
-    dom.classList.remove(name + '-enter-active')
-    dom.classList.add(name + '-leave-from')
+    dom.classList.remove(`${name}-enter-to`)
+    dom.classList.remove(`${name}-enter-active`)
+    dom.classList.add(`${name}-leave-from`)
     requestAnimationFrame(() => {
       setTimeout(() => {
-        dom.classList.remove(name + '-leave-from')
-        dom.classList.add(name + '-leave-to')
-        dom.classList.add(name + '-leave-active')
+        dom.classList.remove(`${name}-leave-from`)
+        dom.classList.add(`${name}-leave-to`)
+        dom.classList.add(`${name}-leave-active`)
       }, 1 + delay)
     })
+  }
+}
+
+function debounce(func: (...args: any[]) => void, wait: number) {
+  let timeout: NodeJS.Timeout | null
+  return function (...args: any[]) {
+    if (timeout) {
+      clearTimeout(timeout)
+    }
+    timeout = setTimeout(() => {
+      func.apply(this, args)
+    }, wait)
   }
 }
