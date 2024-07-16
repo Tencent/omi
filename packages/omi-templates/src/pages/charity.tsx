605201e715bc011eb7156ee58fcc3545747ef874
kiameow <zjzootl@qq.com>
2024-07-16 11:25:54 +0800

    feat(omi-templates):make charity template responsive to multi device

--- packages/omi-templates/src/pages/charity.tsx
+++ packages/omi-templates/src/pages/charity.tsx
@@ -80,15 +80,16 @@ class CharityTemplate extends Component {
         <div class="hero-section w-full h-screen relative">
           <img class="w-full h-full object-cover" src="https://s2.loli.net/2024/07/15/hORgyQbYzvWD4I2.jpg" />
           <div class="absolute bottom-6 left-6">
-            <h1 class="text-white text-8xl font-bold mb-2">携手共筑爱的桥梁<br />关爱留守儿童行动</h1>
-            <p class="text-white text-3xl">让我们共同为每一个孤独的心灵点亮希望的灯塔</p>
+            <h1 class="text-white text-4xl sm:text-6xl lg:text-8xl font-bold mb-2">携手共筑爱的桥梁<br />关爱留守儿童行动</h1>
+            <p class="text-white text-base md:text-3xl">让我们共同为每一个孤独的心灵点亮希望的灯塔</p>
           </div>
         </div>
-        <div class="w-full px-16 py-12 overflow-hidden flex justify-center">
-          <div class="flex gap-20">
+
+        <div class="w-full py-12 overflow-hidden flex justify-center">
+          <div class="grid grid-cols-1 xl:grid-cols-3 gap-20 2xl:gap-40">
             {
               stories.value.map((story, index) => (
-                <div class="w-[320px] flex flex-none flex-col items-center" 
+                <div class="w-[320px] md:w-[600px] xl:w-[320px] flex flex-none flex-col items-center" 
                   key={index}>
                   <a href={story.article_link}><img class="w-[320px] h-[260px] object-cover rounded-lg" src={story.image} /></a>
                   <h3 class="text-2xl font-bold mt-2 mb-2">{story.title}</h3>
@@ -101,29 +102,29 @@ class CharityTemplate extends Component {
 
         <div class={`about-section w-full flex justify-center bg-amber-100 dark:bg-amber-900 py-8`}>
           <div class="w-10/12 flex items-center justify-center gap-12">
-            <div class="w-5/12 flex flex-col justify-center">
-              <h2 class="text-5xl mb-4">关于我们 —— <br />我们的使命与愿景</h2>
+            <div class="w-11/12 md:w-5/12 flex flex-col justify-center">
+              <h2 class="text-3xl md:text-5xl mb-4">关于我们 —— <br />我们的使命与愿景</h2>
               <p class="text-base">我们是一家致力于改善中国留守儿童生活质量的非营利组织。我们的使命是通过教育、健康和情感支持，确保每个孩子都能享有平等的成长机会。我们相信，每个孩子都有无限的潜力，只需一点爱与关注，就能绽放出最耀眼的光芒。</p>
             </div>
-            <div class="w-7/12">
-              <img class="object-cover rounded-lg" src="https://s2.loli.net/2024/07/15/ljcfdbWUwNu2vns.jpg" />
+            <div class="w-7/12 hidden md:block">
+              <img class="object-cover md:h-auto rounded-lg" src="https://s2.loli.net/2024/07/15/ljcfdbWUwNu2vns.jpg" />
             </div>
           </div>
         </div>
 
-        <div class="impact-section w-full py-12 flex gap-20 justify-center">
-          <div class="w-1/3 flex flex-col gap-6">
+        <div class="impact-section w-full py-12 flex flex-col sm:flex-row gap-20 justify-center items-center">
+          <div class="w-11/12 sm:w-1/3 flex sm:flex-col justify-center sm:justify-normal gap-6">
             {
               sortedStats.map(stat => (
                 <div>
-                  <span class="text-4xl font-bold mr-2">{stat.value}</span>
-                  <span class="text-lg">{stat.name}</span>
+                  <span class="text-2xl sm:text-4xl font-bold mr-2">{stat.value}</span>
+                  <span class="text-base sm:text-lg">{stat.name}</span>
                 </div>
               )) 
             }
           </div>
-          <div class="w-1/3 flex flex-col gap-6 justify-center">
-            <h2 class="text-5xl text-center">改变正在发生</h2>
+          <div class="w-9/12 sm:w-1/3 flex flex-col gap-6 justify-center">
+            <h2 class="text-3xl sm:text-5xl text-center whitespace-nowrap">改变正在发生</h2>
             <p>自成立以来，我们的足迹已覆盖全国多个省份，帮助超过3000名留守儿童，显著提高了他们的学习动力、社交能力和心理韧性。</p>
             <div class="flex justify-center items-center gap-4">
               <o-button   
@@ -144,17 +145,17 @@ class CharityTemplate extends Component {
           </div>
         </div>
 
-        <div class={`feature-section w-full flex flex-col items-center py-12 bg-amber-100 dark:bg-amber-900`}>
-          <h2 class="text-5xl mb-16">我们所提供的</h2>
-          <div class="grid grid-cols-3 gap-y-8">
+        <div class={`feature-section w-full flex flex-col items-center py-12 px-6 bg-amber-100 dark:bg-amber-900`}>
+          <h2 class="text-3xl sm:text-5xl mb-16">我们所提供的</h2>
+          <div class="grid grid-cols-2 sm:grid-cols-3 gap-y-8 gap-x-6">
             {
               features.map(feature => (
-                <div class="flex items-center gap-4">
-                  <div>
-                    <i class={`${feature.icon} text-3xl`}></i>
+                <div class="flex gap-4">
+                  <div class="pt-4">
+                    <i class={`${feature.icon} text-xl sm:text-3xl`}></i>
                   </div>
                   <div>
-                    <h3 class="text-2xl">{ feature.name }</h3>
+                    <h3 class="text-xl sm:text-2xl font-bold">{ feature.name }</h3>
                     <p class="text-base">{ feature.description }</p>
                   </div>
                 </div>
@@ -166,7 +167,7 @@ class CharityTemplate extends Component {
         <div class="cta-section w-full py-12">
           <div class="relative w-10/12 mx-auto">
             <img class="w-full mx-auto h-[480px] object-cover rounded-lg" src="https://s2.loli.net/2024/07/15/lNMtiXHabTev76Q.jpg" />
-            <a href={signUpLink} class=""><h2 class="text-7xl text-white underline absolute bottom-8 left-8">加入我们,<br />一起创造更多可能</h2></a>
+            <a href={signUpLink} class=""><h2 class="text-4xl sm:text-5xl md:text-7xl text-white underline absolute bottom-8 left-8">加入我们,<br />一起创造更多可能</h2></a>
           </div>
         </div>
       </>
