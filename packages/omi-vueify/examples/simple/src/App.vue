2ae52c6f11c67ffab0a433fd0b94474689fa85ff
mpbfx <1835886801@qq.com>
2025-07-24 17:53:25 +0800

    fix: 保持vue组件为PascalCase命名 omi组件为kebab-case命名

--- packages/omi-vueify/examples/simple/src/App.vue
+++ packages/omi-vueify/examples/simple/src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
-import TreeComponent from './components/tree-component.vue'
+import TreeComponent from './components/TreeComponent.vue'
 </script>
 
 <template>
